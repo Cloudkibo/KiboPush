2236056043995c2186070fd540f34af5bbc54125
Sojharo <sojharo@live.com>
2017-08-03 06:01:52 +0500

    mongodb production string

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -8,8 +8,8 @@
 module.exports = {
   // MySQL connection options
   mongo: {
-    uri:    process.env.MONGO_URI ||
-    'mongodb://root:a345rq98efw@localhost/kibopush'
+    uri:    process.env.MONGO_URI || 'mongodb://localhost/kibopush'
+    //'mongodb://root:a345rq98efw@localhost/kibopush'
   },
   seedDB: false
 };
