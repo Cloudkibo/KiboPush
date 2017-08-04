9b807750548c7e62ed5750274dff229169b84f8d
Sojharo <sojharo@live.com>
2017-08-04 22:59:13 +0500

    redirect #152

--- server/routes.js
+++ server/routes.js
@@ -38,6 +38,7 @@ module.exports = function (app) {
   });
 
   app.use((req, res) => {
-    res.status(404).send({ url: `${req.originalUrl} not found` });
+    // res.status(404).send({ url: `${req.originalUrl} not found` });
+    res.redirect('/');
   });
 };
