9550d2663de2aa3d92fdd03352a370f6aacd4088
Sojharo <sojharo@live.com>
2017-08-04 22:48:37 +0500

    redirect test  #152

--- server/api/polls/polls.controller.js
+++ server/api/polls/polls.controller.js
@@ -16,140 +16,135 @@ exports.index = function (req, res) {
   Polls.find((err, polls) => {
     logger.serverLog(TAG, polls);
     logger.serverLog(TAG, `Error: ${err}`);
-    res.status(200).json({ status: 'success', payload: polls });
+    res.status(200).json({status: 'success', payload: polls});
   });
 };
 
 exports.create = function (req, res) {
-    const poll = new Polls({ platform: 'facebook',statement: req.body.statement,options: req.body.options,sent: 0 });
+  const poll = new Polls({platform: 'facebook', statement: req.body.statement, options: req.body.options, sent: 0});
 
-    //save model to MongoDB
-    poll.save((err) => {
-      if (err) {
-        res.status(500).json({ status: 'Failed', error: err, description: 'Failed to insert record' });
-      } else {
-        res.status(200).json({ status: 'Success' });
-      }
-    });
+  //save model to MongoDB
+  poll.save((err) => {
+    if (err) {
+      res.status(500).json({status: 'Failed', error: err, description: 'Failed to insert record'});
+    } else {
+      res.status(200).json({status: 'Success'});
+    }
+  });
 };
 
 exports.submitresponses = function (req, res) {
-    logger.serverLog(TAG, `Inside submitresponses of Poll ${JSON.stringify(req.body)}`);
-    /*
-    Expected body
-    {
-    response:String,//response submitted by subscriber
-    pollId: _id of Poll,
-    subscriberId: _id of subscriber,
-    }
-    */
+  logger.serverLog(TAG, `Inside submitresponses of Poll ${JSON.stringify(req.body)}`);
+  /*
+   Expected body
+   {
+   response:String,//response submitted by subscriber
+   pollId: _id of Poll,
+   subscriberId: _id of subscriber,
+   }
+   */
 
-    PollResponse.create(req.body, (err, pollresponse) => {
+  PollResponse.create(req.body, (err, pollresponse) => {
     if (err) {
-       return res.status(404).json({ status: 'failed', description: 'Poll response not created' });
-     }
-      return res.status(200).json({ status: 'success', payload: pollresponse });
-   });
+      return res.status(404).json({status: 'failed', description: 'Poll response not created'});
+    }
+    return res.status(200).json({status: 'success', payload: pollresponse});
+  });
 };
 
 
 exports.getresponses = function (req, res) {
-    logger.serverLog(TAG, 'Inside getresponses of Poll');
+  logger.serverLog(TAG, 'Inside getresponses of Poll');
 
-    PollResponse.find({ pollId: req.params.id }).populate('pollId subscriberId').exec((err, pollresponses) => {
-              if (err) {
-                return res.status(404).json({ status: 'failed', description: 'Poll responses not found' });
-              }
+  PollResponse.find({pollId: req.params.id}).populate('pollId subscriberId').exec((err, pollresponses) => {
+    if (err) {
+      return res.status(404).json({status: 'failed', description: 'Poll responses not found'});
+    }
 
-                return res.status(200).json({ status: 'success', payload: pollresponses });
-             });
+    return res.status(200).json({status: 'success', payload: pollresponses});
+  });
 };
 exports.report = function (req, res) {
 
 };
 exports.send = function (req, res) {
-    logger.serverLog(TAG, 'Inside sendpoll ' + JSON.stringify(req.body));
-    /*
-    Expected request body
-     { platform: 'facebook',statement: req.body.statement,options: req.body.options,sent: 0 });
+  logger.serverLog(TAG, `Inside sendpoll ${JSON.stringify(req.body)}`);
+  /*
+   Expected request body
+   { platform: 'facebook',statement: req.body.statement,options: req.body.options,sent: 0 });
 
-    */
+   */
 
-    var messageData={
-                            "attachment":{
-                              "type":"template",
-                              "payload":{
-                                "template_type":"button",
-                                "text":req.body.statement,
-                                "buttons":[
-                                  {
-                                    "type":"postback",
-                                    "title":req.body.options[0],
-                                     "payload":JSON.stringify({poll_id:req.body._id,option:req.body.options[0]})
-                                  },
-                                  {
-                                    "type":"postback",
-                                    "title":req.body.options[1],
-                                     "payload":JSON.stringify({poll_id:req.body._id,option:req.body.options[1]})
-                                  },
-                                  {
-                                    "type":"postback",
-                                    "title":req.body.options[2],
-                                     "payload":JSON.stringify({poll_id:req.body._id,option:req.body.options[2]})
-                                  }
-                                ]
-                              }
-                            }
-                          }
-        logger.serverLog(TAG, 'Poll to be sent ' + JSON.stringify(messageData));
-        
-        Subscribers.find({pageId:req.body.pageId}, function (err, subscribers) {
-          if(err) { 
-            return res.status(404).json({ status: 'failed', description: 'Subscribers not found'});
+  const messageData = {
+    attachment: {
+      type: 'template',
+      payload: {
+        template_type: 'button',
+        text: req.body.statement,
+        buttons: [
+          {
+            type: 'postback',
+            title: req.body.options[0],
+            payload: JSON.stringify({poll_id: req.body._id, option: req.body.options[0]})
+          },
+          {
+            type: 'postback',
+            title: req.body.options[1],
+            payload: JSON.stringify({poll_id: req.body._id, option: req.body.options[1]})
+          },
+          {
+            type: 'postback',
+            title: req.body.options[2],
+            payload: JSON.stringify({poll_id: req.body._id, option: req.body.options[2]})
           }
-          for(var j=0;j< subscribers.length;j++){
+        ]
+      }
+    }
+  };
+  logger.serverLog(TAG, `Poll to be sent ${JSON.stringify(messageData)}`);
 
-                  var data = {
-                              recipient: {id: subscribers[j].userId}, //this is the subscriber id
-                              message: messageData,
-                            }
-                  var options= {
-                      qs: {access_token: req.body.pageToken},
-                     }
-                  needle.post('https://graph.facebook.com/v2.6/me/messages', data,options, (err, resp) => {
-                    
-                       if (err) {
-                          return res.status(404).json({ status: 'failed', description: err});
-                        }
+  Subscribers.find({pageId: req.body.pageId}, (err, subscribers) => {
+    if (err) {
+      return res.status(404).json({status: 'failed', description: 'Subscribers not found'});
+    }
+    for (let j = 0; j < subscribers.length; j++) {
+      const data = {
+        recipient: {id: subscribers[j].userId}, //this is the subscriber id
+        message: messageData,
+      };
+      const options = {
+        qs: {access_token: req.body.pageToken},
+      };
+      needle.post('https://graph.facebook.com/v2.6/me/messages', data, options, (err, resp) => {
+        if (err) {
+          return res.status(404).json({status: 'failed', description: err});
+        }
 
-                         // return res.status(200).json({ status: 'success', payload: resp.body });
-                    
-                  });
-          }
-          return res.status(200).json({ status: 'success', payload: 'Poll sent successfully.' });
-        });
-    
-
+        // return res.status(200).json({ status: 'success', payload: resp.body });
+      });
+    }
+    return res.status(200).json({status: 'success', payload: 'Poll sent successfully.'});
+  });
 };
 
 
 exports.seed = function (req, res) {
- const rawDocuments = [
-   { platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0 },
-   { platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0 },
-   { platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0 },
-   { platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0 },
-   { platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0 },
-   ];
+  const rawDocuments = [
+    {platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0},
+    {platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0},
+    {platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0},
+    {platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0},
+    {platform: 'facebook', statement: 'Can smoking cause cancer', options: ['Yes', 'No', 'Dont Care'], sent: 0},
+  ];
 
- Polls.insertMany(rawDocuments)
-      .then((mongooseDocuments) => {
-          logger.serverLog(TAG, 'Polls Table Seeded');
-          res.status(200).json({ status: 'Success' });
-      })
-      .catch((err) => {
-          /* Error handling */
-          logger.serverLog(TAG, 'Unable to seed the database');
-          res.status(500).json({ status: 'Failed', err });
-      });
+  Polls.insertMany(rawDocuments)
+    .then((mongooseDocuments) => {
+      logger.serverLog(TAG, 'Polls Table Seeded');
+      res.status(200).json({status: 'Success'});
+    })
+    .catch((err) => {
+      /* Error handling */
+      logger.serverLog(TAG, 'Unable to seed the database');
+      res.status(500).json({status: 'Failed', err});
+    });
 };
