f9af4e788fe59ef6d929925cfb8daee6b5cf4f55
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-08-08 05:59:18 +0500

    Dashboard Client Side Code

---
+++ client/src/redux/reducers/dashboard.reducer.js
@@ -0,0 +1,29 @@
+import * as ActionTypes from '../constants/constants';
+
+const initialState = {
+  dashboard: {
+      pages: 0,
+      subscribers: 0,
+      scheduledBroadcast: 0,
+      recentBroadcasts: [],
+      username: '',
+      activityChart: {
+        polls: 0,
+        messages: 0,
+        surveys: 0,
+  }},
+
+};
+
+export function dashboardInfo(state = initialState, action) {
+  switch (action.type) {
+    case ActionTypes.UPDATE_DASHBOARD:
+      return Object.assign({}, state, {
+        dashboard: action.data
+      });
+
+   
+    default:
+      return state;
+  }
+}
--- client/src/redux/reducers/reducer.js
+++ client/src/redux/reducers/reducer.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import {basicInfo} from './basicinfo.reducer';
 import {pagesInfo} from './pages.reducer';
 import {subscribersInfo} from './subscribers.reducer';
+import {dashboardInfo} from './dashboard.reducer';
 import {broadcastsInfo} from './broadcast.reducer';
 import {pollsInfo} from './poll.reducer';
 import {surveysInfo} from './surveys.reducer.js';
