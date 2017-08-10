1bd9490d77e6ae04b930997bef67bbaabbe2af48
Zarmeen <zarmeennasim@yahoo.com>
2017-08-10 15:34:03 +0500

    webhook

--- server/api/polls/polls.controller.js
+++ server/api/polls/polls.controller.js
@@ -107,7 +107,7 @@ exports.send = function (req, res) {
       for (var z in pages) // todo this for loop doesn't work with async code
       {
         logger.serverLog(TAG, `Page at Z ${JSON.stringify(pages[z])}`);
-        Subscribers.find({ pageId: pages[z]._id}, (err, subscribers) => {
+        Subscribers.find({ pageId: pages[z]._id }, (err, subscribers) => {
           if(subscribers.length > 0)
          {   
                   logger.serverLog(TAG, `Subscribers of page ${JSON.stringify(subscribers)}`);
@@ -118,7 +118,7 @@ exports.send = function (req, res) {
                   //get accesstoken of page
                   // TODO can't we get access token from the page table???
                   //-- Page tokens get expired therefore we need to fetch it from Graph api
-                 /* needle.get(`https://graph.facebook.com/v2.10/${pages[z].pageId}?fields=access_token&access_token=${req.user.fbToken}`, (err, resp) => {
+                  needle.get(`https://graph.facebook.com/v2.10/${pages[z].pageId}?fields=access_token&access_token=${req.user.fbToken}`, (err, resp) => {
                     if (err) {
                       logger.serverLog(TAG, `Page accesstoken from graph api Error${JSON.stringify(err)}`);
                       // TODO this will do problem, res should not be in loop
@@ -126,18 +126,18 @@ exports.send = function (req, res) {
                     }
 
                       logger.serverLog(TAG, `Page accesstoken from graph api ${JSON.stringify(resp.body)}`);
-                  */
+
 
                       for (let j = 0; j < subscribers.length; j++) { // TODO again for loop is not good option
                         logger.serverLog(TAG, `At Subscriber fetched ${JSON.stringify(subscribers[j])}`);
-                        //logger.serverLog(TAG, `At Pages Token ${resp.body.access_token}`);
+                        logger.serverLog(TAG, `At Pages Token ${resp.body.access_token}`);
 
                         const data = {
                           recipient: { id: subscribers[j].senderId }, //this is the subscriber id
                           message: messageData,
                         };
 
-                        needle.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${pages[z].accessToken}`, data, (err, resp) => {
+                        needle.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${resp.body.access_token}`, data, (err, resp) => {
                           logger.serverLog(TAG, `Sending poll to subscriber response ${JSON.stringify(resp.body)}`);
                           if (err) {
                             // TODO this will do problem, res should not be in loop
@@ -149,7 +149,7 @@ exports.send = function (req, res) {
                           // return res.status(200).json({ status: 'success', payload: resp.body });
                         });
                       }
-                //  });
+                  });
           }
         });
       }
