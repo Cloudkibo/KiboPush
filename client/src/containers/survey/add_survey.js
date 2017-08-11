9fb51173f551e47f538c00c9299d0aa1d2f4b1c6
Zarmeen <zarmeennasim@yahoo.com>
2017-08-11 16:22:54 +0500

    survey add options warning

--- client/src/containers/survey/add_survey.js
+++ client/src/containers/survey/add_survey.js
@@ -11,33 +11,33 @@ import HeaderResponsive from '../../components/header/headerResponsive'
 import { connect } from 'react-redux'
 import {createsurvey} from '../../redux/actions/surveys.actions'
 import { bindActionCreators } from 'redux'
-
+import { AlertList, Alert, AlertContainer } from 'react-bs-notifier'
 class AddSurvey extends React.Component {
   constructor (props, context) {
     super(props, context)
-    this.state = {questionType: 'multichoice', surveyQuestions: []}
-		// surveyQuestions will be an array of json object
-		// each json object will have following keys:
-		// statement : //value of question
-		// type: //text or multichoice
-		// choiceCount: //no of options
-		// options: [] array of choice values
+    this.state = {questionType: 'multichoice', surveyQuestions: [], showAlert: false, alertmsg: ''}
+    // surveyQuestions will be an array of json object
+    // each json object will have following keys:
+    // statement : //value of question
+    // type: //text or multichoice
+    // choiceCount: //no of options
+    // options: [] array of choice values
     this.createSurvey = this.createSurvey.bind(this)
   }
 
-	 componentDidMount () {
-   require('../../../public/js/jquery-3.2.0.min.js')
-   require('../../../public/js/jquery.min.js')
-   var addScript = document.createElement('script')
-   addScript.setAttribute('src', '../../../js/theme-plugins.js')
-   document.body.appendChild(addScript)
-   addScript = document.createElement('script')
-   addScript.setAttribute('src', '../../../js/material.min.js')
-   document.body.appendChild(addScript)
-   addScript = document.createElement('script')
-   addScript.setAttribute('src', '../../../js/main.js')
-   document.body.appendChild(addScript)
- }
+  componentDidMount () {
+    require('../../../public/js/jquery-3.2.0.min.js')
+    require('../../../public/js/jquery.min.js')
+    var addScript = document.createElement('script')
+    addScript.setAttribute('src', '../../../js/theme-plugins.js')
+    document.body.appendChild(addScript)
+    addScript = document.createElement('script')
+    addScript.setAttribute('src', '../../../js/material.min.js')
+    document.body.appendChild(addScript)
+    addScript = document.createElement('script')
+    addScript.setAttribute('src', '../../../js/main.js')
+    document.body.appendChild(addScript)
+  }
   componentWillReceiveProps (nextprops) {
     if (nextprops.createwarning) {
       console.log('i am called')
@@ -47,57 +47,77 @@ class AddSurvey extends React.Component {
       })
     }
   }
-	 createSurvey (e) {
-	 	e.preventDefault()
-	 	var surveybody = {
-	 		survey: {
-		 			 title: this.refs.title.value, // title of survey
-				   	 description: this.refs.description.value, // description of survey
-				     image: '' // image url
-				 	},
-   questions: this.state.surveyQuestions
-	 	}
-	 	this.props.createsurvey(surveybody)
-	 }
-	 addClick () {
-	   let surveyQuestions = this.state.surveyQuestions
-	   let choiceCount = 0
-	   let choiceValues = []
-	   if (this.state.questionType == 'multichoice') {
-	   	choiceCount = 3 // by default no. of options will be 3
-	   	choiceValues = ['', '', '']
-	   }
+  createSurvey (e) {
+    e.preventDefault()
+    let flag = 0
+    for (let j = 0; j < this.state.surveyQuestions.length; j++) {
+      if (this.state.surveyQuestions[j].options.length > 0) {
+        for (let k = 0; k < this.state.surveyQuestions[j].options.length; k++) {
+          if (this.state.surveyQuestions[j].options[k] == '') {
+            flag = 1
+            console.log('empty')
+            break
+          }
+        }
+      }
+      if (flag == 1) {
+        break
+      }
+    }
+    if (flag == 0 && this.refs.title.value != '' && this.refs.description.value != '') {
+      var surveybody = {
+        survey: {
+          title: this.refs.title.value, // title of survey
+          description: this.refs.description.value, // description of survey
+          image: '' // image url
+        },
+        questions: this.state.surveyQuestions
+      }
+      this.props.createsurvey(surveybody)
+    } else {
+     // alert('Please fill all the fields.')
+      this.setState({showAlert: true, alertmsg: 'Please fill all the fields.'})
+    }
+  }
+  addClick () {
+    let surveyQuestions = this.state.surveyQuestions
+    let choiceCount = 0
+    let choiceValues = []
+    if (this.state.questionType == 'multichoice') {
+      choiceCount = 3 // by default no. of options will be 3
+      choiceValues = ['', '', '']
+    }
 
-	   surveyQuestions.push({'statement': '', 'type': this.state.questionType, 'choiceCount': choiceCount, 'options': choiceValues})
-	   this.setState({surveyQuestions: surveyQuestions})
-	   console.log('surveyQuestions')
-	   console.log(this.state.surveyQuestions)
-	  }
+    surveyQuestions.push({'statement': '', 'type': this.state.questionType, 'choiceCount': choiceCount, 'options': choiceValues})
+    this.setState({surveyQuestions: surveyQuestions})
+    console.log('surveyQuestions')
+    console.log(this.state.surveyQuestions)
+  }
 
-  	addChoices (qindex) {
-  	   let surveyQuestions = this.state.surveyQuestions.slice()
-  	   let choices = surveyQuestions[qindex].options.slice()
-	   surveyQuestions[qindex].choiceCount = surveyQuestions[qindex].choiceCount + 1
-	   choices.push('')
-	   surveyQuestions[qindex].options = choices
-	   this.setState({surveyQuestions})
-  	}
+  addChoices (qindex) {
+    let surveyQuestions = this.state.surveyQuestions.slice()
+    let choices = surveyQuestions[qindex].options.slice()
+    surveyQuestions[qindex].choiceCount = surveyQuestions[qindex].choiceCount + 1
+    choices.push('')
+    surveyQuestions[qindex].options = choices
+    this.setState({surveyQuestions})
+  }
 
-  	removeChoices (choiceIndex, qindex) {
-  	   console.log('removeChoices called qindex ' + qindex + ' choiceIndex ' + choiceIndex)
-  	   let surveyQuestions = this.state.surveyQuestions.slice()
-  	   let choices = surveyQuestions[qindex].options.slice()
-  	   console.log('choices before')
-  	   console.log(choices)
-  	   choices.splice(choiceIndex, 1)
-  	   console.log('choices after')
-  	   console.log(choices)
-	   surveyQuestions[qindex].choiceCount = surveyQuestions[qindex].choiceCount - 1
-	   surveyQuestions[qindex].options = choices
-	   this.setState({surveyQuestions: surveyQuestions})
-  	}
-  	removeClick (i) {
-	 let surveyQuestions = this.state.surveyQuestions.slice()
+  removeChoices (choiceIndex, qindex) {
+    console.log('removeChoices called qindex ' + qindex + ' choiceIndex ' + choiceIndex)
+    let surveyQuestions = this.state.surveyQuestions.slice()
+    let choices = surveyQuestions[qindex].options.slice()
+    console.log('choices before')
+    console.log(choices)
+    choices.splice(choiceIndex, 1)
+    console.log('choices after')
+    console.log(choices)
+    surveyQuestions[qindex].choiceCount = surveyQuestions[qindex].choiceCount - 1
+    surveyQuestions[qindex].options = choices
+    this.setState({surveyQuestions: surveyQuestions})
+  }
+  removeClick (i) {
+    let surveyQuestions = this.state.surveyQuestions.slice()
     surveyQuestions.splice(i, 1)
     console.log(surveyQuestions)
     this.setState({
@@ -109,114 +129,114 @@ class AddSurvey extends React.Component {
     surveyQuestions[i].statement = event.target.value
     this.setState({surveyQuestions})
     console.log('surveyQuestions')
-	 console.log(this.state.surveyQuestions)
+    console.log(this.state.surveyQuestions)
   }
 
   onhandleChoiceChange (qindex, choiceIndex, event) {
-   	 console.log('onhandleChoiceChange is called')
+    console.log('onhandleChoiceChange is called')
     let surveyQuestions = this.state.surveyQuestions.slice()
     console.log('qindex is ' + qindex)
     console.log('choiceIndex is ' + choiceIndex)
     surveyQuestions[qindex].options[choiceIndex] = event.target.value
     this.setState({surveyQuestions})
     console.log('surveyQuestions')
-	 console.log(this.state.surveyQuestions)
+    console.log(this.state.surveyQuestions)
   }
   /* handleQuestionType (e) {
-	      this.setState({
-	    	'questionType': e.target.value
+        this.setState({
+        'questionType': e.target.value
 
-	    })
-	  } */
+      })
+    } */
 
   createOptionsList (qindex) {
-  		console.log('qindex' + qindex)
-  		let choiceItems = []
-  		var choiceCount = this.state.surveyQuestions[qindex].choiceCount
-  		console.log('choiceCount is ' + choiceCount)
-  		for (var j = 0; j < choiceCount; j++) {
-  			choiceItems.push(
-    <div className='input-group'>
-      <input type='text' placeholder={'Choice' + (j + 1)} className='form-control input-sm' onChange={this.onhandleChoiceChange.bind(this, qindex, j)} />
-      <span className='input-group-btn'>
-        <button className='btn btn-secondary' type='button' onClick={this.removeChoices.bind(this, j, qindex)}>
-          <span className='fa fa-times' />
-        </button>
-      </span>
-    </div>
+    console.log('qindex' + qindex)
+    let choiceItems = []
+    var choiceCount = this.state.surveyQuestions[qindex].choiceCount
+    console.log('choiceCount is ' + choiceCount)
+    for (var j = 0; j < choiceCount; j++) {
+      choiceItems.push(
+        <div className='input-group'>
+          <input type='text' placeholder={'Choice' + (j + 1)} className='form-control input-sm' onChange={this.onhandleChoiceChange.bind(this, qindex, j)} />
+          <span className='input-group-btn'>
+            <button className='btn btn-secondary' type='button' onClick={this.removeChoices.bind(this, j, qindex)}>
+              <span className='fa fa-times' />
+            </button>
+          </span>
+        </div>
 
-  				)
-  		}
-  	   return choiceItems || null
+          )
+    }
+    return choiceItems || null
   }
   createUI () {
     let uiItems = []
     for (let i = 0; i < this.state.surveyQuestions.length; i++) {
-     	if (this.state.surveyQuestions[i].type == 'text') {
-			   uiItems.push(
-  <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
-    <br />
-    <div className='panel panel-default field-editor'>
-      <div className='panel-heading clearfix'>
-        <strong className='panel-title'>Edit Question {i} </strong>
-        <div role='toolbar' className='pull-right btn-toolbar'>
-          <a className='remove' onClick={this.removeClick.bind(this, i)}>
-            <span className='fa fa-times' />
-          </a>
-        </div>
-      </div>
-      <div className='panel-body'>
-        <div className='form-group'>
-          <input className='form-control' placeholder='Enter question here...' onChange={this.handleChange.bind(this, i)} />
-        </div>
-      </div>
-    </div>
-
-  </div>
-
-            )
-     }			else {
-			   uiItems.push(
-  <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
-    <br />
-    <div className='panel panel-default field-editor'>
-      <div className='panel-heading clearfix'>
-        <strong className='panel-title'>Edit Question {i}</strong>
-        <div role='toolbar' className='pull-right btn-toolbar'>
-          <a className='remove' onClick={this.removeClick.bind(this, i)}>
-            <span className='fa fa-times' />
-          </a>
-        </div>
-      </div>
-      <div className='panel-body'>
-        <div className='form-group'>
-          <input className='form-control' placeholder='Enter question here...' onChange={this.handleChange.bind(this, i)} />
-        </div>
-
-        <div className='form-group field field-array'>
-          <fieldset className='col-md-6 scheduler-border'>
-            <legend className='scheduler-border'>
-										Choices
-									</legend>
-            <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
-              <div className='col-xs-10'>
-                <div className='form-group field field-string'>
-                  {this.createOptionsList(i)}
+      if (this.state.surveyQuestions[i].type == 'text') {
+        uiItems.push(
+          <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+            <br />
+            <div className='panel panel-default field-editor'>
+              <div className='panel-heading clearfix'>
+                <strong className='panel-title'>Edit Question {i} </strong>
+                <div role='toolbar' className='pull-right btn-toolbar'>
+                  <a className='remove' onClick={this.removeClick.bind(this, i)}>
+                    <span className='fa fa-times' />
+                  </a>
                 </div>
               </div>
-              <div className='col-sm-6 col-md-4'>
-                <button className='btn btn-secondary btn-sm' onClick={this.addChoices.bind(this, i)}> Add Choices</button>
+              <div className='panel-body'>
+                <div className='form-group'>
+                  <input className='form-control' placeholder='Enter question here...' onChange={this.handleChange.bind(this, i)} />
+                </div>
               </div>
             </div>
-          </fieldset>
 
-        </div>
-      </div>
-    </div>
-  </div>
+          </div>
 
             )
-     }
+      } else {
+        uiItems.push(
+          <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+            <br />
+            <div className='panel panel-default field-editor'>
+              <div className='panel-heading clearfix'>
+                <strong className='panel-title'>Edit Question {i}</strong>
+                <div role='toolbar' className='pull-right btn-toolbar'>
+                  <a className='remove' onClick={this.removeClick.bind(this, i)}>
+                    <span className='fa fa-times' />
+                  </a>
+                </div>
+              </div>
+              <div className='panel-body'>
+                <div className='form-group'>
+                  <input className='form-control' placeholder='Enter question here...' onChange={this.handleChange.bind(this, i)} />
+                </div>
+
+                <div className='form-group field field-array'>
+                  <fieldset className='col-md-6 scheduler-border'>
+                    <legend className='scheduler-border'>
+                    Choices
+                  </legend>
+                    <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                      <div className='col-xs-10'>
+                        <div className='form-group field field-string'>
+                          {this.createOptionsList(i)}
+                        </div>
+                      </div>
+                      <div className='col-sm-6 col-md-4'>
+                        <button className='btn btn-secondary btn-sm' onClick={this.addChoices.bind(this, i)}> Add Choices</button>
+                      </div>
+                    </div>
+                  </fieldset>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+            )
+      }
     }
     return uiItems || null
   }
@@ -233,6 +253,7 @@ class AddSurvey extends React.Component {
           <br />
           <br />
           <br />
+
           <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
             <h2 className='presentation-margin'>Create Survey Form</h2>
             <div className='ui-block'>
@@ -283,7 +304,14 @@ class AddSurvey extends React.Component {
                       <button className='btn btn-secondary' onClick={this.createSurvey}> Create Survey</button>
                       <button className='btn btn-border-think btn-transparent c-grey'>Cancel</button>
                     </div>
+                    {this.state.showAlert == true &&
+                    <center>
+                      <Alert type='danger' >
+                        {this.state.alertmsg}
+                      </Alert>
+                    </center>
 
+                    }
                   </div>
 
                 </div>
