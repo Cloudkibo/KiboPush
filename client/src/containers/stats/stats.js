541b8c53cec0200d15a07c5a641a03f4272a6100
Sojharo <sojharo@live.com>
2017-08-11 16:04:16 +0500

    containers vs components

--- client/src/containers/stats/stats.js
+++ client/src/containers/stats/stats.js
@@ -136,13 +136,13 @@ class Stats extends React.Component {
                       <a href='#' className='more'>
                         <svg className='olymp-three-dots-icon'>
                           <use
-                            xlinkHref='icons/icons.svg#olymp-three-dots-icon'/>
+                            xlinkHref='icons/icons.svg#olymp-three-dots-icon' />
                         </svg>
                       </a>
                     </div>
                     <div className='ui-block-content'>
                       <div className='chart-js chart-js-one-bar'>
-                        <canvas id='one-bar-chart' width={1400} height={380}/>
+                        <canvas id='one-bar-chart' width={1400} height={380} />
                       </div>
                     </div>
                   </div>
@@ -161,25 +161,25 @@ class Stats extends React.Component {
                       </select>
                       <div className='points align-right'>
                         <span>
-                          <span className='statistics-point bg-primary'/>
+                          <span className='statistics-point bg-primary' />
                       Broadcasts
                     </span>
                         <span>
-                          <span className='statistics-point bg-green'/>
+                          <span className='statistics-point bg-green' />
                       Subscribers
                     </span>
                       </div>
                       <a href='#' className='more'>
                         <svg className='olymp-three-dots-icon'>
                           <use
-                            xlinkHref='icons/icons.svg#olymp-three-dots-icon'/>
+                            xlinkHref='icons/icons.svg#olymp-three-dots-icon' />
                         </svg>
                       </a>
                     </div>
                     <div className='ui-block-content'>
                       <div className='chart-js chart-js-line-graphic'>
                         <canvas id='line-graphic-chart' width={730}
-                                height={300}/>
+                          height={300} />
                       </div>
                     </div>
                   </div>
@@ -191,7 +191,7 @@ class Stats extends React.Component {
                       <a href='#' className='more'>
                         <svg className='olymp-three-dots-icon'>
                           <use
-                            xlinkHref='icons/icons.svg#olymp-three-dots-icon'/>
+                            xlinkHref='icons/icons.svg#olymp-three-dots-icon' />
                         </svg>
                       </a>
                     </div>
@@ -201,7 +201,7 @@ class Stats extends React.Component {
                           <li>
                             <div className='points'>
                               <span>
-                                <span className='statistics-point bg-purple'/>
+                                <span className='statistics-point bg-purple' />
                             Broadcasts Sent
                           </span>
                             </div>
@@ -210,7 +210,7 @@ class Stats extends React.Component {
                           <li>
                             <div className='points'>
                               <span>
-                                <span className='statistics-point bg-breez'/>
+                                <span className='statistics-point bg-breez' />
                             Subscribers
                           </span>
                             </div>
@@ -219,7 +219,7 @@ class Stats extends React.Component {
                           <li>
                             <div className='points'>
                               <span>
-                                <span className='statistics-point bg-primary'/>
+                                <span className='statistics-point bg-primary' />
                             Polls
                           </span>
                             </div>
@@ -228,7 +228,7 @@ class Stats extends React.Component {
                           <li>
                             <div className='points'>
                               <span>
-                                <span className='statistics-point bg-yellow'/>
+                                <span className='statistics-point bg-yellow' />
                             Surveys
                           </span>
                             </div>
@@ -237,7 +237,7 @@ class Stats extends React.Component {
                         </ul>
                         <div className='chart-js chart-js-pie-color'>
                           <canvas id='pie-color-chart' width={180}
-                                  height={180}/>
+                            height={180} />
                           <div className='general-statistics'>17
                             <span>Last Month Posts</span>
                           </div>
@@ -258,14 +258,14 @@ class Stats extends React.Component {
                       <a href='#' className='more'>
                         <svg className='olymp-three-dots-icon'>
                           <use
-                            xlinkHref='icons/icons.svg#olymp-three-dots-icon'/>
+                            xlinkHref='icons/icons.svg#olymp-three-dots-icon' />
                         </svg>
                       </a>
                     </div>
                     <div className='ui-block-content'>
                       <div className='circle-progress circle-pie-chart'>
                         <div className='pie-chart' data-value='0.68'
-                             data-startcolor='#38a9ff' data-endcolor='#317cb6'>
+                          data-startcolor='#38a9ff' data-endcolor='#317cb6'>
                           <div className='content'><span>%</span></div>
                         </div>
                       </div>
@@ -286,48 +286,48 @@ class Stats extends React.Component {
                       <a href='#' className='more'>
                         <svg className='olymp-three-dots-icon'>
                           <use
-                            xlinkHref='icons/icons.svg#olymp-three-dots-icon'/>
+                            xlinkHref='icons/icons.svg#olymp-three-dots-icon' />
                         </svg>
                       </a>
                     </div>
                     <div className='ui-block-content'>
                       <div className='world-statistics'>
                         <div className='world-statistics-img'>
-                          <img src='img/world-map.png' alt='map'/>
+                          <img src='img/world-map.png' alt='map' />
                         </div>
                         <ul className='country-statistics'>
                           <li>
-                            <img src='img/flag1.jpg' alt='flag'/>
+                            <img src='img/flag1.jpg' alt='flag' />
                             <span className='country'>United States</span>
                             <span className='count-stat'>86.134</span>
                           </li>
                           <li>
-                            <img src='img/flag2.jpg' alt='flag'/>
+                            <img src='img/flag2.jpg' alt='flag' />
                             <span className='country'>Mexico</span>
                             <span className='count-stat'>35.136</span>
                           </li>
                           <li>
-                            <img src='img/flag3.jpg' alt='flag'/>
+                            <img src='img/flag3.jpg' alt='flag' />
                             <span className='country'>France</span>
                             <span className='count-stat'>12.600</span>
                           </li>
                           <li>
-                            <img src='img/flag4.jpg' alt='flag'/>
+                            <img src='img/flag4.jpg' alt='flag' />
                             <span className='country'>Spain</span>
                             <span className='count-stat'>9.471</span>
                           </li>
                           <li>
-                            <img src='img/flag5.jpg' alt='flag'/>
+                            <img src='img/flag5.jpg' alt='flag' />
                             <span className='country'>Ireland</span>
                             <span className='count-stat'>8.058</span>
                           </li>
                           <li>
-                            <img src='img/flag6.jpg' alt='flag'/>
+                            <img src='img/flag6.jpg' alt='flag' />
                             <span className='country'>Argentina</span>
                             <span className='count-stat'>5.653</span>
                           </li>
                           <li>
-                            <img src='img/flag7.jpg' alt='flag'/>
+                            <img src='img/flag7.jpg' alt='flag' />
                             <span className='country'>Ecuador</span>
                             <span className='count-stat'>2.924</span>
                           </li>
@@ -352,28 +352,28 @@ class Stats extends React.Component {
                       <a href='#' className='more'>
                         <svg className='olymp-three-dots-icon'>
                           <use
-                            xlinkHref='icons/icons.svg#olymp-three-dots-icon'/>
+                            xlinkHref='icons/icons.svg#olymp-three-dots-icon' />
                         </svg>
                       </a>
                     </div>
                     <div className='ui-block-content'>
                       <div className='chart-js chart-js-line-chart'>
-                        <canvas id='line-chart' width={1400} height={380}/>
+                        <canvas id='line-chart' width={1400} height={380} />
                       </div>
                     </div>
                     <hr />
                     <div
                       className='ui-block-content display-flex content-around'>
                       <div className='chart-js chart-js-small-pie'>
-                        <canvas id='pie-small-chart' width={90} height={90}/>
+                        <canvas id='pie-small-chart' width={90} height={90} />
                       </div>
                       <div className='points points-block'>
                         <span>
-                          <span className='statistics-point bg-breez'/>
+                          <span className='statistics-point bg-breez' />
                       Yearly Likes
                     </span>
                         <span>
-                          <span className='statistics-point bg-yellow'/>
+                          <span className='statistics-point bg-yellow' />
                       Yearly Comments
                     </span>
                       </div>
@@ -414,7 +414,7 @@ class Stats extends React.Component {
 function mapStateToProps (state) {
   console.log(state)
   return {
-    subscribers: (state.subscribersInfo.subscribers),
+    subscribers: (state.subscribersInfo.subscribers)
   }
 }
 
