f164a8c60b82df5468c8f0fa8bc78c090fe93bc9
sumairasaeed <sumaira.syedsaeed@gmail.com>
2017-08-25 19:26:19 +0500

    fix for no subscriber alert

--- server/api/broadcasts/broadcasts.model.js
+++ server/api/broadcasts/broadcasts.model.js
@@ -1,17 +1,16 @@
 // WE are referring Messages as Broadcasts, broadcasts and messages will be same thing
 // Zarmeen
 
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId
 
 var broadcastSchema = new Schema({
   platform: String, // TODO define this as enum with values, for now value is facebook
   type: String, // TODO define this as enum with values ['text','attachment']
   text: String, // message body
   userId: {type: Schema.ObjectId, ref: 'users'},
-  datetime: {type: Date, default: Date.now},
-  fileurl: String,
-  attachmentType: String
+  datetime: {type: Date, default: Date.now }
   //  pageId: String, [discuss with sojharo, will we keep it or not]
 })
 
