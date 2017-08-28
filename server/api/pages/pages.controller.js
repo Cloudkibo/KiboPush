00de9728d30ddf506cb55604c3f339bd59e347f3
Sojharo <sojharo@live.com>
2017-08-28 10:19:40 +0500

    page pic commit 246 small bug

--- server/api/pages/pages.controller.js
+++ server/api/pages/pages.controller.js
@@ -180,7 +180,7 @@ function fetchPages (url, user) {
             if (error2 !== null) {
               return logger.serverLog(TAG, `Error occurred ${error2}`)
             }
-            logger.serverLog(TAG, `Data by fb for page Pic ${JSON.stringify(pagePicUrl.body)}`)
+            logger.serverLog(TAG, `Data by fb for page Pic ${JSON.stringify(pagePicUrl.body.data)}`)
             Pages.findOne({pageId: item.id, userId: user._id}, (err, page) => {
               if (err) {
                 logger.serverLog(TAG, `Internal Server Error ${JSON.stringify(err)}`)
