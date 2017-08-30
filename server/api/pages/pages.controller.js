f8b614f1d60bf5508e128da903f2a35b1972082b
Sojharo <sojharo@live.com>
2017-08-30 11:01:46 +0500

    profile picture #303

--- server/api/pages/pages.controller.js
+++ server/api/pages/pages.controller.js
@@ -8,6 +8,7 @@ const TAG = 'api/pages/pages.controller.js'
 const Users = require('../user/Users.model')
 const needle = require('needle')
 const Subscribers = require('../subscribers/Subscribers.model')
+const fetchPages = require('../../auth/facebook/passport').fetchPages
 
 exports.index = function (req, res) {
   logger.serverLog(TAG, 'Get pages API called')
@@ -137,84 +138,5 @@ exports.addPages = function (req, res) {
       })
     }
     //  return res.status(200).json({ status: 'success', payload: user});
-  })
-}
-
-// TODO use this after testing
-function fetchPages (url, user) {
-  const options = {
-    headers: {
-      'X-Custom-Header': 'CloudKibo Web Application'
-    },
-    json: true
-
-  }
-  needle.get(url, options, (err, resp) => {
-    if (err !== null) {
-      logger.serverLog(TAG, 'error from graph api to get pages list data: ')
-      logger.serverLog(TAG, JSON.stringify(err))
-      return
-    }
-    logger.serverLog(TAG, 'resp from graph api to get pages list data: ')
-    logger.serverLog(TAG, JSON.stringify(resp.body))
-
-    const data = resp.body.data
-    const cursor = resp.body.paging
-
-    data.forEach((item) => {
-      const options2 = {
-        url: `https://graph.facebook.com/v2.10/${item.id}/?fields=fan_count&access_token=${item.access_token}`,
-        qs: {access_token: item.access_token},
-        method: 'GET'
-      }
-      needle.get(options2.url, options2, (error, fanCount) => {
-        if (error !== null) {
-          return logger.serverLog(TAG, `Error occurred ${error}`)
-        } else {
-          logger.serverLog(TAG, `Data by fb for page likes ${JSON.stringify(
-            fanCount.body.fan_count)}`)
-          Pages.findOne({pageId: item.id, userId: user._id}, (err, page) => {
-            if (err) {
-              logger.serverLog(TAG,
-                `Internal Server Error ${JSON.stringify(err)}`)
-            }
-            if (!page) {
-              logger.serverLog(TAG,
-                `Page ${item.name} not found. Creating a page`)
-              var pageItem = new Pages({
-                pageId: item.id,
-                pageName: item.name,
-                accessToken: item.access_token,
-                userId: user._id,
-                likes: fanCount.body.fan_count,
-                pagePic: `https://graph.facebook.com/v2.10/${item.id}/picture`,
-                connected: false
-              })
-              // save model to MongoDB
-              pageItem.save((err, page) => {
-                if (err) {
-                  logger.serverLog(TAG, `Error occurred ${err}`)
-                }
-                logger.serverLog(TAG,
-                  `Page ${item.name} created with id ${page.pageId}`)
-              })
-            } else {
-              page.likes = fanCount.body.fan_count
-              page.pagePic = `https://graph.facebook.com/v2.10/${item.id}/picture`
-              page.save((err) => {
-                if (err) {
-                  logger.serverLog(TAG,
-                    `Internal Server Error ${JSON.stringify(err)}`)
-                }
-                logger.serverLog(TAG, `Likes updated for ${page.pageName}`)
-              })
-            }
-          })
-        }
-      })
-    })
-    if (cursor.next) {
-      fetchPages(cursor.next, user)
-    }
   })
 }
--- server/auth/facebook/passport.js
+++ server/auth/facebook/passport.js
@@ -19,7 +19,7 @@ const options = {
   json: true
 }
 
-exports.setup = function (User, config) {
+let setup = function (User, config) {
   passport.use(new FacebookStrategy(
     {
       clientID: config.facebook.clientID,
@@ -77,14 +77,20 @@ exports.setup = function (User, config) {
               }
               logger.serverLog(TAG,
                 `user is updated : ${JSON.stringify(userpaylaod)}`)
+              fetchPages(`https://graph.facebook.com/v2.10/${
+                user.fbId}/accounts?access_token=${
+                user.fbToken}`, user)
               done(null, user)
             })
           } else {
-            payload.save((error) => {
+            payload.save((error, newUser) => {
               if (error) {
                 return done(error)
               }
-              return done(null, payload)
+              fetchPages(`https://graph.facebook.com/v2.10/${
+                payload.fbId}/accounts?access_token=${
+                payload.fbToken}`, newUser)
+              return done(null, newUser)
             })
           }
         })
@@ -93,36 +99,75 @@ exports.setup = function (User, config) {
   ))
 }
 
-// TODO use this after testing
-// eslint-disable-next-line no-unused-vars
-function fetchPages (url, user) {
+let fetchPages = function (url, user) {
+  logger.serverLog(TAG, 'FETCH PAGES FUNCTION CALLED')
+  const options = {
+    headers: {
+      'X-Custom-Header': 'CloudKibo Web Application'
+    },
+    json: true
+
+  }
   needle.get(url, options, (err, resp) => {
-    logger.serverLog(TAG, 'error from graph api to get pages list data: ')
-    logger.serverLog(TAG, JSON.stringify(err))
+    if (err !== null) {
+      logger.serverLog(TAG, 'error from graph api to get pages list data: ')
+      logger.serverLog(TAG, JSON.stringify(err))
+      return
+    }
     logger.serverLog(TAG, 'resp from graph api to get pages list data: ')
     logger.serverLog(TAG, JSON.stringify(resp.body))
-    logger.serverLog(TAG, 'user data for fetch pages: ')
-    logger.serverLog(TAG, JSON.stringify(user))
 
     const data = resp.body.data
     const cursor = resp.body.paging
 
     data.forEach((item) => {
-      Pages.findOne({pageId: item.id, userId: user._id}, (err, page) => {
-        if (err) return logger.serverLog(TAG, 'Error ' + JSON.stringify(err))
-        if (!page) {
-          logger.serverLog(TAG, 'Page not found. Creating a page ')
-          var newPage = new Pages({
-            pageId: item.id,
-            pageName: item.name,
-            accessToken: item.access_token,
-            userId: user._id,
-            connected: false
-          })
-          // save model to MongoDB
-          newPage.save((err2, page) => {
-            if (err2) return logger.serverLog(TAG, 'Error ' + JSON.stringify(err2))
-            logger.serverLog(TAG, `Page ${item.name} created: ${page}`)
+      const options2 = {
+        url: `https://graph.facebook.com/v2.10/${item.id}/?fields=fan_count&access_token=${item.access_token}`,
+        qs: {access_token: item.access_token},
+        method: 'GET'
+      }
+      needle.get(options2.url, options2, (error, fanCount) => {
+        if (error !== null) {
+          return logger.serverLog(TAG, `Error occurred ${error}`)
+        } else {
+          logger.serverLog(TAG, `Data by fb for page likes ${JSON.stringify(
+            fanCount.body.fan_count)}`)
+          Pages.findOne({pageId: item.id, userId: user._id}, (err, page) => {
+            if (err) {
+              logger.serverLog(TAG,
+                `Internal Server Error ${JSON.stringify(err)}`)
+            }
+            if (!page) {
+              logger.serverLog(TAG,
+                `Page ${item.name} not found. Creating a page`)
+              var pageItem = new Pages({
+                pageId: item.id,
+                pageName: item.name,
+                accessToken: item.access_token,
+                userId: user._id,
+                likes: fanCount.body.fan_count,
+                pagePic: `https://graph.facebook.com/v2.10/${item.id}/picture`,
+                connected: false
+              })
+              // save model to MongoDB
+              pageItem.save((err, page) => {
+                if (err) {
+                  logger.serverLog(TAG, `Error occurred ${err}`)
+                }
+                logger.serverLog(TAG,
+                  `Page ${item.name} created with id ${page.pageId}`)
+              })
+            } else {
+              page.likes = fanCount.body.fan_count
+              page.pagePic = `https://graph.facebook.com/v2.10/${item.id}/picture`
+              page.save((err) => {
+                if (err) {
+                  logger.serverLog(TAG,
+                    `Internal Server Error ${JSON.stringify(err)}`)
+                }
+                logger.serverLog(TAG, `Likes updated for ${page.pageName}`)
+              })
+            }
           })
         }
       })
@@ -132,3 +177,5 @@ function fetchPages (url, user) {
     }
   })
 }
+
+module.exports = { fetchPages, setup }
