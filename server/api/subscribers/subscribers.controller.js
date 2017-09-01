e458ff5f9e18bb23c095c719a8d16bec765329d0
Sojharo <sojharo@live.com>
2017-09-01 17:39:57 +0500

    token work #303

--- server/api/subscribers/subscribers.controller.js
+++ server/api/subscribers/subscribers.controller.js
@@ -7,10 +7,14 @@ const Subscribers = require('./Subscribers.model')
 const TAG = 'api/subscribers/subscribers.controller.js'
 
 exports.index = function (req, res) {
-  Subscribers.find({ userId: req.user._id }, (err, subscriber) => {
-    logger.serverLog(TAG, subscriber)
-    logger.serverLog(TAG, `Error: ${err}`)
-    res.status(200).json(subscriber)
+  Subscribers.find({ userId: req.user._id }, (err, subscribers) => {
+    if (err) {
+      logger.serverLog(TAG, `Error on fetching subscribers: ${err}`)
+      return res.status(404)
+      .json({status: 'failed', description: 'Subscribers not found'})
+    }
+    logger.serverLog(TAG, `Total subscribers ${subscribers.length}`)
+    res.status(200).json(subscribers)
   })
 }
 
