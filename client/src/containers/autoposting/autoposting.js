9c9a70513b0b12933d8c9ce7fe9a067419c44f5a
ImranShoukat <imran.shoukat1995@gmail.com>
2017-09-07 23:36:53 +0500

    Autoposting UI

---
+++ client/src/containers/autoposting/autoposting.js
@@ -0,0 +1,91 @@
+/**
+ * Created by sojharo on 20/07/2017.
+ */
+
+import React from 'react'
+import Sidebar from '../../components/sidebar/sidebar'
+import Responsive from '../../components/sidebar/responsive'
+import Header from '../../components/header/header'
+import HeaderResponsive from '../../components/header/headerResponsive'
+import { Link } from 'react-router'
+import { ModalContainer, ModalDialog } from 'react-modal-dialog'
+import AddChannel from './addChannel'
+
+class Autoposting extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isShowingModal: false
+    }
+    this.showDialog = this.showDialog.bind(this)
+    this.closeDialog = this.closeDialog.bind(this)
+  }
+
+  componentDidMount () {
+    require('../../../public/js/jquery-3.2.0.min.js')
+    require('../../../public/js/jquery.min.js')
+    var addScript = document.createElement('script')
+    addScript.setAttribute('src', '../../../js/theme-plugins.js')
+    document.body.appendChild(addScript)
+    addScript = document.createElement('script')
+    addScript.setAttribute('src', '../../../js/material.min.js')
+    document.body.appendChild(addScript)
+    addScript = document.createElement('script')
+    addScript.setAttribute('src', '../../../js/main.js')
+    document.body.appendChild(addScript)
+  }
+
+  showDialog () {
+    this.setState({isShowingModal: true})
+  }
+
+  closeDialog () {
+    this.setState({isShowingModal: false})
+  }
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <HeaderResponsive />
+        <Sidebar />
+        <Responsive />
+        <div className='container'>
+          <br /><br /><br /><br /><br /><br />
+          <div className='row'>
+            <main
+              className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
+              <div className='ui-block'>
+                <div className='birthday-item inline-items badges'>
+                  <h3>Autoposting</h3>
+                  <Link onClick={this.showDialog} className='btn btn-primary btn-sm'
+                    style={{float: 'right'}}>Add Channel</Link>
+
+                    {
+                      this.state.isShowingModal &&
+                      <ModalContainer style={{width: '500px'}}
+                        onClose={this.closeDialog}>
+                        <ModalDialog style={{width: '500px'}}
+                          onClose={this.closeDialog}>
+                          <AddChannel />
+                        </ModalDialog>
+                      </ModalContainer>
+                    }
+
+                  <div className='table-responsive'>
+                    <p>Currently, you do not have any channels. Click on Add Channel button to add new channels. </p>
+                  </div>
+                </div>
+              </div>
+
+            </main>
+
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default Autoposting
