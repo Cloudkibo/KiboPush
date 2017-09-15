3e92efcce9e95a0cd3de03975a74119d4ac9ee01
Sojharo <sojharo@live.com>
2017-09-15 12:15:45 +0500

    youtube webhook #420

--- server/config/pubsubhubbub.js
+++ server/config/pubsubhubbub.js
@@ -14,35 +14,35 @@ const hub = 'http://pubsubhubbub.appspot.com/'
 let pubsub
 
 module.exports = function () {
-  logger.serverLog(TAG, config.pubsubhubbub)
+  logger.serverLog(TAG, JSON.stringify(config.pubsubhubbub))
   pubsub = pubSubHubbub.createServer(config.pubsubhubbub)
 
   pubsub.listen(config.pubsub_port)
 
   pubsub.on('denied', data => {
     logger.serverLog(TAG, 'Denied')
-    logger.serverLog(TAG, data)
+    logger.serverLog(TAG, JSON.stringify(data))
   })
 
   pubsub.on('subscribe', data => {
     logger.serverLog(TAG, 'Subscribe')
-    logger.serverLog(TAG, data)
+    logger.serverLog(TAG, JSON.stringify(data))
     logger.serverLog(TAG, 'Subscribed ' + topic + ' to ' + hub)
   })
 
   pubsub.on('unsubscribe', data => {
     logger.serverLog(TAG, 'Unsubscribed')
-    logger.serverLog(TAG, data)
+    logger.serverLog(TAG, JSON.stringify(data))
     logger.serverLog(TAG, 'Unsubscribed ' + topic + ' to ' + hub)
   })
 
   pubsub.on('error', error => {
     logger.serverLog(TAG, 'Error')
-    logger.serverLog(TAG, error)
+    logger.serverLog(TAG, JSON.stringify(error))
   })
 
   pubsub.on('feed', data => {
-    logger.serverLog(TAG, data)
+    logger.serverLog(TAG, JSON.stringify(data))
     logger.serverLog(TAG, data.feed.toString())
     // pubsub.unsubscribe(topic, hub)
   })
@@ -52,7 +52,7 @@ module.exports = function () {
     pubsub.subscribe(topic, hub, err => {
       if (err) {
         logger.serverLog(TAG, 'Error in subscribing to pubsubhubbub')
-        logger.serverLog(TAG, err)
+        logger.serverLog(TAG, JSON.stringify(err))
       }
     })
   })
