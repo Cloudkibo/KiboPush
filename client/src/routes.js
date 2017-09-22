0d5b397920595ece430cd567a5b1019133e81c6b
Cloudkibo <cloudkibo@Zarmeens-MacBook-Air.local>
2017-09-22 13:44:19 +0500

    #492 #495 #489

--- client/src/routes.js
+++ client/src/routes.js
@@ -28,7 +28,8 @@ import CreatePoll from './containers/polls/CreatePoll'
 import Poll from './containers/polls/poll'
 import PollResult from './containers/polls/PollResult'
 import ViewPoll from './containers/polls/ViewPoll'
-import UserDetails from './containers/operationalDashboard/viewUserDetails'
+import Seemore from './containers/operationalDashboard/seemore'
+import SeeSubscribers from './containers/operationalDashboard/seesubscribers'
 import SubscribeToMessenger from './containers/subscribeToMessenger/subscribeToMessenger'
 import UserGuideBroadcasts from './containers/userGuide/userGuideBroadcasts'
 import UserGuideSurveys from './containers/userGuide/userGuideSurveys'
@@ -93,8 +94,8 @@ const routes = (
     <Route path='/viewsurveydetail' component={ViewSurveyDetail} onEnter={requireAuth} />
     <Route path='/submitsurveyresponse' component={SubmitSurvey} />
     <Route path='/invitesubscribers' component={InviteSubscribers} onEnter={requireAuth} />
-    <Route path='/viewUserDetails' component={UserDetails} onEnter={requireAuth} />
-
+    <Route path='/seemore' component={Seemore} onEnter={requireAuth} />
+    <Route path='/seesubscribers' component={SeeSubscribers} onEnter={requireAuth} />
   </Route>
 
 )
