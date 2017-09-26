2c87c8db2aba930756746614b1b0296029b87b30
Cloudkibo <cloudkibo@Zarmeens-MacBook-Air.local>
2017-09-26 11:42:55 +0500

    fetch all users #491

--- client/src/redux/actions/backdoor.actions.js
+++ client/src/redux/actions/backdoor.actions.js
@@ -5,7 +5,7 @@ export function updateUsersList (data) {
   console.log('Data Fetched From Users', data)
   return {
     type: ActionTypes.LOAD_USERS_LIST,
-    data.payload
+    data: data.payload
   }
 }
 
