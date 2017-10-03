1360f97bf2d81e9afb120322c189229c78df3ef7
anishachhatwani <i130315@nu.edu.pk>
2017-10-03 12:48:24 +0500

    Fix Logic to fetch surveys

--- client/src/containers/operationalDashboard/operationalDashboard.js
+++ client/src/containers/operationalDashboard/operationalDashboard.js
@@ -87,8 +87,10 @@ class OperationalDashboard extends React.Component {
         filtered.push(this.props.users[i])
       }
     }
-    this.displayData(0, filtered)
-    this.setState({ totalLength: filtered.length })
+    if (filtered && filtered.length > 0) {
+      this.displayData(0, filtered)
+      this.setState({ totalLength: filtered.length })
+    }
   }
   render () {
     return (
