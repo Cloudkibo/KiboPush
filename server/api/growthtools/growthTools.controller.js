cbd2581a1fa8a39004d4e509bac3913d9eb4d392
anishachhatwani <i130315@nu.edu.pk>
2017-10-09 16:52:44 +0500

    parse csv file #635

--- server/api/growthtools/growthTools.controller.js
+++ server/api/growthtools/growthTools.controller.js
@@ -6,9 +6,9 @@
 
 const logger = require('../../components/logger')
 const TAG = 'api/growthTools/growthTools.controller.js'
-// const crypto = require('crypto')
-// const path = require('path')
-// const fs = require('fs')
+const crypto = require('crypto')
+const path = require('path')
+const fs = require('fs')
 
 exports.index = function (req, res) {
   logger.serverLog(TAG,
@@ -27,37 +27,37 @@ exports.upload = function (req, res) {
   logger.serverLog(TAG,
     `upload file route called. file is: ${JSON.stringify(req.files)}`)
 
-  // var today = new Date()
-  // var uid = crypto.randomBytes(5).toString('hex')
-  // var serverPath = 'f' + uid + '' + today.getFullYear() + '' +
-  //   (today.getMonth() + 1) + '' + today.getDate()
-  // serverPath += '' + today.getHours() + '' + today.getMinutes() + '' +
-  //   today.getSeconds()
-  // let fext = req.files.file.name.split('.')
-  // serverPath += '.' + fext[fext.length - 1]
-  //
-  // let dir = path.resolve(__dirname, '../../../broadcastFiles/')
-  //
-  // if (req.files.file.size === 0) {
-  //   return res.status(400).json({
-  //     status: 'failed',
-  //     description: 'No file submitted'
-  //   })
-  // }
-  //
-  // fs.rename(
-  //   req.files.file.path,
-  //   dir + '/userfiles/' + serverPath,
-  //   err => {
-  //     if (err) {
-  //       return res.status(500).json({
-  //         status: 'failed',
-  //         description: 'internal server error' + JSON.stringify(err)
-  //       })
-  //     }
-  //     logger.serverLog(TAG,
-  //       `file uploaded, sending response now: ${JSON.stringify(serverPath)}`)
-  //     return res.status(201).json({status: 'success', payload: serverPath})
-  //   }
-  // )
+  var today = new Date()
+  var uid = crypto.randomBytes(5).toString('hex')
+  var serverPath = 'f' + uid + '' + today.getFullYear() + '' +
+    (today.getMonth() + 1) + '' + today.getDate()
+  serverPath += '' + today.getHours() + '' + today.getMinutes() + '' +
+    today.getSeconds()
+  let fext = req.files.file.name.split('.')
+  serverPath += '.' + fext[fext.length - 1]
+
+  let dir = path.resolve(__dirname, '../../../broadcastFiles/')
+
+  if (req.files.file.size === 0) {
+    return res.status(400).json({
+      status: 'failed',
+      description: 'No file submitted'
+    })
+  }
+
+  fs.rename(
+    req.files.file.path,
+    dir + '/userfiles/' + serverPath,
+    err => {
+      if (err) {
+        return res.status(500).json({
+          status: 'failed',
+          description: 'internal server error' + JSON.stringify(err)
+        })
+      }
+      logger.serverLog(TAG,
+        `file uploaded, sending response now: ${JSON.stringify(serverPath)}`)
+      return res.status(201).json({status: 'success', payload: serverPath})
+    }
+  )
 }
