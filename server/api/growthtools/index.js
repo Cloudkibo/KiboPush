bb6ee62a307afd981ece042dbc32bcd7407c8a27
anishachhatwani <i130315@nu.edu.pk>
2017-10-09 16:00:30 +0500

    parse csv file #635

---
+++ server/api/growthtools/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express')
+
+const router = express.Router()
+
+const controller = require('./growthTools.controller')
+const auth = require('../../auth/auth.service')
+
+const multiparty = require('connect-multiparty')
+const multipartyMiddleware = multiparty()
+
+router.get('/', auth.isAuthenticated(), controller.index)
+router.post('/upload', auth.isAuthenticated(), multipartyMiddleware, controller.upload)
+
+module.exports = router
