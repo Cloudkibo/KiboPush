6d75ba0cd7d981d8d62b3e96e79863b672130c75
anishachhatwani <i130315@nu.edu.pk>
2017-10-10 13:12:36 +0500

    upload csv file

--- server/api/growthtools/growthTools.controller.js
+++ server/api/growthtools/growthTools.controller.js
@@ -52,15 +52,16 @@ exports.upload = function (req, res) {
           description: 'internal server error' + JSON.stringify(err)
         })
       }
+      var a = fs.createReadStream(dir + '/userfiles/' + serverPath)
+      .pipe(csv())
+      .on('data', function (data) {
+        logger.serverLog(TAG, data)
+      })
+      logger.serverLog(a)
+      return res.status(201).json({status: 'success', payload: serverPath})
     //  logger.serverLog(TAG,
     //    `file uploaded, sending response now: ${JSON.stringify(serverPath)}`)
     //  return res.status(201).json({status: 'success', payload: serverPath})
     }
   )
-  var a = fs.createReadStream(req.files.file.path)
-  .pipe(csv())
-  .on('data', function (data) {
-    logger.serverLog(TAG, data)
-  })
-  logger.serverLog(a)
 }
