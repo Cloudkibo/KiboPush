3aef2cc8e0d3a1a638ce8b1ddf1a2d70cc6f9737
anishachhatwani <i130315@nu.edu.pk>
2017-10-10 11:19:12 +0500

    upload csv file

--- server/api/growthtools/growthTools.controller.js
+++ server/api/growthtools/growthTools.controller.js
@@ -6,16 +6,13 @@
 
 const logger = require('../../components/logger')
 const TAG = 'api/growthTools/growthTools.controller.js'
-const crypto = require('crypto')
 const path = require('path')
 const fs = require('fs')
-
+const csv = require('csv-parser')
 exports.index = function (req, res) {
   logger.serverLog(TAG,
     'Index route is called')
   res.send({name: 'sojharo'})
-  // var csv = require('csv-parser')
-  // var fs = require('fs')
   // fs.createReadStream('./file.csv')
   // .pipe(csv())
   // .on('data', function (data) {
@@ -26,15 +23,7 @@ exports.index = function (req, res) {
 exports.upload = function (req, res) {
   logger.serverLog(TAG,
     `upload file route called. file is: ${JSON.stringify(req.files)}`)
-
-  var today = new Date()
-  var uid = crypto.randomBytes(5).toString('hex')
-  var serverPath = 'f' + uid + '' + today.getFullYear() + '' +
-    (today.getMonth() + 1) + '' + today.getDate()
-  serverPath += '' + today.getHours() + '' + today.getMinutes() + '' +
-    today.getSeconds()
-  let fext = req.files.file.name.split('.')
-  serverPath += '.' + fext[fext.length - 1]
+  var serverPath = req.files.file.path
 
   let dir = path.resolve(__dirname, '../../../broadcastFiles/')
 
@@ -44,7 +33,8 @@ exports.upload = function (req, res) {
       description: 'No file submitted'
     })
   }
-
+  logger.serverLog(TAG, req.files.file.path)
+  logger.serverLog(TAG, req.files.file)
   fs.rename(
     req.files.file.path,
     dir + '/userfiles/' + serverPath,
@@ -60,4 +50,9 @@ exports.upload = function (req, res) {
       return res.status(201).json({status: 'success', payload: serverPath})
     }
   )
+  fs.createReadStream('./file.csv')
+  .pipe(csv())
+  .on('data', function (data) {
+    logger.serverLog(TAG, data)
+  })
 }
