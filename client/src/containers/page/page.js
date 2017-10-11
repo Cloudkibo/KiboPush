dda4c8269064d8f62c9a134cafbb9d8aaa319995
ImranShoukat <imran.shoukat1995@gmail.com>
2017-10-11 05:15:28 +0500

    Pagination issue and update integration bug

--- client/src/containers/page/page.js
+++ client/src/containers/page/page.js
@@ -225,7 +225,7 @@ class Page extends React.Component {
                     </table>
                     <ReactPaginate previousLabel={'previous'}
                       nextLabel={'next'}
-                      breakLabel={<a href=''>...</a>}
+                      breakLabel={<a>...</a>}
                       breakClassName={'break-me'}
                       pageCount={Math.ceil(this.state.totalLength / 5)}
                       marginPagesDisplayed={2}
