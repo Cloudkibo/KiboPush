ac3da3fd2911d33ec19cd02f46490ee16b5536f4
ImranShoukat <imran.shoukat1995@gmail.com>
2017-10-12 13:16:51 +0500

    Segmentation in polls and surveys

--- client/src/containers/convo/CreateConvo.js
+++ client/src/containers/convo/CreateConvo.js
@@ -281,11 +281,15 @@ class CreateConvo extends React.Component {
     if (this.state.broadcast.length === 0) {
       return
     }
+    var isSegmentedValue = false
+    if (this.state.pageValue !== '' || this.state.genderValue.length > 0 || this.state.localeValue.length > 0) {
+      isSegmentedValue = true
+    }
     console.log(this.state.broadcast)
     var data = {
       platform: 'facebook',
       payload: this.state.broadcast,
-      isSegmented: true,
+      isSegmented: isSegmentedValue,
       segmentationPageIds: [this.state.pageValue],
       segmentationLocale: this.state.localeValue,
       segmentationGender: this.state.genderValue,
@@ -477,7 +481,7 @@ class CreateConvo extends React.Component {
                 </fieldset>
                 <div className='row'>
                   <button style={{float: 'left', marginLeft: 20}} onClick={this.newConvo} className='btn btn-primary btn-sm'> New<br /> Conversation </button>
-                  <button style={{float: 'left', marginLeft: 20}} className='btn btn-primary btn-sm' disabled={(this.state.pageValue == '')} onClick={this.testConvo}> Test<br /> Conversation </button>
+                  <button style={{float: 'left', marginLeft: 20}} className='btn btn-primary btn-sm' disabled={(this.state.pageValue === '')} onClick={this.testConvo}> Test<br /> Conversation </button>
                   <button style={{float: 'left', marginLeft: 20}} id='send' onClick={this.sendConvo} className='btn btn-primary btn-sm' disabled={(this.state.broadcast.length === 0)}>Send<br /> Conversation </button>
 
                 </div>
