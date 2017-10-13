110b7f7e5d3f241e0bbd3ed1632f9cbfc26d3f15
sania <sania@cloudkibo.com>
2017-10-13 10:51:48 -0800

    #626

--- client/src/containers/GrowthTools/customerMatchingUsingPhNum.js
+++ client/src/containers/GrowthTools/customerMatchingUsingPhNum.js
@@ -12,7 +12,8 @@ import { Alert } from 'react-bs-notifier'
 class CustomerMatching extends React.Component {
   constructor (props, context) {
     super(props, context)
-    this.state = { file: '',
+    this.state = {
+      file: '',
       textAreaValue: '',
       fileErrors: [],
       messageErrors: [],
@@ -26,13 +27,15 @@ class CustomerMatching extends React.Component {
     this.onFilesChange = this.onFilesChange.bind(this)
     this.onFilesError = this.onFilesError.bind(this)
   }
-  onFilesChange(files) {
-      this.setState({
-        file: files,
-        fileErrors: []
-      })
+
+  onFilesChange (files) {
+    this.setState({
+      file: files,
+      fileErrors: []
+    })
     console.log(this.state.file[0])
   }
+
   onFilesError (error, file) {
     console.log('error code ' + error.code + ': ' + error.message)
     this.setState({
@@ -41,7 +44,6 @@ class CustomerMatching extends React.Component {
   }
 
   handleSubmit () {
-
     var file = this.state.file
     if (file && file != '') {
       var fileData = new FormData()
@@ -49,17 +51,18 @@ class CustomerMatching extends React.Component {
       fileData.append('filename', file[0].name)
       fileData.append('filetype', file[0].type)
       fileData.append('filesize', file[0].size)
+      fileData.append('text', this.state.textAreaValue)
     }
 
     if (this.validate()) {
-      this.props.saveFileForPhoneNumbers(fileData, this.state.textAreaValue)
+      this.props.saveFileForPhoneNumbers(fileData)
     }
   }
 
   onTextChange (e) {
     this.setState({textAreaValue: e.target.value})
     if (e.target.value) {
-      this.setState({ messageErrors: []})
+      this.setState({messageErrors: []})
     } else {
       this.setState({
         messageErrors: [{errorMsg: 'Enter an invitation message'}]
@@ -125,56 +128,104 @@ class CustomerMatching extends React.Component {
                 <div className='birthday-item inline-items badges'>
                   <h3>Customer Matching Using Phone Numbers</h3>
                   <br />
-                  <h7>Upload a file with .csv extension containing phone numbers of your customers to invite them for a chat on messenger. The
-              file should contain a column with the name 'phone_numbers'. This column should list all the customers&#39; phone numbers. The phone number will be used to send him
-              an invitation on Facebook Messenger.</h7>
-                  <div className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12 dropzone'>
-                  <Files
-                    className='file-upload-area'
-                    onChange={this.onFilesChange}
-                    onError={this.onFilesError}
-                    accepts={['.csv', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel']}
-                    multiple = {false}
-                    maxFileSize={25000000}
-                    minFileSize={0}
-                    clickable>
-                    <div className='align-center'>
-                      <img src='icons/file.png' alt='Text' style={{maxHeight: 40}} />
-                      <h4>Upload here</h4>
-                      <p>Try dropping some files here, or click to select files to upload. Only '.csv' files are accepted</p>
-                      <h4>{this.state.file !== '' ? this.state.file[0].name : ''}</h4>
-                    </div>
-                  </Files>
-                  <div className='row'>
-                    <div className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
-                      <label>File Selected</label>
-                      <input type='text' disabled='true' value={this.state.file ? this.state.file[0].name : ''} style={{ width: '50%'}} />
-                      <div className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12 text-help' style={{color: 'red'}}>
+                  <h7>Upload a file with .csv extension containing phone numbers
+                    of your customers to invite them for a chat on messenger.
+                    The
+                    file should contain a column with the name 'phone_numbers'.
+                    This column should list all the customers&#39; phone
+                    numbers. The phone number will be used to send him
+                    an invitation on Facebook Messenger.
+                  </h7>
+                  <div
+                    className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12 dropzone'>
+                    <Files
+                      className='file-upload-area'
+                      onChange={this.onFilesChange}
+                      onError={this.onFilesError}
+                      accepts={[
+                        'text/csv',
+                        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                        'application/vnd.ms-excel']}
+                      multiple={false}
+                      maxFileSize={25000000}
+                      minFileSize={0}
+                      clickable>
+                      <div className='align-center'>
+                        <img src='icons/file.png' alt='Text'
+                          style={{maxHeight: 40}} />
+                        <h4>Upload here</h4>
+                        <p>Try dropping some files here, or click to select
+                          files to upload. Only '.csv' files are accepted</p>
+                        <h4>{this.state.file !== ''
+                          ? this.state.file[0].name
+                          : ''}</h4>
+                      </div>
+                    </Files>
+                    <div className='row'>
+                      <div
+                        className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
+                        <label>File Selected</label>
+                        <input type='text' disabled='true'
+                          value={this.state.file
+                                 ? this.state.file[0].name
+                                 : ''} style={{width: '50%'}} />
+                        <div
+                          className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12 text-help'
+                          style={{color: 'red'}}>
+                          {
+                            this.state.fileErrors.map(
+                              f => <span>{f.errorMsg}</span>)
+                          }
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <div
+                          className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
+                          <label>File Selected</label>
+                          <input type='text' disabled='true'
+                            value={this.state.file
+                                   ? this.state.file[0].name
+                                   : ''} style={{width: '50%'}} />
+                          <div
+                            className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12 text-help'
+                            style={{color: 'red'}}>
+                            {
+                              this.state.fileErrors.map(
+                                f => <span>{f.errorMsg}</span>)
+                            }
+                          </div>
+                        </div>
+                        <div
+                          className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
+                          <textarea className='textArea'
+                            placeholder='Enter Invitation Message'
+                            value={this.state.textAreaValue}
+                            onChange={this.onTextChange} />
+                          <div
+                            className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12 text-help'
+                            style={{color: 'red'}}>
+                            {
+                              this.state.messageErrors.map(
+                                m => <span>{m.errorMsg}</span>)
+                            }
+                          </div>
+                        </div>
+                        <div
+                          className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
+                          <button onClick={this.handleSubmit}
+                            className='btn btn-primary'>Submit
+                          </button>
+                        </div>
                         {
-                         this.state.fileErrors.map(f => <span>{f.errorMsg}</span>)
+                          this.state.alertMessage !== '' &&
+                          <center>
+                            <Alert type={this.state.type}>
+                              {this.state.alertMessage}
+                            </Alert>
+                          </center>
                         }
                       </div>
                     </div>
-                    <div className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
-                      <textarea className='textArea' placeholder='Enter Invitation Message' value={this.state.textAreaValue} onChange={this.onTextChange} />
-                      <div className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12 text-help' style={{color: 'red'}}>
-                        {
-                           this.state.messageErrors.map(m => <span>{m.errorMsg}</span>)
-                        }
-                      </div>
-                    </div>
-                    <div className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
-                      <button onClick={this.handleSubmit} className='btn btn-primary'>Submit </button>
-                    </div>
-                    {
-                      this.state.alertMessage !== '' &&
-                      <center>
-                        <Alert type={this.state.type}>
-                          {this.state.alertMessage}
-                        </Alert>
-                      </center>
-                    }
-                  </div>
                   </div>
                 </div>
               </div>
