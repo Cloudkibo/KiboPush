2a89d76b92d06eac797e5f670f64b2ebccc10b2d
Ahsan <ahsan.tanweer.26@gmail.com>
2017-10-13 15:52:53 +0500

    Added Tour Guide to the Dashboard

--- client/src/containers/GrowthTools/customerMatchingUsingPhNum.js
+++ client/src/containers/GrowthTools/customerMatchingUsingPhNum.js
@@ -19,7 +19,7 @@ class CustomerMatching extends React.Component {
       messageErrors: [],
       alertMessage: '',
       type: '',
-      disabled : false
+      disabled: false
     }
 
     this.onTextChange = this.onTextChange.bind(this)
@@ -30,16 +30,16 @@ class CustomerMatching extends React.Component {
     this.clickAlert = this.clickAlert.bind(this)
   }
   clickAlert (e) {
-      e.preventDefault()
-      this.setState({
-        file: '',
-        textAreaValue: '',
-        fileErrors: [],
-        messageErrors: [],
-        alertMessage: '',
-        type: '',
-        disabled : false
-      })
+    e.preventDefault()
+    this.setState({
+      file: '',
+      textAreaValue: '',
+      fileErrors: [],
+      messageErrors: [],
+      alertMessage: '',
+      type: '',
+      disabled: false
+    })
   }
 
   onFilesChange (files) {
@@ -106,18 +106,18 @@ class CustomerMatching extends React.Component {
     console.log('componentWillReceiveProps is called', nextProps)
     var res = nextProps.uploadResponse.fileUploadResponse
     if (res.status === 'failed') {
-        this.setState({
-          alertMessage: (`${res.status} : ${res.description}`),
-          type: 'danger',
-          disabled: true
-        })
-      } else if (res.status === 'success') {
-        this.setState({
-          alertMessage: ('Your file has been uploaded successfully.'),
-          type: 'success',
-          disabled: true
-        })
-      } else {
+      this.setState({
+        alertMessage: (`${res.status} : ${res.description}`),
+        type: 'danger',
+        disabled: true
+      })
+    } else if (res.status === 'success') {
+      this.setState({
+        alertMessage: ('Your file has been uploaded successfully.'),
+        type: 'success',
+        disabled: true
+      })
+    } else {
       this.setState({
         alertMessage: '',
         type: '',
@@ -211,12 +211,11 @@ class CustomerMatching extends React.Component {
                         </div>
                         <div
                           className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
-                        { this.state.disabled ?
-                          <button onClick={this.handleSubmit}
+                          { this.state.disabled
+                          ? <button onClick={this.handleSubmit}
                             className='btn btn-primary' disabled >Submit
                           </button>
-                          :
-                          <button onClick={this.handleSubmit}
+                          : <button onClick={this.handleSubmit}
                             className='btn btn-primary' >Submit
                           </button>
                         }
@@ -224,14 +223,14 @@ class CustomerMatching extends React.Component {
                         {
                           this.state.alertMessage !== '' &&
                             <Alert type={this.state.type}>
-                              {this.state.alertMessage} <br/>
-                              <a href="#" className="alert-link" onClick= {this.clickAlert}>Click here to select another file</a>
+                              {this.state.alertMessage} <br />
+                              <a href='#' className='alert-link' onClick={this.clickAlert}>Click here to select another file</a>
                             </Alert>
                         }
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
               </div>
             </main>
           </div>
