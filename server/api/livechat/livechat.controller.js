b9f5dc3256dae86635f74744509af1ebf5a20c2e
ImranShoukat <imran.shoukat1995@gmail.com>
2017-10-16 16:21:28 +0500

    Url meta data

---
+++ server/api/livechat/livechat.controller.js
@@ -0,0 +1,16 @@
+const logger = require('../../components/logger')
+const TAG = 'api/livechat/livechat.controller.js'
+var og = require('open-graph')
+
+exports.geturlmeta = function (req, res) {
+  var url = req.body.url
+  logger.serverLog(TAG, `Url: ${url}`)
+  og(url, function (err, meta) {
+    if (err) {
+      return res.status(404)
+        .json({status: 'failed', description: 'Meta data not found'})
+    }
+    logger.serverLog(TAG, `Url Meta: ${meta}`)
+    res.status(200).json({payload: meta})
+  })
+}
