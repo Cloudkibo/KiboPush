8a42b4855f0875343b21e1dd2969f5456afd4ff2
anishachhatwani <i130315@nu.edu.pk>
2017-10-17 16:40:40 +0500

    UI to show options for attachments, emojis, Gifs, stickers, and thumbs up #740

--- client/src/containers/liveChat/live.js
+++ client/src/containers/liveChat/live.js
@@ -4,23 +4,14 @@
  */
 
 import React from 'react'
-import { Alert } from 'react-bs-notifier'
+//  import { Alert } from 'react-bs-notifier'
 import Sidebar from '../../components/sidebar/sidebar'
 import Responsive from '../../components/sidebar/responsive'
 import Header from '../../components/header/header'
 import HeaderResponsive from '../../components/header/headerResponsive'
-import { Link } from 'react-router'
+//  import { Link } from 'react-router'
 import { connect } from 'react-redux'
-import { loadSubscribersList } from '../../redux/actions/subscribers.actions'
-import {
-  addBroadcast,
-  clearAlertMessage,
-  loadBroadcastsList,
-  sendbroadcast
-} from '../../redux/actions/broadcast.actions'
 import { bindActionCreators } from 'redux'
-import { handleDate } from '../../utility/utils'
-import ReactPaginate from 'react-paginate'
 import ChatBox from './chatbox'
 import Sessions from './sessions'
 import Profile from './profile'
@@ -29,7 +20,7 @@ class LiveChat extends React.Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      activeChat: {},
+      activeChat: {}
     }
   }
 
@@ -46,17 +37,13 @@ class LiveChat extends React.Component {
     addScript.setAttribute('src', '../../../js/main.js')
     document.body.appendChild(addScript)
   }
-
-
-
-
   componentWillReceiveProps (nextProps) {
     console.log('componentWillReceiveProps is called')
     this.setState({activeChat: this.props.chat[0]})
   }
 
   render () {
-    console.log("Chat Received",  this.props.chat)
+    console.log('Chat Received', this.props.chat)
     return (
       <div>
         <Header />
@@ -67,7 +54,7 @@ class LiveChat extends React.Component {
           <br /><br /><br /><br /><br /><br />
           <div className='row'>
             <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12'>
-             <Sessions />
+              <Sessions />
             </div>
             <div className='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12'>
 
@@ -89,7 +76,7 @@ class LiveChat extends React.Component {
 function mapStateToProps (state) {
   console.log(state)
   return {
-    chat: (state.liveChat.chat),
+    chat: (state.liveChat.chat)
   }
 }
 
--- client/src/containers/liveChat/profile.js
+++ client/src/containers/liveChat/profile.js
@@ -4,24 +4,9 @@
  */
 
 import React from 'react'
-import { Alert } from 'react-bs-notifier'
-import Sidebar from '../../components/sidebar/sidebar'
-import Responsive from '../../components/sidebar/responsive'
-import Header from '../../components/header/header'
-import HeaderResponsive from '../../components/header/headerResponsive'
-import { Link } from 'react-router'
 import { connect } from 'react-redux'
-import { loadSubscribersList } from '../../redux/actions/subscribers.actions'
-import {
-  addBroadcast,
-  clearAlertMessage,
-  loadBroadcastsList,
-  sendbroadcast
-} from '../../redux/actions/broadcast.actions'
 import { bindActionCreators } from 'redux'
-import { handleDate } from '../../utility/utils'
-import ReactPaginate from 'react-paginate'
-import ChatBox from './chatbox'
+//  import ChatBox from './chatbox'
 
 class Profile extends React.Component {
   constructor (props, context) {
@@ -44,57 +29,53 @@ class Profile extends React.Component {
     addScript.setAttribute('src', '../../../js/main.js')
     document.body.appendChild(addScript)
   }
-
-
-
-
   componentWillReceiveProps (nextProps) {
     console.log('componentWillReceiveProps is called')
   }
 
   render () {
     return (
-        <div className='ui-block'>
-                <div className='friend-item'>
-                  <div className='friend-header-thumb'>
-                    <img src='img/friend3.jpg' alt='friend' />
-                  </div>
-                  <div className='friend-item-content'>
+      <div className='ui-block'>
+        <div className='friend-item'>
+          <div className='friend-header-thumb'>
+            <img src='img/friend3.jpg' alt='friend' />
+          </div>
+          <div className='friend-item-content'>
 
-                    <div className='more'>
-                      <ul className='more-dropdown'>
-                        <li>
-                          <a href='#'>Report Profile</a>
-                        </li>
-                        <li>
-                          <a href='#'>Block Profile</a>
-                        </li>
-                        <li>
-                          <a href='#'>Turn Off Notifications</a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div className='friend-avatar'>
-                      <div className='author-thumb'>
-                        <img src='img/avatar3.jpg' alt='author' />
-                      </div>
-                      <div className='author-content'>
-                        <a href='#' className='h5 author-name'>Nicholas Grissom</a>
-                        <div className='country'>Los Angeles, CA</div>
-                      </div>
-                    </div>
+            <div className='more'>
+              <ul className='more-dropdown'>
+                <li>
+                  <a href='#'>Report Profile</a>
+                </li>
+                <li>
+                  <a href='#'>Block Profile</a>
+                </li>
+                <li>
+                  <a href='#'>Turn Off Notifications</a>
+                </li>
+              </ul>
+            </div>
+            <div className='friend-avatar'>
+              <div className='author-thumb'>
+                <img src='img/avatar3.jpg' alt='author' />
+              </div>
+              <div className='author-content'>
+                <a href='#' className='h5 author-name'>Nicholas Grissom</a>
+                <div className='country'>Los Angeles, CA</div>
+              </div>
+            </div>
 
-                    <div className='swiper-container swiper-swiper-unique-id-2 initialized swiper-container-horizontal' id='swiper-unique-id-2'>
-                      <div className='swiper-wrapper' ><div className='swiper-slide swiper-slide-duplicate swiper-slide-prev swiper-slide-duplicate-next' data-swiper-slide-index='1' >
-                        <p className='friend-about' data-swiper-parallax='-500' >
-                              Hi!, I’m Marina and I’m a Community Manager for “Gametube”. Gamer and full-time mother.
-                            </p>
-                      </div>     
-                  </div>
-                </div>
+            <div className='swiper-container swiper-swiper-unique-id-2 initialized swiper-container-horizontal' id='swiper-unique-id-2'>
+              <div className='swiper-wrapper' ><div className='swiper-slide swiper-slide-duplicate swiper-slide-prev swiper-slide-duplicate-next' data-swiper-slide-index='1' >
+                <p className='friend-about' data-swiper-parallax='-500' >
+                      Hi!, I’m Marina and I’m a Community Manager for “Gametube”. Gamer and full-time mother.
+                    </p>
               </div>
               </div>
-              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     )
   }
@@ -103,7 +84,7 @@ class Profile extends React.Component {
 function mapStateToProps (state) {
   console.log(state)
   return {
-    chat: (state.liveChat.chat),
+    chat: (state.liveChat.chat)
   }
 }
 
