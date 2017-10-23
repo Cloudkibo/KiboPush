f07135d4303a9d85fd6c8998c048c919f9965e92
ImranShoukat <imran.shoukat1995@gmail.com>
2017-10-23 21:58:33 +0500

    Send Emoji #676

--- client/src/containers/liveChat/chatbox.js
+++ client/src/containers/liveChat/chatbox.js
@@ -4,7 +4,13 @@
  */
 
 import React from 'react'
-import { fetchUserChats, uploadAttachment, deletefile, sendAttachment } from '../../redux/actions/livechat.actions'
+import {
+  fetchUserChats,
+  uploadAttachment,
+  deletefile,
+  sendAttachment,
+  sendChatMessage
+} from '../../redux/actions/livechat.actions'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import ReactPlayer from 'react-player'
@@ -131,30 +137,50 @@ class ChatBox extends React.Component {
     if (e.which === 13) {
       e.preventDefault()
       console.log('state in onEnter: ', this.state)
+      var payload = {}
+      var session = this.props.session
+      var data = {}
       if (this.state.uploadedId !== '') {
-        var payload = {
+        payload = {
           componentType: this.state.componentType,
           fileName: this.state.attachment.name,
           fileurl: this.state.uploadedId,
           size: this.state.attachment.size,
           type: this.state.attachmentType
         }
-      }
-      var session = this.props.session
-      var data = {
-        sender_id: session.page_id._id, // this is the page id: _id of Pageid
-        recipient_id: session.subscriber_id._id, // this is the subscriber id: _id of subscriberId
-        sender_fb_id: session.page_id.pageId, // this is the (facebook) :page id of pageId
-        recipient_fb_id: session.subscriber_id.pageId, // this is the (facebook) subscriber id : pageid of subscriber id
-        session_id: session._id,
-        company_id: session.company_id, // this is admin id till we have companies
-        payload: payload, // this where message content will go
-        url_meta: '',
-        status: 'unseen' // seen or unseen
+        data = {
+          sender_id: session.page_id._id, // this is the page id: _id of Pageid
+          recipient_id: session.subscriber_id._id, // this is the subscriber id: _id of subscriberId
+          sender_fb_id: session.page_id.pageId, // this is the (facebook) :page id of pageId
+          recipient_fb_id: session.subscriber_id.pageId, // this is the (facebook) subscriber id : pageid of subscriber id
+          session_id: session._id,
+          company_id: session.company_id, // this is admin id till we have companies
+          payload: payload, // this where message content will go
+          url_meta: '',
+          status: 'unseen' // seen or unseen
+        }
+        console.log(data)
+        this.props.sendAttachment(data, this.handleSendAttachment)
+      } else if (this.state.textAreaValue !== '') {
+        payload = {
+          componentType: 'text',
+          text: this.state.textAreaValue
+        }
+        data = {
+          sender_id: session.page_id._id, // this is the page id: _id of Pageid
+          recipient_id: session.subscriber_id._id, // this is the subscriber id: _id of subscriberId
+          sender_fb_id: session.page_id.pageId, // this is the (facebook) :page id of pageId
+          recipient_fb_id: session.subscriber_id.pageId, // this is the (facebook) subscriber id : pageid of subscriber id
+          session_id: session._id,
+          company_id: session.company_id, // this is admin id till we have companies
+          payload: payload, // this where message content will go
+          url_meta: '',
+          status: 'unseen' // seen or unseen
+        }
+        console.log(data)
+        this.props.sendChatMessage(data)
       }
     }
-    console.log(data)
-    this.props.sendAttachment(data, this.handleSendAttachment)
   }
 
   sendThumbsUp () {
@@ -335,8 +361,7 @@ class ChatBox extends React.Component {
                         <img style={{width: '34px', height: '34px'}} src={this.props.session.subscriber_id.profilePic} alt='author' />
                       </div>
                       {
-                        msg.payload.componentType
-                        ? (msg.payload.componentType === 'video'
+                        msg.payload.componentType && (msg.payload.componentType === 'video'
                           ? <div className='notification-event'>
                             <div className='facebook-chat-right'>
                               <ReactPlayer
@@ -375,18 +400,20 @@ class ChatBox extends React.Component {
                               />
                             </div>
                           </div>
+                          : msg.payload.text.split(' ').length === 0
+                          ? <div className='notification-event'>
+                            <span style={{fontSize: '20px'}} className='chat-message-item-right'>{msg.payload.text}</span>
+                            {/**
+                              <span className='notification-date'><time className='entry-date updated' datetime='2004-07-24T18:18'>{msg.timestamp}</time></span>
+                            **/}
+                          </div>
                           : <div className='notification-event'>
-                            <div className='facebook-chat-right'>
-                              <h6 style={{color: '#fff'}}><i className='fa fa-file-text-o' /><strong> {msg.payload.fileurl.split('?')[0].split('/')[msg.payload.fileurl.split('?')[0].split('/').length - 1]}</strong></h6>
-                            </div>
+                            <span className='chat-message-item-right'>{msg.payload.text}</span>
+                            {/**
+                              <span className='notification-date'><time className='entry-date updated' datetime='2004-07-24T18:18'>{msg.timestamp}</time></span>
+                            **/}
                           </div>
                         )
-                        : <div className='notification-event'>
-                          <span className='chat-message-item-right'>{msg.payload.text}</span>
-                          {/**
-                            <span className='notification-date'><time className='entry-date updated' datetime='2004-07-24T18:18'>{msg.timestamp}</time></span>
-                          **/}
-                        </div>
                       }
                     </li>
                   )
@@ -761,7 +788,8 @@ function mapDispatchToProps (dispatch) {
     fetchUserChats: (fetchUserChats),
     uploadAttachment: (uploadAttachment),
     deletefile: (deletefile),
-    sendAttachment: (sendAttachment)
+    sendAttachment: (sendAttachment),
+    sendChatMessage: (sendChatMessage)
   }, dispatch)
 }
 export default connect(mapStateToProps, mapDispatchToProps)(ChatBox)
