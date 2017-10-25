686c4fb257d8c3a699796f493cf5a50bd69d17ca
Sojharo <sojharo@live.com>
2017-10-25 19:19:19 +0500

    unread count #742 attachment url #849

--- server/api/sessions/sessions.controller.js
+++ server/api/sessions/sessions.controller.js
@@ -24,10 +24,33 @@ exports.index = function (req, res) {
             return res.status(500)
               .json({status: 'failed', description: 'Internal Server Error'})
           }
-          sessions[0] = _.merge(sessions[0], { chats })
-          return res.status(200).json({
-            status: 'success',
-            payload: sessions
+          sessions[0] = _.merge(sessions[0], {chats})
+
+          LiveChat.aggregate([
+            {$match: {status: 'unseen'}},
+            {$group: {_id: '$session_id', count: {$sum: 1}}}
+          ], (err2, gotUnreadCount) => {
+            if (err2) {
+              return res.status(500)
+                .json({status: 'failed', description: 'Internal Server Error'})
+            }
+            logger.serverLog(TAG, gotUnreadCount)
+
+            for (let i = 0; i < gotUnreadCount.length; i++) {
+              for (let j = 0; j < sessions.length; j++) {
+                if (sessions[j]._id === gotUnreadCount[i]._id) {
+                  sessions[j] = _.merge(sessions[j],
+                    {unreadCount: gotUnreadCount[i].count})
+                }
+              }
+            }
+
+            logger.serverLog(TAG, sessions)
+
+            return res.status(200).json({
+              status: 'success',
+              payload: sessions
+            })
           })
         })
       } else {
@@ -44,35 +67,36 @@ exports.show = function (req, res) {
   logger.serverLog(TAG,
     `Inside show session, req body = ${JSON.stringify(req.params)}`)
   Sessions.findOne({_id: req.params.id})
-  .populate('subscriber_id page_id')
-  .exec(function (err, session) {
-    if (err) {
-      return res.status(500)
-      .json({status: 'failed', description: 'Internal Server Error'})
-    }
-    if (session) {
-      LiveChat.find({session_id: session._id}, (err, chats) => {
-        if (err) {
-          return res.status(500)
+    .populate('subscriber_id page_id')
+    .exec(function (err, session) {
+      if (err) {
+        return res.status(500)
           .json({status: 'failed', description: 'Internal Server Error'})
-        }
-        session = _.merge(session, { chats })
-        return res.status(200).json({
-          status: 'success',
-          payload: session
+      }
+      if (session) {
+        LiveChat.find({session_id: session._id}, (err, chats) => {
+          if (err) {
+            return res.status(500)
+              .json({status: 'failed', description: 'Internal Server Error'})
+          }
+          session = _.merge(session, {chats})
+          return res.status(200).json({
+            status: 'success',
+            payload: session
+          })
         })
-      })
-    } else {
-      return res.status(404).json({
-        status: 'failed',
-        description: 'Session with given id is not found on server.'
-      })
-    }
-  })
+      } else {
+        return res.status(404).json({
+          status: 'failed',
+          description: 'Session with given id is not found on server.'
+        })
+      }
+    })
 }
 
 // get fb session
 exports.markread = function (req, res) {
+  // todo tell fb users that message is read
   logger.serverLog(TAG,
     `Inside mark read, req body = ${JSON.stringify(req.params)}`)
   LiveChat.update(
