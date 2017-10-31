2370c73fbf6073136211f5dc785ff28fd2d34b4d
Baqar <bjafri5@gmail.com>
2017-10-31 10:04:14 +0500

    edit-menu UI

--- client/src/containers/menu/menu.js
+++ client/src/containers/menu/menu.js
@@ -28,18 +28,21 @@ class Menu extends React.Component {
       backgroundColor: '#ffffff',
       text: '+Add Menu Item',
       openPopover: false,
+      optionSelected: '',
+      popoverHeight: '300px',
       itemMenus: [{
-      label: 'First Menu',
-      submenu: [],
-    }],
-
+        label: 'First Menu',
+        submenu: []
+      }]
     }
+    this.option1 = 'Add submenu'
+    this.option2 = 'Reply with a message'
+    this.option3 = 'Open website'
     this.pageChange = this.pageChange.bind(this)
     this.saveItem = this.saveItem.bind(this)
     this.handleClick = this.handleClick.bind(this)
     this.onSelectItem = this.onSelectItem.bind(this)
     this.handleClose = this.handleClose.bind(this)
-    this.handleCheckbox = this.handleClose.bind(this)
   }
 
   componentDidMount () {
@@ -66,13 +69,19 @@ class Menu extends React.Component {
       this.setState({pageOptions: myPages})
     }
   }
-  handleCheckbox (event) {
-    console.log('checkbox Selected: ', event.target.value)
-    this.setState({itemType: event.target.value})
-    var temp = this.state.itemMenus
-    temp[0]
-    console.log('item type', this.state.itemType)
+  handleOption (option) {
+    console.log('option Selected: ', option)
+    let popoverHeight = '300px'
+    if (option === this.option2) {
+      popoverHeight = '335px'
+    } else if (option === this.option3) {
+      popoverHeight = '365px'
+    }
+    this.setState({optionSelected: option, popoverHeight: popoverHeight})
+    // this.setState({itemType: event.target.value})
+    // console.log('item type', this.state.itemType)
   }
+
   pageChange (val) {
     console.log('Selected: ' + JSON.stringify(val))
     if (val === null) {
@@ -91,69 +100,106 @@ class Menu extends React.Component {
     this.props.addMenuItem({pageId: this.state.pageValue, menuItemType: this.state.itemType, title: this.state.itemName})
   }
   handleClose (e) {
-    this.setState({openPopover: false})
+    console.log('handleClose(e)')
+    // console.log(document.getElementById('popover'))
+    console.log(document.getElementById('popover').contains(e.target))
+    let optionClicked = document.getElementById('popover').contains(document.getElementById(e.target.id))
+    if (optionClicked) {
+      return
+    }
+    this.setState({openPopover: false, popoverHeight: '300px', optionSelected: ''})
   }
   saveItem (event) {
-    console.log('event.target.', event.target.value)
+    // console.log('event.target.', event.target.value)
     this.setState({itemName: event.target.value})
     console.log('this.state.itemName', this.state.itemName)
     //  this.props.addMenuItem({pageId: this.state.pageValue, menuItem: this.state.itemName, menuItemType: 'weblink'})
   }
   onSelectItem (e) {
-    this.setState({openPopover: !this.state.openPopover})
-    this.setState({itemselected: true, backgroundColor: '#f2f2f2', text: 'Menu Item'})
-    this.setState({openPopover: !this.state.openPopover})
+    console.log('onSelectItem(e)')
+    // this.setState({openPopover: !this.state.openPopover})
+    this.setState({openPopover: !this.state.openPopover, itemselected: true, backgroundColor: '#f2f2f2', text: 'Menu Item'})
+    // this.setState({openPopover: !this.state.openPopover})
   }
-  addItem(){
-    var temp  = this.state.itemMenus
+  addItem () {
+    var temp = this.state.itemMenus
 
-    if(temp.length >= 3){
-      return;
+    if (temp.length >= 3) {
+      return
     }
 
     temp.push({
       label: 'Menu Name',
-      submenu: [],
+      submenu: []
     })
 
     this.setState({itemMenus: temp})
   }
-  render () {
 
-    console.log("this dot target", this.refs['0-item'])
+  popoverMouse (e) {
+    console.log('mouse on popover')
+  }
+  render () {
+    if (this.target) {
+      console.log('this.target', this.target, this.refs[this.target])
+    }
+    console.log('popover', this.state.openPopover)
 
     let popup = <Popover
-      style={{boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)', borderRadius: '5px', zIndex: 25, width: '300px', height: '250px'}}
+      style={{boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)', borderRadius: '5px', zIndex: 25, width: '300px', height: this.state.popoverHeight}}
       placement='right'
-      target={this.refs[this.target]}
+      target={this.refs['0-item']}
       show={this.state.openPopover}
-      onHide={this.handleClose} >
-      <div className='ui-block-title'>
-        <p><b>When Pressed:</b></p>
+      onHide={this.handleClose}
+      mouseOver={this.popoverMouse.bind(this)}>
+      <div id='popover'>
+        <div className='ui-block-title' style={{marginBottom: '20px'}} >
+          <h4>Edit Menu Item</h4>
+        </div>
+        <form id='menuForm' style={{marginBottom: '20px'}}>
+          <h5>When Pressed:</h5>
+          <div id='divOption1'>
+            <label id='labelOption1' className='radio-inline'>
+              <input id='option1' type='radio' checked={this.state.optionSelected === this.option1} name='menuOption' value='option1' onChange={() => this.handleOption(this.option1)} />
+              {this.option1}
+            </label>
+          </div>
+          <div id='divOption2'>
+            <label id='labelOption2' className='radio-inline'>
+              <input id='option2' type='radio' checked={this.state.optionSelected === this.option2} name='menuOption' value='option2' onChange={() => this.handleOption(this.option2)} />
+              {this.option2}
+            </label>
+          </div>
+          <div id='divOption3'>
+            <label id='labelOption3' className='radio-inline'>
+              <input id='option3' type='radio' checked={this.state.optionSelected === this.option3} name='menuOption' value='option3' onChange={() => this.handleOption(this.option3)} />
+              {this.option3}
+            </label>
+          </div>
+        </form>
+        {this.state.optionSelected === this.option2 &&
+        <div className='container'>
+
+          <Link to='CreateMessage'>
+            <div className='row'>
+              <button style={{margin: 'auto', marginBottom: '20px', color: '#333', backgroundColor: '#fff', borderColor: '#ccc'}} className='btn btn-block'> + Create New Message </button>
+            </div>
+          </Link>
+        </div>
+    }
+        {this.state.optionSelected === this.option3 &&
+        <div className='container'>
+          <div className='row'>
+            <label><b>Website URL to open</b></label>
+            <input style={{marginBottom: '20px'}} type='url' className='form-control' />
+          </div>
+        </div>
+    }
+        <button onClick={this.handleClick} className='btn btn-primary btn-sm pull-right'> Done </button>
+        <button style={{color: '#333', backgroundColor: '#fff', borderColor: '#ccc'}} onClick={this.handleClose} className='btn pull-left'> Cancel </button>
       </div>
-      <form>
-        <div className='checkbox'>
-          <label>
-            <input type='checkbox' value='option1' onClick={this.handleCheckbox} />
-            Open submenu
-          </label>
-        </div>
-        <div className='checkbox'>
-          <label>
-            <input type='checkbox' value='option2' />
-            Reply with a message
-          </label>
-        </div>
-        <div className='checkbox'>
-          <label>
-            <input type='checkbox' value='option2' />
-            Open website
-          </label>
-        </div>
-      </form>
-      <button onClick={this.handleClick} className='btn btn-primary btn-sm pull-right'> Done </button>
-      <button style={{color: '#333', backgroundColor: '#fff', borderColor: '#ccc'}} onClick={this.handleClose} className='btn pull-left'> Cancel </button>
     </Popover>
+
     return (
       <div>
         <Header />
@@ -184,48 +230,45 @@ class Menu extends React.Component {
             <h4 style={{paddingLeft: '22px'}}>Edit Menu</h4>
             <ul style={{paddingLeft: '20px', width: '30%'}}>
               {
-                
-                this.state.itemMenus.map((itm, index) => {
-                if(this.state.itemMenus[index + 1] || index === 2){
-                  return  <li>
-                          <div ref={index + "-item"} style={{paddingTop: '5px'}} className='align-center'>
-                            <form className='form-inline'>
-                              <div className='form-group'><input type='text' placeholder='Menu Item' value={itm.label} className='form-control' onChange={this.saveItem}  onClick={() => { this.target=index + "-item"; this.onSelectItem(index);}}  style={{width: '350px'}} /></div>
-                            </form>
-                            {popup}
-                          </div>
-                          <li style={{marginLeft: 50}}>
-                               <div ref={index + "-sub-item"} style={{paddingTop: '5px'}} className='align-center' >
-                                <form className='form-inline'>
-                                  <div className='form-group'><input type='text' placeholder='Menu Item' value={itm.label} onClick={() => { this.target=index + "-sub-item"; this.onSelectItem(index);}} className='form-control' onChange={this.saveItem}  style={{width: '350px'}} /></div>
-                                </form>
-                                {popup}
-                              </div>
-                          </li>
-                        </li>
-                }else{
-                  return  <li>
-                          <div ref={index + "-item"}  style={{paddingTop: '5px'}}  className='align-center'>
-                            <form className='form-inline'>
-                              <div className='form-group'><input type='text' placeholder='Menu Item' value={itm.label} className='form-control' onChange={this.saveItem} onClick={() => { this.target=index + "-item"; this.onSelectItem(index)}}  style={{width: '350px'}} /> <div onClick={this.addItem.bind(this)} style={{margin: 10}}><i className="fa fa-plus" aria-hidden="true"></i></div></div>
-                            </form>
-                            {popup}
-                          </div>
-                          <li style={{marginLeft: 50}}>
-                              <div ref={index + "-sub-item"} style={{paddingTop: '5px'}} className='align-center' >
-                                <form className='form-inline'>
-                                  <div className='form-group'><input type='text' placeholder='Menu Item' value={itm.label} className='form-control' onClick={() => { this.target=index + "-sub-item"; this.onSelectItem(index);}} onChange={this.saveItem}  style={{width: '350px'}} /></div>
-                                </form>
-                                {popup}
-                              </div>
-                          </li>
-                        </li>
-                }
-                
 
+                this.state.itemMenus.map((itm, index) => {
+                  if (this.state.itemMenus[index + 1] || index === 2) {
+                    return <li>
+                      <div id={index} ref={index + '-item'} style={{paddingTop: '5px'}} className='align-center'>
+                        <form className='form-inline'>
+                          <div className='form-group'><input type='text' placeholder='Menu Item' value={itm.label} className='form-control' onChange={this.saveItem} onClick={() => { this.target = index + '-item'; this.onSelectItem(index) }} style={{width: '350px'}} /></div>
+                        </form>
+                        {popup}
+                      </div>
+                      <li style={{marginLeft: 50}}>
+                        <div id={index} ref={index + '-sub-item'} style={{paddingTop: '5px'}} className='align-center' >
+                          <form className='form-inline'>
+                            <div className='form-group'><input type='text' placeholder='Menu Item' value={itm.label} onClick={() => { this.target = index + '-sub-item'; this.onSelectItem(index) }} className='form-control' onChange={this.saveItem} style={{width: '350px'}} /></div>
+                          </form>
+                          {popup}
+                        </div>
+                      </li>
+                    </li>
+                  } else {
+                    return <li>
+                      <div id={index} ref={index + '-item'} style={{paddingTop: '5px'}} className='align-center'>
+                        <form className='form-inline'>
+                          <div className='form-group'><input type='text' placeholder='Menu Item' value={itm.label} className='form-control' onChange={this.saveItem} onClick={() => { this.target = index + '-item'; this.onSelectItem(index) }} style={{width: '350px'}} /> <div onClick={this.addItem.bind(this)} style={{margin: 10}}><i className='fa fa-plus' aria-hidden='true' /></div></div>
+                        </form>
+                        {popup}
+                      </div>
+                      <li style={{marginLeft: 50}}>
+                        <div id={index} ref={index + '-sub-item'} style={{paddingTop: '5px'}} className='align-center' >
+                          <form className='form-inline'>
+                            <div className='form-group'><input type='text' placeholder='Menu Item' value={itm.label} className='form-control' onClick={() => { this.target = index + '-sub-item'; this.onSelectItem(index) }} onChange={this.saveItem} style={{width: '350px'}} /></div>
+                          </form>
+                          {popup}
+                        </div>
+                      </li>
+                    </li>
+                  }
                 })
               }
-                
 
               <li><input style={{margin: 10}} type='text' readOnly value='Powered by KiboPush' className='form-control' style={{width: '350px'}} /></li>
               <p><b>Note: </b>Only three menu items can be added.</p>
