2e29a0aa68edc686631cd18de3b5314bfe6e2e14
anishachhatwani <i130315@nu.edu.pk>
2017-11-01 15:41:43 +0500

    Stacked bar for data displaying sent vs seen

--- client/src/redux/actions/dashboard.actions.js
+++ client/src/redux/actions/dashboard.actions.js
@@ -30,3 +30,31 @@ export function loadDashboardData () {
     // dispatch(updateDashboard(res))
   }
 }
+export function updateSentVsSeen (data) {
+  console.log('Data Fetched From Server', data)
+  return {
+    type: ActionTypes.UPDATE_SENT_VS_SEEN,
+    data
+  }
+}
+
+export function sentVsSeen () {
+  // here we will fetch list of subscribers from endpoint
+  return (dispatch) => {
+    callApi('dashboard/sentVsSeen')
+      .then(res => dispatch(updateSentVsSeen(res.payload)))
+    // var res = {
+    //   pages: 13,
+    //   subscribers: 26,
+    //   scheduledBroadcast: 0,
+    //   recentBroadcasts: [],
+    //   username: 'Dayem Siddiqui',
+    //   activityChart: {
+    //     polls: 24,
+    //     messages: 13,
+    //     surveys: 45
+    //   }
+    // }
+    // dispatch(updateDashboard(res))
+  }
+}
