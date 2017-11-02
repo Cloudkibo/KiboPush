40d700552a49915ae07b43c9d1f0597409132fde
Sojharo <sojharo@live.com>
2017-11-02 22:04:52 +0500

    new payload discussed with dayem #788

--- server/api/menu/menu.controller.js
+++ server/api/menu/menu.controller.js
@@ -3,9 +3,10 @@
  */
 const logger = require('../../components/logger')
 const TAG = 'api/menu/menu.controller.js'
+const Pages = require('../../api/pages/Pages.model')
 let Menu = require('./menu.model')
 const needle = require('needle')
-const _ = requrie('lodash')
+const _ = require('lodash')
 
 // Get list of menu items
 exports.index = function (req, res) {
@@ -47,33 +48,51 @@ exports.create = function (req, res) {
     return res.status(400)
       .json({status: 'failed', description: 'Parameters are missing'})
   }
-  const menu = new Menu(req.body)
 
-  // save model to MongoDB
-  menu.save((err, workflow) => {
+  Pages.findOne({pageId: req.body.pageId, userId: req.body.userId}, (err, page) => {
     if (err) {
-      res.status(500).json({
-        status: 'Failed',
+      logger.serverLog(TAG,
+        `Internal Server Error ${JSON.stringify(err)}`)
+      return res.status(500).json({
+        status: 'failed',
         error: err,
-        description: 'Failed to insert record'
+        description: 'Failed to find page. Internal Server Error'
       })
-      logger.serverLog(TAG, JSON.stringify(err))
-    } else {
-      const requestUrl = `https://graph.facebook.com/v2.6/me/messenger_profile?access_token=${page.access_token}`
-
-      needle.request('post', requestUrl, req.body.payload, {json: true},
-        (err, resp) => {
-          if (!err) {
-            logger.serverLog(TAG,
-              `Menu added to page ${page.pageName}`)
-          }
-          if (err) {
-            logger.serverLog(TAG,
-              `Internal Server Error ${JSON.stringify(err)}`)
-          }
-        })
-
-      res.status(201).json({status: 'Success', payload: workflow})
     }
+    if (!page) {
+      return res.status(404).json({
+        status: 'failed',
+        description: 'Page not found'
+      })
+    }
+    const menu = new Menu(req.body)
+
+    // save model to MongoDB
+    menu.save((err, workflow) => {
+      if (err) {
+        res.status(500).json({
+          status: 'failed',
+          error: err,
+          description: 'Failed to insert record'
+        })
+        logger.serverLog(TAG, JSON.stringify(err))
+      } else {
+        const requestUrl = `https://graph.facebook.com/v2.6/me/messenger_profile?access_token=${page.access_token}`
+
+        needle.request('post', requestUrl, req.body.payload, {json: true},
+          (err, resp) => {
+            if (!err) {
+              logger.serverLog(TAG,
+                `Menu added to page ${page.pageName}`)
+            }
+            if (err) {
+              logger.serverLog(TAG,
+                `Internal Server Error ${JSON.stringify(err)}`)
+            }
+          })
+
+        res.status(201).json({status: 'Success', payload: workflow})
+      }
+    })
   })
 }
