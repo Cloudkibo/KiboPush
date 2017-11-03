1bb81fb2a878576f7e3b850655c57e8ed09c5584
sania <sania@cloudkibo.com>
2017-11-02 21:12:24 -0800

    UI for Invite to Subscriber #925

--- client/src/containers/subscribeToMessenger/subscribeToMessenger.js
+++ client/src/containers/subscribeToMessenger/subscribeToMessenger.js
@@ -15,13 +15,9 @@ import {
 } from '../../redux/actions/pages.actions'
 import { bindActionCreators } from 'redux'
 import { Link } from 'react-router'
-import CopyToClipboard from 'react-copy-to-clipboard'
-import { Alert } from 'react-bs-notifier'
-
 class SubscribeToMessenger extends React.Component {
   constructor (props, context) {
     super(props, context)
-    this.subscribeToMessenger = this.subscribeToMessenger.bind(this)
     this.onChange = this.onChange.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.onChangeValue = this.onChangeValue.bind(this)
@@ -104,12 +100,6 @@ class SubscribeToMessenger extends React.Component {
     document.body.appendChild(addScript)
   }
 
-  subscribeToMessenger () {
-    // this.props.addBroadcast('', {platform: 'Facebook', type: 'message', created_at: '15th Aug 2017', sent: 41});
-    // console.log("Broadcast added");
-   // alert('poll created')
-  }
-
   render () {
     return (
       <div>
@@ -117,42 +107,34 @@ class SubscribeToMessenger extends React.Component {
         <HeaderResponsive />
         <Sidebar />
         <Responsive />
-
         <div className='container'>
-          <br />
-          <br />
-          <br />
           {this.state.showbutton === true
-            ? <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+            ? <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12' style={{top: '100px'}}>
               <h2 className='presentation-margin'>Subscribe to Messenger</h2>
               <div className='ui-block'>
                 <div className='news-feed-form'>
-                  {this.props.pages &&
-                  <div>
-                    <label>Choose Page</label>
+                  { this.props.pages &&
+                    <div className='col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6'>
+                      <label>Choose Page</label>
+                      <div className='form-group form-inline'>
+                        <select className='input-sm' onChange={this.onChangeValue}>
 
-                    <div className='form-group form-inline'>
-
-                      <select className='input-sm' onChange={this.onChangeValue}>
-
-                        {this.props.pages.map((page, i) => (
-                          <option value={page.pageId}>{page.pageName}</option>
-                      ))
-                  }
-                      </select>
+                          { this.props.pages.map((page, i) => (
+                            <option value={page.pageId}>{page.pageName}</option>
+                            ))
+                          }
+                        </select>
+                      </div>
                     </div>
-                    <br />
-                  </div>
-              }
+                  }
                   <div className='alert alert-success'>
                     <h4 className='block'>Code for Send To Messenger Button</h4>
-                  To embed the facebook messenger button on your website, you
-                  need to put this line inside &lt;body&gt; tag of HTML of your
-                  website's each page.
-                  <br /><br />
+                    To embed the facebook messenger button on your website, you
+                    need to put this line inside &lt;body&gt; tag of HTML of your
+                    website&#39;s each page.
+                    <br /><br />
                     <center>
                       <code className='codeBox'>
-
                       &lt;a class='btn' href="https://m.me/{this.state.pageid}"
                       style='{'background:' + this.state.buttonColor +
                     ';color: ' + this.state.fontColor +
@@ -161,18 +143,16 @@ class SubscribeToMessenger extends React.Component {
                     </code>
                     </center>
                     <br />
-                  Note: For css, we are using Bootstrap library. The class btn
-                  is defined in Bootrap css file.<br />
-
+                    Note: For css, we are using Bootstrap library. The class btn
+                    is defined in Bootrap css file.
+                    <br />
                   </div>
 
                   <div className='tab-content'>
                     <div className='tab-pane active' id='home-1' role='tabpanel'
-                      aria-expanded='true'>
-
+                      aria-expanded='true' style={{display: 'flex', flexDirection: 'row'}}>
                       <br />
-
-                      <div className='col-xl-12'>
+                      <div className='col-xl-6'>
                         <div className='form-group'>
                           <label for='colorbtn'> Choose Color</label>
                           <select className='form-control' id='colorbtn'
@@ -180,7 +160,6 @@ class SubscribeToMessenger extends React.Component {
                             onChange={this.handleChange.bind(this)}>
                             <option value='blue'>Blue</option>
                             <option value='white'>White</option>
-
                           </select>
                         </div>
                         <div className='form-group'>
@@ -191,7 +170,9 @@ class SubscribeToMessenger extends React.Component {
                         </div>
                         <br />
                         <br />
-                        <div className='form-group'>
+                      </div>
+                      <div className='col-xl-6'>
+                        <div className='form-group' style={{textAlign: 'center', border: 'dashed', height: '135px', padding: '20px', color: '#3c763d'}}>
                           <label for='textbtn'> Button Preview</label>
                           <br />
                           <a className='btn' href='#' style={{
@@ -203,35 +184,13 @@ class SubscribeToMessenger extends React.Component {
                               className='fa fa-facebook fa-lg' /> {this.state.buttonText}
                           </a>
                         </div>
-
-                         This is a link to your page, you can send it to your friends
-                  <br />
-
-                        <input value={this.state.fblink} />
-
-                        <CopyToClipboard text={this.state.fblink}
-                          onCopy={() => this.setState({copied: true})}>
-                          <button onClick={() => { this.setState({copied: true}) }}
-                            className='uk-button uk-button-small uk-button-primary'
-                            style={{margin: 5}}>Copy
-                    </button>
-                        </CopyToClipboard>
-                        {this.state.copied &&
-                        <center>
-                          <Alert type='success'>
-                      Copied!
-                    </Alert>
-                        </center>
-                    }
                       </div>
-
                     </div>
-
                   </div>
                 </div>
               </div>
             </div>
-          : <div className='alert alert-success'>
+          : <div className='alert alert-success' style={{marginTop: '150px'}}>
             <h4 className='block'>Code for Send To Messenger Button</h4>
                   You dont have any connected pages. Please connect pages first.
                   <Link to='/addpages' className='btn btn-sm btn-blue'> Add Pages</Link>
