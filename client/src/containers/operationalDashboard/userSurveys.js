58720d4e545bad9f5b47f7165d2a80f45eda6bb7
sania <sania@cloudkibo.com>
2017-11-07 10:30:58 -0800

    add filters on polls and surveys #1000 - pagination

--- client/src/containers/operationalDashboard/userSurveys.js
+++ client/src/containers/operationalDashboard/userSurveys.js
@@ -94,6 +94,7 @@ class SurveysInfo extends React.Component {
     if (!val) {
       this.setState({selectedFilterValue: null})
       this.displayData(0, this.props.surveys)
+      this.setState({ totalLength: this.props.surveys.length })
     } else if (val.value === 10) {
       console.log('Selected:', val.value)
       this.filterByDays(10)
