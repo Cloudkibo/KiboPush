6195c40738db1e413635abc77b1e749559c529a8
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-11-11 03:23:05 +0500

    Subscriber

--- client/src/containers/subscriber/subscriber.js
+++ client/src/containers/subscriber/subscriber.js
@@ -171,71 +171,111 @@ class Subscriber extends React.Component {
             <div className='m-content'>
               <div className='row'>
                 <div className='col-xl-12 col-lg-12  col-md-12 col-sm-12 col-xs-12'>
-                  <h3>Subscribers</h3>
-                  <div className='ui-block'>
-                    <div className='birthday-item inline-items badges'>
-                      <Link to='/invitesubscribers' className='btn btn-primary btn-sm'
-                        style={{float: 'right'}}>Invite Subscribers</Link>
-
-                      { this.props.subscribers && this.props.subscribers.length > 0
-                  ? <div className='table-responsive'>
-                    <form>
-                      <div className='form-row'>
-                        <div style={{display: 'inline-block'}} className='form-group col-md-4'>
-                          <label> Search </label>
-                          <input type='text' placeholder='Search Subscribers' className='form-control' onChange={this.searchSubscriber} />
-                        </div>
-                        <div style={{display: 'inline-block'}} className='form-group col-md-4'>
-                          <label> Gender </label>
-                          <select className='input-sm' value={this.state.filterByGender} onChange={this.handleFilterByGender} >
-                            <option value='' disabled>Filter by Gender...</option>
-                            <option value='male'>male</option>
-                            <option value='female'>female</option>
-                            <option value='other'>other</option>
-                            <option value=''>all</option>
-                          </select>
-                        </div>
-                        <div style={{display: 'inline-block'}} className='form-group col-md-4'>
-                          <label> Locale </label>
-                          <select className='input-sm' value={this.state.filterByLocale} onChange={this.handleFilterByLocale} >
-                            <option value='' disabled>Filter by Locale...</option>
-                            {
-                              this.props.locales.map((locale, i) => (
-                                <option value={locale}>{locale}</option>
-                              ))
-                            }
-                            <option value=''>all</option>
-                          </select>
+                  <div className='m-portlet m-portlet--mobile'>
+                    <div className='m-portlet__head'>
+                      <div className='m-portlet__head-caption'>
+                        <div className='m-portlet__head-title'>
+                          <h3 className='m-portlet__head-text'>
+                            Page Subscribers
+                          </h3>
                         </div>
                       </div>
-                    </form>
+                    </div>
+
+                    <div className='m-portlet__body'>
+
+                      <div className='m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30'>
+                        <div className='row align-items-center'>
+                          <div className='col-xl-8 order-2 order-xl-1'>
+                            <div className='form-group m-form__group row align-items-center'>
+                              <div className='col-md-4'>
+                                <div className='m-form__group m-form__group--inline'>
+                                  <div className='m-form__label'>
+                                    <label>Gender:</label>
+                                  </div>
+                                  <div className='m-form__control'>
+                                    <div className='btn-group bootstrap-select form-control m-bootstrap-select m-bootstrap-select--solid dropup'><button type='button' className='btn dropdown-toggle bs-placeholder btn-default' data-toggle='dropdown' role='button' data-id='m_form_status' title='All' aria-expanded='false'><span className='filter-option pull-left'>All</span>&nbsp;<span className='bs-caret'><span className='caret' /></span></button><div className='dropdown-menu open' role='combobox' x-placement='bottom-start'><ul className='dropdown-menu inner' role='listbox' aria-expanded='false'><li data-original-index='0' className='selected'><a tabindex='0' className='' data-tokens='null' role='option' aria-disabled='false' aria-selected='true'><span className='text'>All</span><span className='glyphicon glyphicon-ok check-mark' /></a></li><li data-original-index='1'><a tabindex='0' className='' data-tokens='null' role='option' aria-disabled='false' aria-selected='false'><span className='text'>Pending</span><span className='glyphicon glyphicon-ok check-mark' /></a></li><li data-original-index='2'><a tabindex='0' className='' data-tokens='null' role='option' aria-disabled='false' aria-selected='false'><span className='text'>Delivered</span><span className='glyphicon glyphicon-ok check-mark' /></a></li><li data-original-index='3'><a tabindex='0' className='' data-tokens='null' role='option' aria-disabled='false' aria-selected='false'><span className='text'>Canceled</span><span className='glyphicon glyphicon-ok check-mark' /></a></li></ul>
+                                    </div>
+                                      <select className='form-control m-bootstrap-select m-bootstrap-select--solid' id='m_form_status' tabindex='-98'>
+                                        <option value=''>All</option>
+                                        <option value='1'>Pending</option>
+                                        <option value='2'>Delivered</option>
+                                        <option value='3'>Canceled</option>
+                                      </select></div>
+                                  </div>
+                                </div>
+                                <div className='d-md-none m--margin-bottom-10' />
+                              </div>
+                              <div className='col-md-4'>
+                                <div className='m-form__group m-form__group--inline'>
+                                  <div className='m-form__label'>
+                                    <label className='m-label m-label--single'>Local:</label>
+                                  </div>
+                                  <div className='m-form__control'>
+                                    <div className='btn-group bootstrap-select form-control m-bootstrap-select m-bootstrap-select--solid'>
+                                      <button type='button' className='btn dropdown-toggle bs-placeholder btn-default' data-toggle='dropdown' role='button' data-id='m_form_type' title='All'><span className='filter-option pull-left'>All</span>&nbsp;<span className='bs-caret'><span className='caret' /></span></button>
+                                      <div className='dropdown-menu open' role='combobox'>
+                                        <ul className='dropdown-menu inner' role='listbox' aria-expanded='false'><li data-original-index='0' className='selected'><a tabindex='0' className='' data-tokens='null' role='option' aria-disabled='false' aria-selected='true'><span className='text'>All</span><span className='glyphicon glyphicon-ok check-mark' /></a></li><li data-original-index='1'><a tabindex='0' className='' data-tokens='null' role='option' aria-disabled='false' aria-selected='false'><span className='text'>Online</span><span className='glyphicon glyphicon-ok check-mark' /></a></li><li data-original-index='2'><a tabindex='0' className='' data-tokens='null' role='option' aria-disabled='false' aria-selected='false'><span className='text'>Retail</span><span className='glyphicon glyphicon-ok check-mark' /></a></li><li data-original-index='3'><a tabindex='0' className='' data-tokens='null' role='option' aria-disabled='false' aria-selected='false'><span className='text'>Direct</span><span className='glyphicon glyphicon-ok check-mark' /></a></li></ul></div><select className='form-control m-bootstrap-select m-bootstrap-select--solid' id='m_form_type' tabindex='-98'>
+                                          <option value=''>All</option>
+                                          <option value='1'>Online</option>
+                                          <option value='2'>Retail</option>
+                                          <option value='3'>Direct</option>
+                                        </select></div>
+                                  </div>
+                                </div>
+                                <div className='d-md-none m--margin-bottom-10' />
+                              </div>
+                              <div className='col-md-4'>
+                                <div className='m-input-icon m-input-icon--left'>
+                                  <input type='text' className='form-control m-input m-input--solid' placeholder='Search...' id='generalSearch' />
+                                  <span className='m-input-icon__icon m-input-icon__icon--left'>
+                                    <span><i className='la la-search' /></span>
+                                  </span>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className='col-xl-4 order-1 order-xl-2 m--align-right'>
+                            <Link to='/invitesubscribers' className='btn btn-focus m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill'>
+                              <span>
+                                <i className='la la-cart-plus' />
+                                <span>Invite Subscribers</span>
+                              </span>
+                            </Link>
+                            <div className='m-separator m-separator--dashed d-xl-none' />
+                          </div>
+                        </div>
+                      </div>
+
+                      { this.props.subscribers && this.props.subscribers.length > 0
+                  ? <div className='m_datatable m-datatable m-datatable--default m-datatable--loaded'>
                     {
                       this.state.subscribersData && this.state.subscribersData.length > 0
                       ? <div>
-                        <table className='table table-striped'>
-                          <thead>
-                            <tr>
-                              <th>Profile Picture</th>
-                              <th>Name</th>
-                              <th>Page</th>
-                              <th>Locale</th>
-                              <th>Gender</th>
+                        <table className='m-datatable__table'>
+                          <thead className='m-datatable__head'>
+                            <tr className="m-datatable__row">
+                              <th className="m-datatable__cell--center m-datatable__cell m-datatable__cell--sort">Profile Picture</th>
+                              <th className="m-datatable__cell--center m-datatable__cell m-datatable__cell--sort">Name</th>
+                              <th className="m-datatable__cell--center m-datatable__cell m-datatable__cell--sort">Page</th>
+                              <th className="m-datatable__cell--center m-datatable__cell m-datatable__cell--sort">Locale</th>
+                              <th className="m-datatable__cell--center m-datatable__cell m-datatable__cell--sort">Gender</th>
                             </tr>
                           </thead>
-                          <tbody>
+                          <tbody className="m-datatable__body">
                             {
                               this.state.subscribersData.map((subscriber, i) => (
-                                <tr>
-                                  <td>
+                                <tr className="m-datatable__row m-datatable__row--even">
+                                  <td className="m-datatable__cell--center m-datatable__cell">
                                     <img alt='pic'
                                       src={(subscriber.profilePic) ? subscriber.profilePic : ''}
                                       className='img-circle' width='60' height='60'
                                     />
                                   </td>
-                                  <td>{subscriber.firstName} {subscriber.lastName}</td>
-                                  <td>{subscriber.pageId.pageName}</td>
-                                  <td>{subscriber.locale}</td>
-                                  <td>{subscriber.gender}</td>
+                                  <td className="m-datatable__cell--center m-datatable__cell">{subscriber.firstName} {subscriber.lastName}</td>
+                                  <td className="m-datatable__cell--center m-datatable__cell">{subscriber.pageId.pageName}</td>
+                                  <td className="m-datatable__cell--center m-datatable__cell">{subscriber.locale}</td>
+                                  <td className="m-datatable__cell--center m-datatable__cell">{subscriber.gender}</td>
                                 </tr>
                               ))
                             }
