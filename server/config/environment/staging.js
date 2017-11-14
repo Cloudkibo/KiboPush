5dd4f52a26b1e11283a91c09db56f1afd3b21708
Sojharo <sojharo@live.com>
2017-11-14 12:43:05 +0500

    staging ssl config

--- server/config/environment/staging.js
+++ server/config/environment/staging.js
@@ -15,6 +15,6 @@
    facebook: {
      clientID: process.env.FACEBOOK_ID,
      clientSecret: process.env.FACEBOOK_SECRET,
-     callbackURL: `${process.env.DOMAIN}`
+     callbackURL: `${process.env.DOMAIN}/auth/facebook/callback`
    }
  }
