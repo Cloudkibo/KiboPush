0044ad493aa1d550fb8779cb7c493b996c399fa9
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-11-20 14:47:41 +0500

    J'aime pa coding

--- client/src/containers/page/page.js
+++ client/src/containers/page/page.js
@@ -70,11 +70,12 @@ class Page extends React.Component {
     addScript.setAttribute('src', '../../../js/theme-plugins.js')
     document.body.appendChild(addScript)
     addScript = document.createElement('script')
-    addScript.setAttribute('src', '../../../assets/vendors/base/vendors.bundle.js')
-    document.body.appendChild(addScript)
-    addScript = document.createElement('script')
     addScript.setAttribute('src', '../../../assets/demo/default/base/scripts.bundle.js')
     document.body.appendChild(addScript)
+    addScript = document.createElement('script')
+    addScript.setAttribute('src', '../../../assets/vendors/base/vendors.bundle.js')
+    document.body.appendChild(addScript)
+    
     var datatable = $('#m_datatable').mDatatable({
       pagination: true,
       paging: true,
