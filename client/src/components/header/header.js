b89e8c868c636188f7d3af7c9a7546770aba853e
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-11-20 12:17:57 +0500

    Header Added

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -86,88 +86,8 @@ class Header extends React.Component {
               <div id='m_header_topbar' className='m-topbar  m-stack m-stack--ver m-stack--general'>
                 <div className='m-stack__item m-topbar__nav-wrapper'>
                   <ul className='m-topbar__nav m-nav m-nav--inline'>
-                    <li className='m-nav__item m-topbar__notifications m-topbar__notifications--img m-dropdown m-dropdown--large m-dropdown--header-bg-fill m-dropdown--arrow m-dropdown--align-center m-dropdown--mobile-full-width' data-dropdown-toggle='click' data-dropdown-persistent='true'>
-                      <a href='#' className='m-nav__link m-dropdown__toggle' id='m_topbar_notification_icon'>
-                        <span className='m-nav__link-badge m-badge m-badge--dot m-badge--dot-small m-badge--danger' />
-                        <span className='m-nav__link-icon'>
-                          <i className='flaticon-music-2' />
-                        </span>
-                      </a>
-                      <div className='m-dropdown__wrapper'>
-                        <span className='m-dropdown__arrow m-dropdown__arrow--center' />
-                        <div className='m-dropdown__inner'>
-                          <div className='m-dropdown__body'>
-                            <div className='m-dropdown__content'>
-                              <ul className='nav nav-tabs m-tabs m-tabs-line m-tabs-line--brand' role='tablist'>
-                                <li className='nav-item m-tabs__item'>
-                                  <a className='nav-link m-tabs__link active' data-toggle='tab' href='#topbar_notifications_notifications' role='tab'>Alerts</a>
-                                </li>
-                              </ul>
-                              <div className='tab-content'>
-                                <div className='tab-pane active' id='topbar_notifications_notifications' role='tabpanel'>
-                                  <div className='m-scrollable' data-scrollable='true' data-max-height='250' data-mobile-max-height='200'>
-                                    <div className='m-list-timeline m-list-timeline--skin-light'>
-                                      <div className='m-list-timeline__items'>
-                                        <div className='m-list-timeline__item'>
-                                          <span className='m-list-timeline__badge -m-list-timeline__badge--state-success' />
-                                          <span className='m-list-timeline__text'>12 new users registered</span>
-                                          <span className='m-list-timeline__time'>Just now</span>
-                                        </div>
-                                        <div className='m-list-timeline__item'>
-                                          <span className='m-list-timeline__badge' />
-                                          <span className='m-list-timeline__text'>System shutdown
-                                            <span className='m-badge m-badge--success m-badge--wide'>pending</span>
-                                          </span>
-                                          <span className='m-list-timeline__time'>14 mins</span>
-                                        </div>
-                                        <div className='m-list-timeline__item'>
-                                          <span className='m-list-timeline__badge' />
-                                          <span className='m-list-timeline__text'>New invoice received</span>
-                                          <span className='m-list-timeline__time'>20 mins</span>
-                                        </div>
-                                        <div className='m-list-timeline__item'>
-                                          <span className='m-list-timeline__badge' />
-                                          <span className='m-list-timeline__text'>DB overloaded 80%
-                                            <span className='m-badge m-badge--info m-badge--wide'>settled</span>
-                                          </span>
-                                          <span className='m-list-timeline__time'>1 hr</span>
-                                        </div>
-                                        <div className='m-list-timeline__item'>
-                                          <span className='m-list-timeline__badge' />
-                                          <span className='m-list-timeline__text'>System error -
-                                            <a href='#' className='m-link'>Check</a>
-                                          </span>
-                                          <span className='m-list-timeline__time'>2 hrs</span>
-                                        </div>
-                                        <div className='m-list-timeline__item m-list-timeline__item--read'>
-                                          <span className='m-list-timeline__badge' />
-                                          <span href='' className='m-list-timeline__text'>New order received
-                                            <span className='m-badge m-badge--danger m-badge--wide'>urgent</span>
-                                          </span>
-                                          <span className='m-list-timeline__time'>7 hrs</span>
-                                        </div>
-                                        <div className='m-list-timeline__item m-list-timeline__item--read'>
-                                          <span className='m-list-timeline__badge' />
-                                          <span className='m-list-timeline__text'>Production server down</span>
-                                          <span className='m-list-timeline__time'>3 hrs</span>
-                                        </div>
-                                        <div className='m-list-timeline__item'>
-                                          <span className='m-list-timeline__badge' />
-                                          <span className='m-list-timeline__text'>Production server up</span>
-                                          <span className='m-list-timeline__time'>5 hrs</span>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
                     <li className='m-nav__item m-topbar__quick-actions m-topbar__quick-actions--img m-dropdown m-dropdown--large m-dropdown--header-bg-fill m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push m-dropdown--mobile-full-width m-dropdown--skin-light' data-dropdown-toggle='click'>
-                      <a href='#' className='m-nav__link m-dropdown__toggle'>
+                      <a href="#" className='m-nav__link m-dropdown__toggle'>
                         <span className='m-nav__link-badge m-badge m-badge--dot m-badge--info m--hide' />
                         <span className='m-nav__link-icon'>
                           <i className='flaticon-share' />
@@ -181,24 +101,24 @@ class Header extends React.Component {
                               <div className='m-scrollable' data-scrollable='false' data-max-height='380' data-mobile-max-height='200'>
                                 <div className='m-nav-grid m-nav-grid--skin-light'>
                                   <div className='m-nav-grid__row'>
-                                    <a href='#' className='m-nav-grid__item'>
+                                    <Link to='/createbroadcast' className='m-nav-grid__item'>
                                       <i className='m-nav-grid__icon flaticon-file' />
                                       <span className='m-nav-grid__text'>Send New Broadcast</span>
-                                    </a>
-                                    <a href='#' className='m-nav-grid__item'>
+                                    </Link>
+                                    <Link to='/createpoll' className='m-nav-grid__item'>
                                       <i className='m-nav-grid__icon flaticon-time' />
                                       <span className='m-nav-grid__text'>Send New Poll</span>
-                                    </a>
+                                    </Link>
                                   </div>
                                   <div className='m-nav-grid__row'>
-                                    <a href='#' className='m-nav-grid__item'>
+                                    <Link to='/addsurvey' className='m-nav-grid__item'>
                                       <i className='m-nav-grid__icon flaticon-folder' />
                                       <span className='m-nav-grid__text'>Send New Survey</span>
-                                    </a>
-                                    <a href='#' className='m-nav-grid__item'>
+                                    </Link>
+                                    <Link to='/createworkflow' className='m-nav-grid__item'>
                                       <i className='m-nav-grid__icon flaticon-clipboard' />
                                       <span className='m-nav-grid__text'>Create New Workflow</span>
-                                    </a>
+                                    </Link>
                                   </div>
                                 </div>
                               </div>
