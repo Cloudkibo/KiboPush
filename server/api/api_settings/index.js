d701d9bd1df3d60a4c3f78569936c156880c272d
Sojharo <sojharo@live.com>
2017-11-27 11:49:16 +0500

    server work #1205

--- server/api/api_settings/index.js
+++ server/api/api_settings/index.js
@@ -8,7 +8,9 @@ const router = express.Router()
 const controller = require('./api_settings.controller')
 const auth = require('../../auth/auth.service')
 
+router.post('/', auth.isAuthenticated(), controller.index)
 router.post('/enable', auth.isAuthenticated(), controller.enable)
 router.post('/disable', auth.isAuthenticated(), controller.disable)
+router.post('/reset', auth.isAuthenticated(), controller.reset)
 
 module.exports = router
