7f08af1377072bb17ad2db1ff6856fb85f985d12
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2017-12-07 01:59:43 +0500

    Hello World

--- client/src/containers/subscriber/subscriber.js
+++ client/src/containers/subscriber/subscriber.js
@@ -36,10 +36,10 @@ class Subscriber extends React.Component {
     // addScript.setAttribute('src', '../../../js/theme-plugins.js')
     // document.body.appendChild(addScript)
     var addScript = document.createElement('script')
-    addScript.setAttribute('src', '../../../assets/demo/default/base/scripts.bundle.js')
+    addScript.setAttribute('src', '../../../assets/vendors/base/vendors.bundle.js')
     document.body.appendChild(addScript)
     addScript = document.createElement('script')
-    addScript.setAttribute('src', '../../../assets/vendors/base/vendors.bundle.js')
+    addScript.setAttribute('src', '../../../assets/demo/default/base/scripts.bundle.js')
     document.body.appendChild(addScript)
     document.title = 'KiboPush | Subscribers'
     // var datatable = $('#m_datatable').mDatatable({
