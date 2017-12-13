7fa8f5941d780c87f41f051757a2d2e8ef299f0a
sania <sania@cloudkibo.com>
2017-12-13 11:59:33 -0800

    Client- Fetch Info ...#757

--- client/src/redux/reducers/menu.reducer.js
+++ client/src/redux/reducers/menu.reducer.js
@@ -1,7 +1,21 @@
 import * as ActionTypes from '../constants/constants'
-export function menuInfo (state = {}, action) {
+const initialState = {
+  menuitems: []
+}
+export function menuInfo (state = initialState, action) {
   switch (action.type) {
     case ActionTypes.ADD_MENU_ITEM:
+      return Object.assign({}, state, {
+        menuitems: [...state.menuitems, action.data]
+      })
+    default:
+      return state
+  }
+}
+
+export function indexByPage (state = initialState, action) {
+  switch (action.type) {
+    case ActionTypes.UPDATE_INDEX_BY_PAGE:
       return Object.assign({}, state, {
         menuitems: [...state.menuitems, action.data]
       })
