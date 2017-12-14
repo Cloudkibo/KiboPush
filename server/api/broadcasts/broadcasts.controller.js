2bc7e6806708aa2f2767cb23dc71b1764011aa52
Sojharo <sojharo@live.com>
2017-12-14 19:58:35 +0500

    crash #1085

--- server/api/broadcasts/broadcasts.controller.js
+++ server/api/broadcasts/broadcasts.controller.js
@@ -627,57 +627,59 @@ function sendautomatedmsg (req, page) {
                 recipient: {id: req.sender.id}, // this is the subscriber id
                 message: messageData
               }
-              needle.post(
-                `https://graph.facebook.com/v2.6/me/messages?access_token=${response.body.access_token}`,
-                data, (err4, respp) => {
-                  logger.serverLog(TAG,
-                    `Sending workflow response to subscriber response ${JSON.stringify(
-                      respp.body)}`)
+              if (messageData.text !== undefined) {
+                needle.post(
+                  `https://graph.facebook.com/v2.6/me/messages?access_token=${response.body.access_token}`,
+                  data, (err4, respp) => {
+                    logger.serverLog(TAG,
+                      `Sending workflow response to subscriber response ${JSON.stringify(
+                        respp.body)}`)
 
-                  Subscribers.findOne({senderId: req.sender.id},
-                    (err, subscriber) => {
-                      if (err) return logger.serverLog(TAG, err)
-                      if (!subscriber) {
-                        return logger.serverLog(TAG,
-                          `No subscriber was found for workflow`)
-                      }
-                      Sessions.findOne({
-                        subscriber_id: subscriber._id,
-                        page_id: page._id,
-                        company_id: page.userId._id
-                      }, (err, session) => {
-                        if (err) {
+                    Subscribers.findOne({senderId: req.sender.id},
+                      (err, subscriber) => {
+                        if (err) return logger.serverLog(TAG, err)
+                        if (!subscriber) {
                           return logger.serverLog(TAG,
-                            `At get session ${JSON.stringify(err)}`)
+                            `No subscriber was found for workflow`)
                         }
-                        if (!session) {
-                          return logger.serverLog(TAG,
-                            `No chat session was found for workflow`)
-                        }
-                        const chatMessage = new LiveChat({
-                          sender_id: page._id, // this is the page id: _id of Pageid
-                          recipient_id: subscriber._id, // this is the subscriber id: _id of subscriberId
-                          sender_fb_id: page.pageId, // this is the (facebook) :page id of pageId
-                          recipient_fb_id: subscriber.senderId, // this is the (facebook) subscriber id : pageid of subscriber id
-                          session_id: session._id,
-                          company_id: page.userId._id, // this is admin id till we have companies
-                          payload: {
-                            componentType: 'text',
-                            text: messageData.text
-                          }, // this where message content will go
-                          status: 'unseen' // seen or unseen
-                        })
-                        chatMessage.save((err, chatMessageSaved) => {
+                        Sessions.findOne({
+                          subscriber_id: subscriber._id,
+                          page_id: page._id,
+                          company_id: page.userId._id
+                        }, (err, session) => {
                           if (err) {
                             return logger.serverLog(TAG,
-                              `At save chat${JSON.stringify(err)}`)
+                              `At get session ${JSON.stringify(err)}`)
                           }
-                          logger.serverLog(TAG,
-                            'Chat message saved for workflow sent')
+                          if (!session) {
+                            return logger.serverLog(TAG,
+                              `No chat session was found for workflow`)
+                          }
+                          const chatMessage = new LiveChat({
+                            sender_id: page._id, // this is the page id: _id of Pageid
+                            recipient_id: subscriber._id, // this is the subscriber id: _id of subscriberId
+                            sender_fb_id: page.pageId, // this is the (facebook) :page id of pageId
+                            recipient_fb_id: subscriber.senderId, // this is the (facebook) subscriber id : pageid of subscriber id
+                            session_id: session._id,
+                            company_id: page.userId._id, // this is admin id till we have companies
+                            payload: {
+                              componentType: 'text',
+                              text: messageData.text
+                            }, // this where message content will go
+                            status: 'unseen' // seen or unseen
+                          })
+                          chatMessage.save((err, chatMessageSaved) => {
+                            if (err) {
+                              return logger.serverLog(TAG,
+                                `At save chat${JSON.stringify(err)}`)
+                            }
+                            logger.serverLog(TAG,
+                              'Chat message saved for workflow sent')
+                          })
                         })
                       })
-                    })
-                })
+                  })
+              }
             }
           })
       }
