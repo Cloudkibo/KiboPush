532bff7e0786631c35e23122e92bdc89c295b5c2
anishachhatwani <i130315@nu.edu.pk>
2017-12-18 14:11:56 +0500

    send reply

--- client/src/containers/menu/CreateMessage.js
+++ client/src/containers/menu/CreateMessage.js
@@ -152,39 +152,39 @@ class CreateMessage extends React.Component {
   }
 
   handleImage (obj) {
-    var temp = this.state.message
-    var isPresent = false
-    temp.map((data) => {
-      if (data.id === obj.id) {
-        data = obj
-        isPresent = true
-      }
-    })
-
-    if (!isPresent) {
-      temp.push(obj)
-    }
-
-    this.setState({ message: temp })
-        // console.log("Image Uploaded", obj)
+    // var temp = this.state.message
+    // var isPresent = false
+    // temp.map((data) => {
+    //   if (data.id === obj.id) {
+    //     data = obj
+    //     isPresent = true
+    //   }
+    // })
+    //
+    // if (!isPresent) {
+    //   temp.push(obj)
+    // }
+    //  var temp = obj
+    this.setState({ message: obj })
+    console.log('Image Uploaded', this.state.message)
   }
 
   handleFile (obj) {
-    var temp = this.state.message
-    var isPresent = false
-    temp.map((data) => {
-      if (data.id === obj.id) {
-        data = obj
-        isPresent = true
-      }
-    })
+    // var temp = this.state.message
+    // var isPresent = false
+    // temp.map((data) => {
+    //   if (data.id === obj.id) {
+    //     data = obj
+    //     isPresent = true
+    //   }
+    // })
+    //
+    // if (!isPresent) {
+    //   temp.push(obj)
+    // }
 
-    if (!isPresent) {
-      temp.push(obj)
-    }
-
-    this.setState({ message: temp })
-        // console.log("Image Uploaded", obj)
+    this.setState({ message: obj })
+    console.log('Image Uploaded', this.state.message)
   }
 
   removeComponent (obj) {
