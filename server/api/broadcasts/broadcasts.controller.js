f69f8d6469ae57423f166090d78661a7675e8bb5
anishachhatwani <i130315@nu.edu.pk>
2017-12-19 16:50:05 +0500

    send reply

--- server/api/broadcasts/broadcasts.controller.js
+++ server/api/broadcasts/broadcasts.controller.js
@@ -428,9 +428,10 @@ function updateseenstatus (req) {
 
 function sendReply (req) {
   logger.serverLog(TAG, `Inside sendReply ${JSON.stringify(req)}`)
+  parsedData = JSON.parse(req.postback.payload)
+  req.postback.parsedData.forEach(payloadItem => {
   let messageData = utility.prepareSendAPIPayload(
-    req.sender.id,
-    JSON.parse(req.postback.payload))
+    req.sender.id, parsedData)
   logger.serverLog(TAG, `utility ${JSON.stringify(messageData)}`)
   Pages.find({pageId: req.recipient.id}, (err, pages) => {
     if (err) {
