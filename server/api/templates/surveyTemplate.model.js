3804d96c767bbf34fbb2fbbc4ae8a4ec73380d90
anishachhatwani <i130315@nu.edu.pk>
2017-12-26 14:54:52 +0500

    fix alert

--- server/api/templates/surveyTemplate.model.js
+++ server/api/templates/surveyTemplate.model.js
@@ -7,7 +7,8 @@ let Schema = mongoose.Schema
 const surveySchema = new Schema({
   title: String, // title of survey
   description: String, // description of survey
-  category: String
+  category: String,
+  datetime: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('surveyTemplate', surveySchema)
