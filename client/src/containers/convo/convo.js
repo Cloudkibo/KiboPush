393e26a614b5d5d8c832748ca5921988a3012718
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-01-16 06:31:32 +0500

    Change Text

--- client/src/containers/convo/convo.js
+++ client/src/containers/convo/convo.js
@@ -251,7 +251,7 @@ class Convo extends React.Component {
                               onClose={this.closeDialog}>
                               <ModalDialog style={{width: '500px'}}
                                 onClose={this.closeDialog}>
-                                <h3>Create Poll</h3>
+                                <h3>Create Broadcast</h3>
                                 <p>To create a new broadcast from scratch, click on Create New Broadcast. To use a template broadcast and modify it, click on Use Template</p>
                                 <div style={{width: '100%', textAlign: 'center'}}>
                                   <div style={{display: 'inline-block', padding: '5px'}}>
