48497261ae2a7d14c5e59c079970f12b3c299da3
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-01-18 05:32:51 +0500

    Added CompanyId

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -35,6 +35,19 @@ exports.index = function (req, res) {
     CompanyUsers.findOne({userId: req.user._id}, (err, companyUser) => {
       logger.serverLog(TAG,
         'Company Users Found ' + JSON.stringify(err) + JSON.stringify(companyUser))
+      if (err) {
+        return res.status(500).json({
+          status: 'failed',
+          description: `Internal Server Error ${JSON.stringify(err)}`
+        })
+      }
+      if (!companyUser) {
+        return res.status(404).json({
+          status: 'failed',
+          description: 'The user account does not belong to any company. Please contact support'
+        })
+      }
+      user.companyId = companyUser.companyId
     })
 
     res.status(200).json({status: 'success', payload: user})
