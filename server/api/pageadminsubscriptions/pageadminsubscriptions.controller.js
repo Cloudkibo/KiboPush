6651f6cc5a8fe3f287e30e02f1613fda51758bee
Sojharo <sojharo@live.com>
2018-01-19 11:18:32 +0500

    bug fixed

--- server/api/pageadminsubscriptions/pageadminsubscriptions.controller.js
+++ server/api/pageadminsubscriptions/pageadminsubscriptions.controller.js
@@ -1,11 +1,6 @@
 'use strict'
 
-var _ = require('lodash')
 var PageAdminSubscriptions = require('./pageadminsubscriptions.model')
-var department = require('../department/department.model')
-var configuration = require('../configuration/configuration.model')
-var User = require('../user/user.model')
-var logger = require('../../components/logger/logger')
 
 // Get list of companyprofiles
 exports.index = function (req, res) {
