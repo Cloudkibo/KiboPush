b6cc1202fb3ac527fe89d806e0e6fb3247a7f37f
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-01-22 05:56:41 +0500

    Basic Logging

--- server/api/broadcasts/broadcasts.controller.js
+++ server/api/broadcasts/broadcasts.controller.js
@@ -97,7 +97,7 @@ exports.getfbMessage = function (req, res) {
   logger.serverLog(TAG,
     `something received from facebook ${JSON.stringify(req.body)}`)
 
-   if(req.body.entry[0].messaging[0].message.quick_reply.payload){
+   if(req.body.entry[0]){
           // let resp = JSON.parse(event.message.quick_reply.payload)
           logger.serverLog(TAG, "Got a response to quick reply")
           // if(resp.poll_id){
