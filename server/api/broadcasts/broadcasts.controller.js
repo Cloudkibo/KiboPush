9d79644428e40a040f88233d8ada69b7f165665f
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-01-22 13:01:20 +0500

    Stepwise Check

--- server/api/broadcasts/broadcasts.controller.js
+++ server/api/broadcasts/broadcasts.controller.js
@@ -97,7 +97,7 @@ exports.getfbMessage = function (req, res) {
   logger.serverLog(TAG,
     `something received from facebook ${JSON.stringify(req.body)}`)
 
-   if(req.body.entry[0].messaging[0].message.quick_reply){
+   if(req.body.entry && req.body.entry[0].messaging[0] && req.body.entry[0].messaging[0].message && req.body.entry[0].messaging[0].message.quick_reply){
           let resp = JSON.parse(req.body.entry[0].messaging[0].message.quick_reply.payload)
           logger.serverLog(TAG, "Got a response to quick reply")
           if(resp.poll_id){
