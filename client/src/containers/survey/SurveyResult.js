de68fb363e4caf77291e8815426aa24b8cc02f8a
anishachhatwani <i130315@nu.edu.pk>
2018-01-22 17:34:57 +0500

    survey and poll report

--- client/src/containers/survey/SurveyResult.js
+++ client/src/containers/survey/SurveyResult.js
@@ -9,14 +9,19 @@ import Sidebar from '../../components/sidebar/sidebar'
 import Header from '../../components/header/header'
 import { loadsurveyresponses } from '../../redux/actions/surveys.actions'
 import Response from './Response'
+import json2csv from 'json2csv'
+import fileDownload from 'js-file-download'
 
+var responseData = []
 class SurveyResult extends React.Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      show: false
+      show: false,
     }
     this.getFile = this.getFile.bind(this)
+    this.sortData = this.sortData.bind(this)
+    this.exists = this.exists.bind(this)
   }
 
   componentDidMount () {
@@ -32,30 +37,70 @@ class SurveyResult extends React.Component {
   componentWillReceiveProps (nextprops) {
     this.setState({show: true})
   }
+  exists (newSubscriber) {
+    for (let i = 0; i < responseData.length; i++) {
+      if (responseData[i].subscriberId === newSubscriber) {
+        return true
+      }
+    }
+    return false
+  }
+  sortData (subscriber) {
+    var temp = {
+      subscriberId: subscriber,
+      q1: []
+    }
+    for (var i = 0; i < this.props.responses.length; i++) {
+      if (this.props.responses[i].subscriberId._id === subscriber) {
+        console.log('this.props.responses[i].response', this.props.responses[i].response)
+        temp.q1.push(this.props.responses[i].response)
+      }
+    }
+    responseData.push(temp)
+    return temp
+  }
   getFile () {
+    var jsonStructure = {}
+    var quesNo = 'Q'
     console.log('this.props.responses', this.props.responses)
+    console.log('this.props.questions', this.props.questions)
     let usersPayload = []
     for (let i = 0; i < this.props.responses.length; i++) {
-      usersPayload.push({
-        SurveyId: this.props.responses[i].surveyId._id,
-        PageId: this.props.responses[i].subscriberId.pageId,
-        SubscriberId: this.props.responses[i].subscriberId._id,
-        SubscriberName: this.props.responses[i].subscriberId.firstName + ' ' + this.props.responsesfull[i].subscriberId.lastName,
-        Q1: this.props.responsesfull[i].response,
-        DateTime: this.props.responsesfull[i].datetime
-      })
+      console.log('etes', this.exists(this.props.responses[i].subscriberId._id))
+      if (this.exists(this.props.responses[i].subscriberId._id) === false) {
+        var temp = this.sortData(this.props.responses[i].subscriberId._id)
+        jsonStructure = {
+          SurveyId: this.props.responses[i].surveyId._id,
+          PageId: this.props.responses[i].subscriberId.pageId,
+          SubscriberId: this.props.responses[i].subscriberId._id,
+          SubscriberName: this.props.responses[i].subscriberId.firstName + ' ' + this.props.responses[i].subscriberId.lastName,
+          Q1: temp.q1[0],
+          Q2: temp.q1[1]
+        }
+        if (temp.q1.length > 2) {
+          for (var k = 2; k < temp.q1.length; k++) {
+            quesNo = quesNo + (k + 1)
+            console.log('quesNo', quesNo)
+            jsonStructure['Q' + (k + 1)] = temp.q1[k]
+            quesNo = 'Q'
+          }
+        }
+        jsonStructure['DateTime'] = this.props.responses[i].datetime
+        usersPayload.push(jsonStructure)
+        console.log('this.sortData', usersPayload)
+      }
     }
-    // var info = usersPayload
-    // var keys = []
-    // var val = info[0]
-    //
-    // for (var j in val) {
-    //   var subKey = j
-    //   keys.push(subKey)
-    // }
-    // var data = json2csv({data: usersPayload, fields: keys})
-    // console.log('data', data)
-    // fileDownload(data, 'pollReport.csv')
+    var info = usersPayload
+    var keys = []
+    var val = info[0]
+
+    for (var j in val) {
+      var subKey = j
+      keys.push(subKey)
+    }
+    var data = json2csv({data: usersPayload, fields: keys})
+    console.log('data', data)
+    fileDownload(data, 'surveyReport.csv')
   }
   render () {
     return (
@@ -109,7 +154,7 @@ class SurveyResult extends React.Component {
                             onClick={() => this.gotoView()}>Back
                           </button>
                           {this.state.show &&
-                          <div className='pull-left' style={{display: 'inline-block', paddingTop: '40px', marginLeft: '15px'}} onClick={this.getFile}>
+                          <div className='pull-left' style={{display: 'inline-block', marginLeft: '15px'}} onClick={this.getFile}>
                             <div style={{display: 'inline-block', verticalAlign: 'middle'}}>
                               <label>Get data in CSV file: </label>
                             </div>
