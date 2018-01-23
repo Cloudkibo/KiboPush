a7f8885fbe5f4f4b297c0c00d410b5da73e365f4
anishachhatwani <i130315@nu.edu.pk>
2018-01-23 19:18:46 +0500

    categories

--- client/src/containers/templates/categories.js
+++ client/src/containers/templates/categories.js
@@ -188,13 +188,6 @@ class Category extends React.Component {
           className='m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body'>
           <Sidebar />
           <div className='m-grid__item m-grid__item--fluid m-wrapper' style={{height: 'fit-content'}}>
-            <div className='m-subheader '>
-              <div className='d-flex align-items-center'>
-                <div className='mr-auto'>
-                  <h3 className='m-subheader__title'>Templates</h3>
-                </div>
-              </div>
-            </div>
             <div className='m-content'>
               <div className='row'>
                 <div
