80c84b56da44aff32ad401782c89c57e7fa5b0d9
sania <sania@cloudkibo.com>
2018-01-24 14:11:03 -0800

    Preview Video #1646

--- client/src/containers/menu/Image.js
+++ client/src/containers/menu/Image.js
@@ -102,7 +102,7 @@ class Image extends React.Component {
           { this.state.showPreview &&
             <div style={{padding: '10px', marginTop: '40px'}}>
               <a href={this.state.imgSrc} target='_blank' download>
-                <h6 style={{marginTop: '10px'}}><i className='fa fa-file-image-o' /><strong>Download Image</strong></h6>
+                <h6><i className='fa fa-file-image-o' /><strong> Download Image </strong></h6>
               </a>
             </div>
           }
