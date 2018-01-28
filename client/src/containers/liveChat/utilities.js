d25d37a4765516bce0b13df6ce28295bbd15c0c7
Imran Shoukat <ishoukat@localhost.localdomain>
2018-01-29 03:23:55 +0500

    live chat bugs #1654

--- client/src/containers/liveChat/utilities.js
+++ client/src/containers/liveChat/utilities.js
@@ -71,12 +71,7 @@ export function displayDate (x) {
 
 export function validURL (str) {
   /* eslint-disable */
-  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-   '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-   '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-   '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-   '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-   '(\#[-a-z\d_]*)?$', 'i') // fragment locater
+  var pattern = /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
    /* eslint-enable */
   if (!pattern.test(str)) {
     return false
