3024d998799a537971a13defb51d7ecc73d4982b
anishachhatwani <i130315@nu.edu.pk>
2018-02-08 13:57:13 +0500

    phone lists

--- server/api/growthtools/growthtools.model.js
+++ server/api/growthtools/growthtools.model.js
@@ -7,7 +7,7 @@ let phoneNumberSchema = new Schema({
   userId: { type: Schema.ObjectId, ref: 'users' },
   companyId: { type: Schema.ObjectId, ref: 'companyprofile' },
   datetime: { type: Date, default: Date.now },
-  hasSubscribed: { type: Boolean }
+  hasSubscribed: { type: Boolean, default: false }
 })
 
 module.exports = mongoose.model('phoneNumber', phoneNumberSchema)
