949b8810b546eb1f01fa625597427b1699e77fd2
Baqar <bjafri5@gmail.com>
2018-02-13 10:37:36 +0500

    Added broadcast-component class

--- client/src/containers/convo/Image.js
+++ client/src/containers/convo/Image.js
@@ -73,13 +73,13 @@ class Image extends React.Component {
 
   render () {
     return (
-      <div>
+      <div className='broadcast-component'>
         <div onClick={() => { this.props.onRemove({id: this.props.id}) }} style={{ float: 'right', height: 20+'px', margin: -15+'px'}}>
           <span style={{cursor: 'pointer'}} className='fa-stack'>
             <i className='fa fa-times fa-stack-2x' />
           </span>
         </div>
-        <div className='broadcast-component ui-block hoverborder' style={{minHeight: 100, maxWidth: 400, padding: 25}}>
+        <div className='ui-block hoverborder' style={{minHeight: 100, maxWidth: 400, padding: 25}}>
           {
           this.state.loading
           ? <div className='align-center'><center><Halogen.RingLoader color='#FF5E3A' /></center></div>
