9e385695598e3843fc675c4d1cd6fd9ec7dbf762
anishachhatwani <i130315@nu.edu.pk>
2018-03-07 04:39:32 +0500

    issues

--- client/src/containers/convo/Text.js
+++ client/src/containers/convo/Text.js
@@ -98,8 +98,14 @@ class Text extends React.Component {
 
   render () {
     console.log('this.state.button', this.state.button)
+    let textStyles
+    if (this.props.removeState) {
+      textStyles = {marginBottom: 40 + 'px'}
+    } else {
+      textStyles = {marginBottom: 40 + 'px', width: '60%'}
+    }
     return (
-      <div className='broadcast-component' style={{marginBottom: 40 + 'px'}}>
+      <div className='broadcast-component' style={textStyles}>
         {this.props.removeState &&
           <div onClick={() => { this.props.onRemove({id: this.props.id}) }} style={{ float: 'right', height: 20 + 'px' }}>
             <span style={{cursor: 'pointer'}} className='fa-stack'>
