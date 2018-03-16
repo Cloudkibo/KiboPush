d590b9ba954ee60ba4adc3fdbe45d060e4788fe1
anishachhatwani <i130315@nu.edu.pk>
2018-03-16 11:34:44 +0500

    issues

--- client/src/containers/convo/editTemplate.js
+++ client/src/containers/convo/editTemplate.js
@@ -324,7 +324,11 @@ class EditTemplate extends React.Component {
     this.initTab()
     if (this.props.pages.length > 0) {
       console.log('componentDidMount pageValue set')
-      this.setState({pageValue: this.props.pages[0].pageId})
+      var temp = []
+      for (var j = 0; j < this.props.pages.length; j++) {
+        temp.push(this.props.pages[j].pageId)
+      }
+      this.setState({pageValue: temp})
     }
     var compProp = this.props
     var comp = this
@@ -543,7 +547,7 @@ class EditTemplate extends React.Component {
 
   testConvo () {
     console.log('in test convo')
-    var check = this.props.adminPageSubscription.filter((obj) => { return obj.pageId.pageId === this.state.pageValue })
+    var check = this.props.adminPageSubscription.filter((obj) => { return obj.pageId.pageId === this.state.pageValue[0] })
     console.log('Check', check)
     if (check.length <= 0) {
       this.setState({showMessengerModal: true})
@@ -997,7 +1001,7 @@ class EditTemplate extends React.Component {
                                         <h3>Connect to Messenger:</h3>
                                         <MessengerPlugin
                                           appId={this.props.fbAppId}
-                                          pageId={this.state.pageValue}
+                                          pageId={JSON.stringify(this.state.pageValue[0])}
                                           passthroughParams={this.props.user._id}
                                           onClick={() => { console.log('Click on Messenger'); this.setState({showMessengerModal: false}) }}
                                         />
