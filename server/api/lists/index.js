c990c0817ca136a7eb26ca77fd805e91beba117b
Sojharo <sojharo@live.com>
2018-03-19 14:50:37 +0500

    lists permissions removed

--- server/api/lists/index.js
+++ server/api/lists/index.js
@@ -13,33 +13,32 @@ const controller = require('./lists.controller')
 
 router.get('/allLists',
   auth.isAuthenticated(),
-  auth.doesPlanPermitsThisAction('customer_matching'),
-  auth.doesRolePermitsThisAction('customerMatchingPermission'),
+  // auth.doesPlanPermitsThisAction('customer_matching'),
+  // auth.doesRolePermitsThisAction('customerMatchingPermission'),
   controller.allLists)
-// todo this is also coded very badly
-//  router.get('/otherPages', auth.isAuthenticated(), controller.otherPages)
+
 // todo remove this, this is not being used, discuss
 router.post('/createList',
   auth.isAuthenticated(),
-  auth.doesPlanPermitsThisAction('customer_matching'),
-  auth.doesRolePermitsThisAction('customerMatchingPermission'),
+  // auth.doesPlanPermitsThisAction('customer_matching'),
+  // auth.doesRolePermitsThisAction('customerMatchingPermission'),
   controller.createList)
 
 router.post('/editList',
     auth.isAuthenticated(),
-    auth.doesPlanPermitsThisAction('customer_matching'),
-    auth.doesRolePermitsThisAction('customerMatchingPermission'),
+    // auth.doesPlanPermitsThisAction('customer_matching'),
+    // auth.doesRolePermitsThisAction('customerMatchingPermission'),
     controller.editList)
 
 router.get('/viewList/:id',
     auth.isAuthenticated(),
-    auth.doesPlanPermitsThisAction('customer_matching'),
-    auth.doesRolePermitsThisAction('customerMatchingPermission'),
+    // auth.doesPlanPermitsThisAction('customer_matching'),
+    // auth.doesRolePermitsThisAction('customerMatchingPermission'),
     controller.viewList)
 
 router.delete('/deleteList/:id',
     auth.isAuthenticated(),
-    auth.doesPlanPermitsThisAction('customer_matching'),
-    auth.doesRolePermitsThisAction('customerMatchingPermission'),
+    // auth.doesPlanPermitsThisAction('customer_matching'),
+    // auth.doesRolePermitsThisAction('customerMatchingPermission'),
     controller.deleteList)
 module.exports = router
