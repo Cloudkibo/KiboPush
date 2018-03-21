3dff209bf64d5200420eb48d021dd2d9f19a205d
anishachhatwani <i130315@nu.edu.pk>
2018-03-22 00:54:03 +0500

    issues

--- server/api/backdoor/backdoor.controller.js
+++ server/api/backdoor/backdoor.controller.js
@@ -1207,84 +1207,22 @@ exports.sendEmail = function (req, res) {
           {
             $match: {
               $and: [
-              {'datetime': {
-                $gte: new Date(
-                  (new Date().getTime() - (days * 24 * 60 * 60 * 1000))),
-                $lt: new Date(
-                  (new Date().getTime()))
-              }
-            }, {companyId: companyUser.companyId},
-          {isEnabledByPage: true}, {isSubscribed: true}]
-          }}
-        ], (err, subscribers) => {
-          if (err) {
-            logger.serverLog(TAG, `Error on fetching subscribers: ${err}`)
-          }
-          data.subscribers = subscribers.length
-          console.log('subscribers', subscribers.length)
-          //if (subscribers.length > 50) {
-          Polls.aggregate([
-            {
-              $match: {
-                $and: [
                 {'datetime': {
                   $gte: new Date(
                     (new Date().getTime() - (days * 24 * 60 * 60 * 1000))),
                   $lt: new Date(
                     (new Date().getTime()))
                 }
-              }, {companyId: companyUser.companyId}]
+              }, {companyId: companyUser.companyId},
+            {isEnabledByPage: true}, {isSubscribed: true}]
             }}
-          ], (err, polls) => {
+          ], (err, subscribers) => {
             if (err) {
-              return res.status(404).json({
-                status: 'failed',
-                description: `Error in getting surveys count ${JSON.stringify(err)}`
-              })
+              logger.serverLog(TAG, `Error on fetching subscribers: ${err}`)
             }
-            data.polls = polls.length
-          })
-          Surveys.aggregate([
-            {
-              $match: {
-                $and: [
-                {'datetime': {
-                  $gte: new Date(
-                    (new Date().getTime() - (days * 24 * 60 * 60 * 1000))),
-                  $lt: new Date(
-                    (new Date().getTime()))
-                }
-              }, {companyId: companyUser.companyId}]
-            }}
-          ], (err, surveys) => {
-            if (err) {
-              return res.status(404).json({
-                status: 'failed',
-                description: `Error in getting surveys count ${JSON.stringify(err)}`
-              })
-            }
-            data.surveys = surveys.length
-          })
-          Broadcasts.aggregate([
-            {
-              $match: {
-                $and: [
-                {'datetime': {
-                  $gte: new Date(
-                    (new Date().getTime() - (days * 24 * 60 * 60 * 1000))),
-                  $lt: new Date(
-                    (new Date().getTime()))
-                }
-              }, {companyId: companyUser.companyId}]
-            }}
-          ], (err, broadcasts) => {
-            if (err) {
-              return res.status(404).json({
-                status: 'failed',
-                description: `Error in getting surveys count ${JSON.stringify(err)}`
-              })
-            }
-            LiveChat.aggregate([
+            data.subscribers = subscribers.length
+            //if (subscribers.length > 50) {
+            Polls.aggregate([
               {
                 $match: {
                   $and: [
@@ -1296,52 +1234,111 @@ exports.sendEmail = function (req, res) {
                   }
                 }, {companyId: companyUser.companyId}]
               }}
-            ], (err, livechat) => {
+            ], (err, polls) => {
               if (err) {
                 return res.status(404).json({
                   status: 'failed',
                   description: `Error in getting surveys count ${JSON.stringify(err)}`
                 })
               }
-            data.liveChat = livechat.length
-            console.log('data', data)
-            let sendgrid = require('sendgrid')(config.sendgrid.username,
-              config.sendgrid.password)
-
-            let email = new sendgrid.Email({
-              to: user.email,
-              from: 'support@cloudkibo.com',
-              subject: 'KiboPush: Weekly Summary',
-              text: 'Welcome to KiboPush'
+              data.polls = polls.length
             })
-
-            email.setHtml(
-              '<body style="min-width: 80%;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;margin: 0;padding: 0;direction: ltr;background: #f6f8f1;width: 80% !important;"><table class="body", style="width:100%"> ' +
-              '<tr> <td class="center" align="center" valign="top"> <!-- BEGIN: Header --> <table class="page-header" align="center" style="width: 100%;background: #1f1f1f;"> <tr> <td class="center" align="center"> ' +
-              '<!-- BEGIN: Header Container --> <table class="container" align="center"> <tr> <td> <table class="row "> <tr>  </tr> </table> <!-- END: Logo --> </td> <td class="wrapper vertical-middle last" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;"> <!-- BEGIN: Social Icons --> <table class="six columns"> ' +
-              '<tr> <td> <table class="wrapper social-icons" align="right" style="float: right;"> <tr> <td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;"> ' +
-              '<p style="color: #ffffff">Weekly Summary</p> </td></tr> </table> </td> </tr> </table> ' +
-              '<!-- END: Social Icons --> </td> </tr> </table> </td> </tr> </table> ' +
-              '<!-- END: Header Container --> </td> </tr> </table> <!-- END: Header --> <!-- BEGIN: Content --> <table class="container content" align="center"> <tr> <td> <table class="row note"> ' +
-              '<tr> <td class="wrapper last"> <p> Hello ' + user.name + ', <br> Thank you for joining KiboPush. <br></p><p>Here is your weekly Summary <br> Subscribers: ' + data.subscribers + '<br> Live Chat: ' + data.liveChat + '<br> Broadcasts: ' + data.broadcasts + ' <br> Surveys: ' + data.surveys + ' <br> Polls: ' + data.polls + ' </p> <!-- BEGIN: Note Panel --> <table class="twelve columns" style="margin-bottom: 10px"> ' +
-              '<tr> <td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;"> </td> <td class="expander"> </td> </tr> </table> <!-- END: Note Panel --> </td> </tr> </table><span class="devider" style="border-bottom: 1px solid #eee;margin: 15px -15px;display: block;"></span> <!-- END: Disscount Content --> </td> </tr> </table> </td> </tr> </table> <!-- END: Content --> <!-- BEGIN: Footer --> <table class="page-footer" align="center" style="width: 100%;background: #2f2f2f;"> <tr> <td class="center" align="center" style="vertical-align: middle;color: #fff;"> <table class="container" align="center"> <tr> <td style="vertical-align: middle;color: #fff;"> <!-- BEGIN: Unsubscribet --> <table class="row"> <tr> <td class="wrapper last" style="vertical-align: middle;color: #fff;"><span style="font-size:12px;"><i>This ia a system generated email and reply is not required.</i></span> </td> </tr> </table> <!-- END: Unsubscribe --> ' +
-              '<!-- END: Footer Panel List --> </td> </tr> </table> </td> </tr> </table> <!-- END: Footer --> </td> </tr></table></body>')
-
-            // email.setHtml('<h1>KiboPush</h1><br><br>Use the following link to verify your account <br><br> <a href="https://app.kibopush.com/api/email_verification/verify/' + tokenString + '"> https://app.kibopush.com/api/email_verification/verify/' + tokenString + '</a>')
-
-            sendgrid.send(email, function (err, json) {
+            Surveys.aggregate([
+              {
+                $match: {
+                  $and: [
+                  {'datetime': {
+                    $gte: new Date(
+                      (new Date().getTime() - (days * 24 * 60 * 60 * 1000))),
+                    $lt: new Date(
+                      (new Date().getTime()))
+                  }
+                }, {companyId: companyUser.companyId}]
+              }}
+            ], (err, surveys) => {
               if (err) {
-                logger.serverLog(TAG,
-                  `Internal Server Error on sending email : ${JSON.stringify(
-                    err)}`)
+                return res.status(404).json({
+                  status: 'failed',
+                  description: `Error in getting surveys count ${JSON.stringify(err)}`
+                })
               }
-              // console.log(json);
+              data.surveys = surveys.length
             })
+            Broadcasts.aggregate([
+              {
+                $match: {
+                  $and: [
+                  {'datetime': {
+                    $gte: new Date(
+                      (new Date().getTime() - (days * 24 * 60 * 60 * 1000))),
+                    $lt: new Date(
+                      (new Date().getTime()))
+                  }
+                }, {companyId: companyUser.companyId}]
+              }}
+            ], (err, broadcasts) => {
+              if (err) {
+                return res.status(404).json({
+                  status: 'failed',
+                  description: `Error in getting surveys count ${JSON.stringify(err)}`
+                })
+              }
+              LiveChat.aggregate([
+                {
+                  $match: {
+                    $and: [
+                    {'datetime': {
+                      $gte: new Date(
+                        (new Date().getTime() - (days * 24 * 60 * 60 * 1000))),
+                      $lt: new Date(
+                        (new Date().getTime()))
+                    }
+                  }, {companyId: companyUser.companyId}]
+                }}
+              ], (err, livechat) => {
+                if (err) {
+                  return res.status(404).json({
+                    status: 'failed',
+                    description: `Error in getting surveys count ${JSON.stringify(err)}`
+                  })
+                }
+              data.liveChat = livechat.length
+              let sendgrid = require('sendgrid')(config.sendgrid.username,
+                config.sendgrid.password)
+
+              let email = new sendgrid.Email({
+                to: user.email,
+                from: 'support@cloudkibo.com',
+                subject: 'KiboPush: Weekly Summary',
+                text: 'Welcome to KiboPush'
+              })
+
+              email.setHtml(
+                '<body style="min-width: 80%;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;margin: 0;padding: 0;direction: ltr;background: #f6f8f1;width: 80% !important;"><table class="body", style="width:100%"> ' +
+                '<tr> <td class="center" align="center" valign="top"> <!-- BEGIN: Header --> <table class="page-header" align="center" style="width: 100%;background: #1f1f1f;"> <tr> <td class="center" align="center"> ' +
+                '<!-- BEGIN: Header Container --> <table class="container" align="center"> <tr> <td> <table class="row "> <tr>  </tr> </table> <!-- END: Logo --> </td> <td class="wrapper vertical-middle last" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;"> <!-- BEGIN: Social Icons --> <table class="six columns"> ' +
+                '<tr> <td> <table class="wrapper social-icons" align="right" style="float: right;"> <tr> <td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;"> ' +
+                '<p style="color: #ffffff">Weekly Summary</p> </td></tr> </table> </td> </tr> </table> ' +
+                '<!-- END: Social Icons --> </td> </tr> </table> </td> </tr> </table> ' +
+                '<!-- END: Header Container --> </td> </tr> </table> <!-- END: Header --> <!-- BEGIN: Content --> <table class="container content" align="center"> <tr> <td> <table class="row note"> ' +
+                '<tr> <td class="wrapper last"> <p> Hello ' + user.name + ', <br> Thank you for joining KiboPush. <br></p><p>Here is your weekly Summary <br> Subscribers: ' + data.subscribers + '<br> Live Chat: ' + data.liveChat + '<br> Broadcasts: ' + data.broadcasts + ' <br> Surveys: ' + data.surveys + ' <br> Polls: ' + data.polls + ' </p> <!-- BEGIN: Note Panel --> <table class="twelve columns" style="margin-bottom: 10px"> ' +
+                '<tr> <td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;"> </td> <td class="expander"> </td> </tr> </table> <!-- END: Note Panel --> </td> </tr> </table><span class="devider" style="border-bottom: 1px solid #eee;margin: 15px -15px;display: block;"></span> <!-- END: Disscount Content --> </td> </tr> </table> </td> </tr> </table> <!-- END: Content --> <!-- BEGIN: Footer --> <table class="page-footer" align="center" style="width: 100%;background: #2f2f2f;"> <tr> <td class="center" align="center" style="vertical-align: middle;color: #fff;"> <table class="container" align="center"> <tr> <td style="vertical-align: middle;color: #fff;"> <!-- BEGIN: Unsubscribet --> <table class="row"> <tr> <td class="wrapper last" style="vertical-align: middle;color: #fff;"><span style="font-size:12px;"><i>This ia a system generated email and reply is not required.</i></span> </td> </tr> </table> <!-- END: Unsubscribe --> ' +
+                '<!-- END: Footer Panel List --> </td> </tr> </table> </td> </tr> </table> <!-- END: Footer --> </td> </tr></table></body>')
+
+              // email.setHtml('<h1>KiboPush</h1><br><br>Use the following link to verify your account <br><br> <a href="https://app.kibopush.com/api/email_verification/verify/' + tokenString + '"> https://app.kibopush.com/api/email_verification/verify/' + tokenString + '</a>')
+
+              sendgrid.send(email, function (err, json) {
+                if (err) {
+                  logger.serverLog(TAG,
+                    `Internal Server Error on sending email : ${JSON.stringify(
+                      err)}`)
+                }
+              })
+            })
+            //}
           })
-          //}
         })
       })
-    })
     })
   })
   return res.status(200)
