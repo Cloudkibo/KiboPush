71b077ddb5a87c5803d0a8fc205afc7982dd6215
anishachhatwani <i130315@nu.edu.pk>
2018-03-22 01:35:29 +0500

    issues

--- server/api/URLforClickedCount/URL.controller.js
+++ server/api/URLforClickedCount/URL.controller.js
@@ -1,5 +1,6 @@
 const URL = require('./URL.model')
 const AutopostingMessages = require('./../autoposting_messages/autoposting_messages.model')
+const Broadcasts = require('./../broadcasts/broadcasts.model')
 
 exports.index = function (req, res) {
   URL.findOne({_id: req.params.id}, (err, URLObject) => {
@@ -10,9 +11,30 @@ exports.index = function (req, res) {
       })
     }
 
-    console.log('got this url object', URLObject)
-
     AutopostingMessages.update({_id: URLObject.module.id}, {$inc: {clicked: 1}}, (err, updatedData) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'failed',
+          description: `Internal Server Error ${JSON.stringify(err)}`
+        })
+      }
+
+      res.writeHead(301, {Location: URLObject.originalURL})
+      res.end()
+    })
+  })
+}
+
+exports.broadcast = function (req, res) {
+  URL.findOne({_id: req.params.id}, (err, URLObject) => {
+    if (err) {
+      return res.status(500).json({
+        status: 'failed',
+        description: `Internal Server Error ${JSON.stringify(err)}`
+      })
+    }
+
+    Broadcasts.update({_id: URLObject.module.id}, {$inc: {clicks: 1}}, (err, updatedData) => {
       if (err) {
         return res.status(500).json({
           status: 'failed',
