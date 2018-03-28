3e33ced6696f207b6f59289a40ec4ff055194644
anishachhatwani <i130315@nu.edu.pk>
2018-03-29 02:11:34 +0500

    issues

--- client/src/containers/teams/createTeam.js
+++ client/src/containers/teams/createTeam.js
@@ -7,13 +7,12 @@ import Sidebar from '../../components/sidebar/sidebar'
 import Header from '../../components/header/header'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Link } from 'react-router'
+import { browserHistory, Link } from 'react-router'
 import { getuserdetails } from '../../redux/actions/basicinfo.actions'
 import { loadMyPagesList } from '../../redux/actions/pages.actions'
 import { createTeam } from '../../redux/actions/teams.actions'
 import { loadMembersList } from '../../redux/actions/members.actions'
 import AlertContainer from 'react-alert'
-
 class CreateTeam extends React.Component {
   constructor (props, context) {
     super(props, context)
@@ -23,21 +22,62 @@ class CreateTeam extends React.Component {
       name: '',
       description: '',
       pageIds: [],
-      agentIds: []
+      agentIds: [],
+      showDropDown: false,
+      showDropDown1: false
     }
     this.createTeam = this.createTeam.bind(this)
     this.updateName = this.updateName.bind(this)
     this.updateDescription = this.updateDescription.bind(this)
+    this.showDropDown = this.showDropDown.bind(this)
+    this.hideDropDown = this.hideDropDown.bind(this)
+    this.showDropDown1 = this.showDropDown1.bind(this)
+    this.hideDropDown1 = this.hideDropDown1.bind(this)
+    this.changeAgent = this.changeAgent.bind(this)
+    this.changePage = this.changePage.bind(this)
+    this.removeAgent = this.removeAgent.bind(this)
+    this.removePage = this.removePage.bind(this)
+    this.exists = this.exists.bind(this)
+    this.existsPage = this.existsPage.bind(this)
+  }
+  showDropDown () {
+    this.setState({showDropDown: true})
   }
 
+  hideDropDown () {
+    this.setState({showDropDown: false})
+  }
+  showDropDown1 () {
+    this.setState({showDropDown1: true})
+  }
+
+  hideDropDown1 () {
+    this.setState({showDropDown1: false})
+  }
   componentDidMount () {
     this.scrollToTop()
   }
   componentWillReceiveProps (nextProps) {
-
+    console.log(nextProps, 'nextProps')
   }
   createTeam () {
-    this.props.createTeam({name: this.state.name, description: this.state.description, agentIds: this.state.agentIds, pageIds: this.state.pageIds})
+    if (this.state.name === '') {
+      this.msg.error('Please write a name')
+    } else if (this.state.description === '') {
+      this.msg.error('Please write a description')
+    } else if (this.state.agentIds.length === 0) {
+      this.msg.error('Please select one agent atleast')
+    } else if (this.state.pageIds.length === 0) {
+      this.msg.error('Please select one page atleast')
+    }
+    var agents = []
+    for (var i = 0; i < this.state.agentIds.length; i++) {
+      agents.push(this.state.agentIds[i]._id)
+    }
+    this.props.createTeam({name: this.state.name, description: this.state.description, agentIds: agents, pageIds: this.state.pageIds})
+    browserHistory.push({
+      pathname: `/teams`
+    })
   }
   updateDescription (e) {
     this.setState({description: e.target.value})
@@ -47,6 +87,66 @@ class CreateTeam extends React.Component {
   }
   scrollToTop () {
     this.top.scrollIntoView({behavior: 'instant'})
+  }
+  changeAgent (agent) {
+    var temp = this.state.agentIds
+    for (var i = 0; i < this.props.members.length; i++) {
+      if (agent === this.props.members[i].userId.name) {
+        temp.push(this.props.members[i])
+      }
+    }
+    this.setState({agentIds: temp})
+  }
+  removeAgent (agent) {
+    var index = -1
+    var temp = this.state.agentIds
+    for (var i = 0; i < this.state.agentIds.length; i++) {
+      if (agent === this.state.agentIds[i].userId.name) {
+        index = i
+      }
+    }
+    if (index > -1) {
+      temp.splice(index, 1)
+    }
+    this.setState({agentIds: temp})
+  }
+  changePage (page) {
+    var temp = this.state.pageIds
+    for (var i = 0; i < this.props.pages.length; i++) {
+      if (page === this.props.pages[i].pageName) {
+        temp.push(this.props.pages[i])
+      }
+    }
+    this.setState({pageIds: temp})
+  }
+  removePage (page) {
+    var index = -1
+    var temp = this.state.pageIds
+    for (var i = 0; i < this.state.pageIds.length; i++) {
+      if (page === this.state.pageIds[i].pageName) {
+        index = i
+      }
+    }
+    if (index > -1) {
+      temp.splice(index, 1)
+    }
+    this.setState({pageIds: temp})
+  }
+  exists (agent) {
+    for (var i = 0; i < this.state.agentIds.length; i++) {
+      if (this.state.agentIds[i].userId.name === agent) {
+        return true
+      }
+    }
+    return false
+  }
+  existsPage (page) {
+    for (var i = 0; i < this.state.pageIds.length; i++) {
+      if (this.state.pageIds[i].pageName === page) {
+        return true
+      }
+    }
+    return false
   }
   render () {
     // const { disabled, stayOpen } = this.state
@@ -93,16 +193,140 @@ class CreateTeam extends React.Component {
                              />
                         </div>
                       </div>
-                    </div>
-                    <div className='m-portlet__foot m-portlet__foot--fit' style={{'overflow': 'auto'}}>
-                      <div className='m-form__actions' style={{'float': 'right', 'marginTop': '25px', 'marginRight': '20px'}}>
-                        <button className='btn btn-primary' onClick={this.createTeam}> Create Team
-                        </button>
-                        <Link
-                          to='/poll'
-                          className='btn btn-secondary' style={{'marginLeft': '10px'}}>
-                          Cancel
-                        </Link>
+                      <br />
+                      <div className='row'>
+                        <div className='col-lg-8 col-md-8 col-sm-8'>
+                          <label>Agents:</label>
+                          {this.state.agentIds && this.state.agentIds.length > 0 &&
+                            <div>
+                              <ul className='list-unstyled'>
+                                {
+                                this.state.agentIds.map((agent, i) => (
+                                  <li key={agent.userId._id} className='m-nav__item'>
+                                    <span>
+                                      <img alt='pic' style={{height: '30px'}} src={(agent.userId.facebookInfo) ? agent.userId.facebookInfo.profilePic : 'icons/users.jpg'} />&nbsp;&nbsp;
+                                      <span>{agent.userId.name}</span>&nbsp;&nbsp;&nbsp;
+                                      <i className='fa fa-times' onClick={() => this.removeAgent(agent.userId.name)} />
+                                    </span>
+                                  </li>
+                                  ))
+                                }
+                              </ul>
+                            </div>
+                          }
+                          <br />
+                          <div className='m-dropdown m-dropdown--inline m-dropdown--arrow' data-dropdown-toggle='click' aria-expanded='true' onClick={this.showDropDown}>
+                            <a href='#' className='m-dropdown__toggle btn btn-success dropdown-toggle'>
+                            Add Agents
+                            </a>
+                            {
+                                this.state.showDropDown &&
+                                <div className='m-dropdown__wrapper'>
+                                  <span className='m-dropdown__arrow m-dropdown__arrow--left m-dropdown__arrow--adjust' />
+                                  <div className='m-dropdown__inner'>
+                                    <div className='m-dropdown__body'>
+                                      <div className='m-dropdown__content'>
+                                        <ul className='m-nav'>
+                                          <li className='m-nav__section m-nav__section--first'>
+                                            <span className='m-nav__section-text'>
+                                              Agents
+                                            </span>
+                                          </li>
+                                          {
+                                            this.props.members.map((member, i) => (
+                                              <li key={member._id} className='m-nav__item'>
+                                                <a onClick={() => this.changeAgent(member.userId.name)} className='m-nav__link' style={{cursor: 'pointer'}}>
+                                                  { this.exists(member.userId.name) === true
+                                                  ? <span style={{fontWeight: 600}} className='m-nav__link-text'>
+                                                    <i className='la la-check' /> {member.userId.name}
+                                                  </span>
+                                                  : <span className='m-nav__link-text'>
+                                                    {member.userId.name}
+                                                  </span>}
+                                                </a>
+                                              </li>
+                                            ))
+                                          }
+                                        </ul>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              }
+                          </div>
+                        </div>
+                        <div className='col-lg-4 col-md-4 col-sm-4'>
+                          <label>Assigned to Pages:</label>
+                          {this.state.pageIds && this.state.pageIds.length > 0 &&
+                            <div>
+                              <ul className='list-unstyled'>
+                                {
+                                this.state.pageIds.map((page, i) => (
+                                  <li key={page._id} className='m-nav__item'>
+                                    <span>
+                                      <img alt='pic' style={{height: '30px'}} src={(page.pagePic) ? page.pagePic : 'icons/users.jpg'} />&nbsp;&nbsp;
+                                      <span>{page.pagePic}</span>&nbsp;&nbsp;&nbsp;
+                                      <i className='fa fa-times' onClick={() => this.removePage(page.pagePic)} />
+                                    </span>
+                                  </li>
+                                  ))
+                                }
+                              </ul>
+                            </div>
+                          }
+                          <br />
+                          <div className='m-dropdown m-dropdown--inline m-dropdown--arrow' data-dropdown-toggle='click' aria-expanded='true' onClick={this.showDropDown1}>
+                            <a href='#' className='m-dropdown__toggle btn btn-success dropdown-toggle'>
+                            Add Pages
+                            </a>
+                            {
+                                this.state.showDropDown1 &&
+                                <div className='m-dropdown__wrapper'>
+                                  <span className='m-dropdown__arrow m-dropdown__arrow--left m-dropdown__arrow--adjust' />
+                                  <div className='m-dropdown__inner'>
+                                    <div className='m-dropdown__body'>
+                                      <div className='m-dropdown__content'>
+                                        <ul className='m-nav'>
+                                          <li className='m-nav__section m-nav__section--first'>
+                                            <span className='m-nav__section-text'>
+                                              Pages
+                                            </span>
+                                          </li>
+                                          {
+                                            this.props.pages.map((page, i) => (
+                                              <li key={page._id} className='m-nav__item'>
+                                                <a onClick={() => this.changePage(page.pageName)} className='m-nav__link' style={{cursor: 'pointer'}}>
+                                                  { this.existsPage(page.pageName) === true
+                                                  ? <span style={{fontWeight: 600}} className='m-nav__link-text'>
+                                                    <i className='la la-check' /> {page.pageName}
+                                                  </span>
+                                                  : <span className='m-nav__link-text'>
+                                                    {page.pageName}
+                                                  </span>}
+                                                </a>
+                                              </li>
+                                            ))
+                                          }
+                                        </ul>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              }
+                          </div>
+                        </div>
+                      </div>
+                      <br /><br />
+                      <div className='m-portlet__foot m-portlet__foot--fit' style={{'overflow': 'auto'}}>
+                        <div className='m-form__actions' style={{'float': 'right', 'marginTop': '25px', 'marginRight': '20px'}}>
+                          <button className='btn btn-primary' onClick={this.createTeam}> Create Team
+                          </button>
+                          <Link
+                            to='/teams'
+                            className='btn btn-secondary' style={{'marginLeft': '10px'}}>
+                            Cancel
+                          </Link>
+                        </div>
                       </div>
                     </div>
                   </div>
