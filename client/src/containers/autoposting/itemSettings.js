31514c6a9ffdd5abdb0b133297c8ebe5f56972d1
Baqar <bjafri5@gmail.com>
2018-04-02 18:43:28 +0500

    auto posting tags

--- client/src/containers/autoposting/itemSettings.js
+++ client/src/containers/autoposting/itemSettings.js
@@ -245,8 +245,8 @@ class ItemSettings extends React.Component {
     if (this.props.tags) {
       let optionsTag = []
       for (let i = 0; i < this.props.tags.length; i++) {
-        if (this.props.autopostingData.segmentationTags !== '') {
-          if (this.props.autopostingData.segmentationTags.indexOf(this.props.tags[i].tag) !== -1) {
+        if (this.props.location.state.item.segmentationTags !== '') {
+          if (this.props.location.state.item.segmentationTags.indexOf(this.props.tags[i].tag) !== -1) {
             optionsTag[i] = {text: this.props.tags[i].tag, id: this.props.tags[i].tag, selected: true}
           } else {
             optionsTag[i] = {text: this.props.tags[i].tag, id: this.props.tags[i].tag}
