5b1327aad6b9651ec19dec13e5b2641e95659206
anishachhatwani <i130315@nu.edu.pk>
2018-04-04 03:03:18 +0500

    issues

---
+++ client/src/redux/actions/notifications.actions.js
@@ -0,0 +1,22 @@
+import * as ActionTypes from '../constants/constants'
+import callApi from '../../utility/api.caller.service'
+export const API_URL = '/api'
+
+export function showNotifications (data) {
+  return {
+    type: ActionTypes.SHOW_NOTIFICATIONS,
+    data
+  }
+}
+
+export function fetchNotifications () {
+  console.log('fetchNotifications')
+  return (dispatch) => {
+    callApi('notifications').then(res => {
+      console.log('response from notifications', res)
+      if (res.status === 'success') {
+        dispatch(showNotifications(res.payload.notifications))
+      }
+    })
+  }
+}
