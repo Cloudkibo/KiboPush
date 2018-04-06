5f380e4c72a12a7b6fb588a035beefe4b369d76a
anishachhatwani <i130315@nu.edu.pk>
2018-04-06 13:04:47 +0500

    issues

---
+++ client/src/containers/smart_replies/editBot.js
@@ -0,0 +1,223 @@
+/**
+ * Created by sojharo on 20/07/2017.
+ */
+
+import React from 'react'
+import Sidebar from '../../components/sidebar/sidebar'
+import Header from '../../components/header/header'
+import { connect } from 'react-redux'
+import {
+  editWorkFlow,
+  loadWorkFlowList,
+  clearAlertMessages
+} from '../../redux/actions/workflows.actions'
+import { bindActionCreators } from 'redux'
+import { Link } from 'react-router'
+import { Alert } from 'react-bs-notifier'
+
+class EditWorkflow extends React.Component {
+  constructor (props) {
+    super(props)
+    this.gotoWorkflow = this.gotoWorkflow.bind(this)
+    this.changeCondition = this.changeCondition.bind(this)
+    this.changeKeywords = this.changeKeywords.bind(this)
+    this.changeReply = this.changeReply.bind(this)
+    this.changeActive = this.changeActive.bind(this)
+    this.state = {
+      condition: props.location.state.condition,
+      keywords: props.location.state.keywords,
+      reply: props.location.state.reply,
+      isActive: props.location.state.isActive === true ? 'Yes' : 'No',
+      alertMessage: '',
+      alertType: ''
+    }
+  }
+
+  componentDidMount () {
+    document.title = 'KiboPush | Edit Workflow'
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.successMessage) {
+      this.setState({
+        alertMessage: nextProps.successMessage,
+        alertType: 'success'
+      })
+    } else if (nextProps.errorMessage) {
+      this.setState({
+        alertMessage: nextProps.errorMessage,
+        alertType: 'danger'
+      })
+    } else {
+      this.setState({
+        alertMessage: '',
+        alertType: ''
+      })
+    }
+  }
+
+  gotoWorkflow () {
+    this.props.editWorkFlow({
+      condition: this.state.condition,
+      keywords: this.state.keywords,
+      reply: this.state.reply,
+      isActive: this.state.isActive,
+      _id: this.props.location.state._id
+    })
+  }
+
+  changeCondition (event) {
+    this.setState({condition: event.target.value})
+  }
+
+  changeKeywords (event) {
+    this.setState({keywords: event.target.value.split(',')})
+  }
+
+  changeReply (event) {
+    this.setState({reply: event.target.value})
+  }
+
+  changeActive (event) {
+    this.setState({isActive: event.target.value})
+  }
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <div
+          className='m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body'>
+          <Sidebar />
+          <div className='m-grid__item m-grid__item--fluid m-wrapper'>
+            <div className='m-subheader '>
+              <div className='d-flex align-items-center'>
+                <div className='mr-auto'>
+                  <h3 className='m-subheader__title'>Edit Workflow</h3>
+                </div>
+              </div>
+            </div>
+            <div className='m-content'>
+              <div className='m-portlet m-portlet--mobile'>
+                <div className='m-portlet__head'>
+                  <div className='m-portlet__head-caption'>
+                    <div className='m-portlet__head-title'>
+                      <h3 className='m-portlet__head-text'>
+                        Form
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+                <form className='m-form m-form--label-align-right'>
+                  <div className='m-portlet__body'>
+                    <div className='m-form__section m-form__section--first'>
+                      <div className='form-group m-form__group row'>
+                        <label className='col-lg-2 col-form-label'>
+                          Rule
+                        </label>
+                        <div className='col-lg-6' id='rules'>
+                          <div className='col-10'>
+                            <select className='form-control m-input' onChange={this.changeCondition}
+                              value={this.state.condition}>
+                              <option value='message_is'>Message is</option>
+                              <option value='message_contains'>Message Contains</option>
+                              <option value='message_begins'>Message Begins with</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='form-group m-form__group row'>
+                        <label className='col-lg-2 col-form-label'>
+                          Keywords (separated by comma)
+                        </label>
+                        <div className='col-lg-6'>
+                          <input className='form-control m-input'
+                            onChange={this.changeKeywords}
+                            value={this.state.keywords}
+                            id='keywords'
+                            placeholder='hi,hello,hey' />
+                        </div>
+                      </div>
+                      <div className='form-group m-form__group row'>
+                        <label className='col-lg-2 col-form-label'>
+                          Reply
+                        </label>
+                        <div className='col-lg-6'>
+                          <textarea className='form-control m-input'
+                            onChange={this.changeReply}
+                            value={this.state.reply} rows='5'
+                            id='exampleInputReply' />
+                        </div>
+                      </div>
+                      <div className='form-group m-form__group row'>
+                        <label className='col-lg-2 col-form-label'>
+                          Is Active?
+                        </label>
+                        <div className='col-lg-6'>
+                          <select className='form-control m-input' onChange={this.changeActive}
+                            value={this.state.isActive} id='isActive'>
+                            <option value='Yes'>Yes</option>
+                            <option value='No'>No</option>
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='m-portlet__foot m-portlet__foot--fit'>
+                    <div className='m-form__actions m-form__actions'>
+                      <div className='row'>
+                        <div className='col-lg-2' />
+                        <div className='col-lg-6'>
+                          <button className='btn btn-primary' type='button' onClick={this.gotoWorkflow} >
+                            Save Changes
+                          </button>
+                          <span>&nbsp;&nbsp;</span>
+                          <Link to='workflows'>
+                            <button className='btn btn-secondary'>
+                              Back
+                            </button>
+                          </Link>
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <span>&nbsp;&nbsp;</span>
+                      </div>
+                      <div className='row'>
+                        <div className='col-lg-2' />
+                        <div className='col-lg-6'>
+                          {
+                            this.state.alertMessage !== '' &&
+                            <center>
+                              <Alert type={this.state.alertType}>
+                                {this.state.alertMessage}
+                              </Alert>
+                            </center>
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    workflows: (state.workflowsInfo.workflows),
+    successMessage: (state.workflowsInfo.successMessageEdit),
+    errorMessage: (state.workflowsInfo.errorMessageEdit)
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(
+    {loadWorkFlowList: loadWorkFlowList, editWorkFlow: editWorkFlow, clearAlertMessages: clearAlertMessages}, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(EditWorkflow)
