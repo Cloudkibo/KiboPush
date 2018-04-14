8c603cdee6896ce98e2edcb3f769aeb4800291bc
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-04-15 02:17:58 +0500

    Updated the bot model

--- server/api/smart_replies/Bots.model.js
+++ server/api/smart_replies/Bots.model.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 
 const botSchema = new Schema({
-  pageId: {type: Schema.ObjectId, ref: 'pages'}, // TODO ENUMS
+  pageId: {type: String, ref: 'pages'}, // TODO ENUMS
   userId: {type: Schema.ObjectId, ref: 'users'},
   botName: String,
   witAppId: String,
