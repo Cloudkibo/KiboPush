49e74adba61ff71f862676754948616915ad9c19
Sojharo <sojharo@live.com>
2018-04-16 00:20:52 +0500

    azure

--- server/config/environment/staging.js
+++ server/config/environment/staging.js
@@ -8,8 +8,7 @@
  module.exports = {
    // MongoDB connection options
    mongo: {
-     uri: 'mongodb://kibopush:4Pf8D9uXqNcan8Ymp0u9tRD3pHhmJ3xa9RHIC6g4GRG2QmgrGd7X8ghN6fgAb488l3Y6ymZelAjV8Lf3FWPWVw==@kibopush.documents.azure.com:10255/kibopush?ssl=true'
-     //uri: 'mongodb://localhost/kibopush-staging'
+     uri: 'mongodb://localhost/kibopush-staging'
    },
    seedDB: false,
 
