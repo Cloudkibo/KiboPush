bfa8b77f054df2916daff535d1dcfcd29311d2c5
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-04-16 19:18:41 +0500

    Bot Object from array

--- server/api/smart_replies/bots.controller.js
+++ server/api/smart_replies/bots.controller.js
@@ -112,7 +112,7 @@ exports.details = function (req, res) {
       }
         logger.serverLog(TAG,
               `returning Bot details ${JSON.stringify(bot)}`)
-      return res.status(200).json({status: 'success', payload: bot})
+      return res.status(200).json({status: 'success', payload: bot[0]})
     });
 }
 
