119bd52cec26497d4da4c02cf096048f978c92ca
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-04-22 23:24:56 +0500

    Auto Generated

--- server/api/smart_replies/bots.controller.js
+++ server/api/smart_replies/bots.controller.js
@@ -65,7 +65,7 @@ function getWitResponse(message, token, bot, pageId, senderId){
 function sendMessenger(message, pageId, senderId){
   let messageData = utility.prepareSendAPIPayload(
                 senderId,
-                 {"componentType":"text","text": message} , true)
+                 {"componentType":"text","text": message + "  (This is an auto-generated message)"} , true)
       Pages.findOne({pageId: pageId}, (err, page) => {
           if (err) {
             logger.serverLog(TAG, `ERROR ${JSON.stringify(err)}`)
