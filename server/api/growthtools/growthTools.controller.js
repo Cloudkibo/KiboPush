72f7bdda793c9655bc98e1720adb33019f1a10c5
anishachhatwani <i130315@nu.edu.pk>
2018-04-26 01:49:12 +0500

    issues

--- server/api/growthtools/growthTools.controller.js
+++ server/api/growthtools/growthTools.controller.js
@@ -16,8 +16,6 @@ const CompanyUsers = require('./../companyuser/companyuser.model')
 let request = require('request')
 const _ = require('lodash')
 const Lists = require('../lists/lists.model')
-let config = require('./../../config/environment')
-const csvToJson = require('csvtojson')
 exports.upload = function (req, res) {
   var today = new Date()
   var uid = crypto.randomBytes(5).toString('hex')
@@ -46,11 +44,6 @@ exports.upload = function (req, res) {
           description: 'internal server error' + JSON.stringify(err)
         })
       }
-      const csvFilePath = dir + '/userfiles' + serverPath
-      csvToJson().fromFile(csvFilePath).on('json', (jsonObj) => {
-        logger.serverLog(TAG, `data from csv file ${JSON.stringify(jsonObj)}`)
-        logger.serverLog(TAG, `data from csv file ${JSON.stringify(jsonObj[0])}`)
-      })
       let respSent = false
       fs.createReadStream(dir + '/userfiles' + serverPath)
         .pipe(csv())
@@ -65,7 +58,7 @@ exports.upload = function (req, res) {
             return res.status(201)
               .json({
                 status: 'success',
-                payload: { columns: keys, file: dir + '/userfiles' + serverPath }
+                payload: { fileColumns: keys, file: dir + '/userfiles' + serverPath }
               })
           } else {
             return res.status(404)
@@ -74,24 +67,7 @@ exports.upload = function (req, res) {
         })
     })
 }
-/*exports.upload = function (req, res) {
-  var today = new Date()
-  var uid = crypto.randomBytes(5).toString('hex')
-  var serverPath = 'f' + uid + '' + today.getFullYear() + '' +
-    (today.getMonth() + 1) + '' + today.getDate()
-  serverPath += '' + today.getHours() + '' + today.getMinutes() + '' +
-    today.getSeconds()
-  let fext = req.files.file.name.split('.')
-  serverPath += '.' + fext[fext.length - 1]
-
-  let dir = path.resolve(__dirname, '../../../broadcastFiles/')
-
-  if (req.files.file.size === 0) {
-    return res.status(400).json({
-      status: 'failed',
-      description: 'No file submitted'
-    })
-  }
+exports.sendMessage = function (req, res) {
   CompanyUsers.findOne({domain_email: req.user.domain_email}, (err, companyUser) => {
     if (err) {
       return res.status(500).json({
@@ -105,7 +81,7 @@ exports.upload = function (req, res) {
         description: 'The user account does not belong to any company. Please contact support'
       })
     }
-    let newFileName = req.files.file.name.substring(0, req.files.file.name.indexOf('.'))
+    let newFileName = req.body.fileName(0, req.body.fileName.indexOf('.'))
 
     Lists.update({initialList: true, userId: req.user._id, companyId: companyUser.companyId, listName: newFileName}, {
       listName: newFileName,
@@ -121,181 +97,164 @@ exports.upload = function (req, res) {
         })
       }
     })
-    fs.rename(
-      req.files.file.path,
-      dir + '/userfiles' + serverPath,
-      err => {
-        if (err) {
-          return res.status(500).json({
-            status: 'failed',
-            description: 'internal server error' + JSON.stringify(err)
-          })
-        }
-        let respSent = false
-        fs.createReadStream(dir + '/userfiles' + serverPath)
-          .pipe(csv())
-          .on('data', function (data) {
-            if (data.phone_numbers && data.names) {
-              var result = data.phone_numbers.replace(/[- )(]/g, '')
-              // var savePhoneNumber = new PhoneNumber({
-              //   name: data.name,
-              //   number: result,
-              //   userId: req.user._id
-              // })
-              PhoneNumber.find({number: result, userId: req.user._id, companyId: companyUser.companyId, pageId: req.body._id}, (err2, phone) => {
-                if (err2) {
+    let respSent = false
+    fs.createReadStream(req.body.path)
+      .pipe(csv())
+      .on('data', function (data) {
+        var result = data.req.body.phone_numbers.replace(/[- )(]/g, '')
+        // var savePhoneNumber = new PhoneNumber({
+        //   name: data.req.body.names,
+        //   number: result,
+        //   userId: req.user._id
+        // })
+        PhoneNumber.find({number: result, userId: req.user._id, companyId: companyUser.companyId, pageId: req.body._id}, (err2, phone) => {
+          if (err2) {
+            return res.status(500).json({
+              status: 'failed',
+              description: 'phone number create failed'
+            })
+          }
+          if (phone.length === 0) {
+            let phoneNumber = new PhoneNumber({
+              name: data.names,
+              number: result,
+              userId: req.user._id,
+              companyId: companyUser.companyId,
+              pageId: req.body._id,
+              fileName: newFileName,
+              hasSubscribed: false
+            })
+            phoneNumber.save((err2) => {
+              if (err2) {
+                logger.serverLog(TAG, {
+                  status: 'failed',
+                  description: 'PollBroadcast create failed',
+                  err2
+                })
+              }
+            })
+          } else {
+            let filename = []
+            for (let i = 0; i < phone[0].fileName.length; i++) {
+              filename.push(phone[0].fileName[i])
+            }
+            if (exists(filename, req.body.fileName) === false) {
+              filename.push(newFileName)
+            }
+            PhoneNumber.update({number: result, userId: req.user._id, companyId: companyUser.companyId, pageId: req.body._id}, {
+              name: data.names,
+              number: result,
+              userId: req.user._id,
+              companyId: companyUser.companyId,
+              pageId: req.body._id,
+              fileName: filename
+            }, {upsert: true}, (err2, phonenumbersaved) => {
+              if (err2) {
+                return res.status(500).json({
+                  status: 'failed',
+                  description: 'phone number create failed'
+                })
+              }
+              PhoneNumber.find({companyId: companyUser.companyId, hasSubscribed: true, fileName: newFileName}, (err, number) => {
+                if (err) {
                   return res.status(500).json({
                     status: 'failed',
-                    description: 'phone number create failed'
+                    description: 'phone number not found'
                   })
                 }
-                if (phone.length === 0) {
-                  let phoneNumber = new PhoneNumber({
-                    name: data.names,
-                    number: result,
-                    userId: req.user._id,
-                    companyId: companyUser.companyId,
-                    pageId: req.body._id,
-                    fileName: newFileName,
-                    hasSubscribed: false
-                  })
-                  phoneNumber.save((err2) => {
-                    if (err2) {
-                      logger.serverLog(TAG, {
-                        status: 'failed',
-                        description: 'PollBroadcast create failed',
-                        err2
-                      })
+                if (number.length > 0) {
+                  let findNumber = []
+                  let findPage = []
+                  for (let a = 0; a < number.length; a++) {
+                    findNumber.push(number[a].number)
+                    findPage.push(number[a].pageId)
+                  }
+                  let subscriberFindCriteria = {isSubscribedByPhoneNumber: true, companyId: companyUser.companyId, isSubscribed: true}
+                  subscriberFindCriteria = _.merge(subscriberFindCriteria, {
+                    phoneNumber: {
+                      $in: findNumber
+                    },
+                    pageId: {
+                      $in: findPage
                     }
                   })
-                } else {
-                  let filename = []
-                  for (let i = 0; i < phone[0].fileName.length; i++) {
-                    filename.push(phone[0].fileName[i])
-                  }
-                  if (exists(filename, req.files.file.name) === false) {
-                    filename.push(newFileName)
-                  }
-                  PhoneNumber.update({number: result, userId: req.user._id, companyId: companyUser.companyId, pageId: req.body._id}, {
-                    name: data.names,
-                    number: result,
-                    userId: req.user._id,
-                    companyId: companyUser.companyId,
-                    pageId: req.body._id,
-                    fileName: filename
-                  }, {upsert: true}, (err2, phonenumbersaved) => {
-                    if (err2) {
+                  Subscribers.find(subscriberFindCriteria).populate('pageId').exec((err, subscribers) => {
+                    if (err) {
                       return res.status(500).json({
                         status: 'failed',
-                        description: 'phone number create failed'
+                        description: `Internal Server Error ${JSON.stringify(err)}`
                       })
                     }
-                    PhoneNumber.find({companyId: companyUser.companyId, hasSubscribed: true, fileName: newFileName}, (err, number) => {
+                    let temp = []
+                    for (let i = 0; i < subscribers.length; i++) {
+                      temp.push(subscribers[i]._id)
+                    }
+                    Lists.update({listName: newFileName, userId: req.user._id, companyId: companyUser.companyId}, {
+                      content: temp
+                    }, (err2, savedList) => {
                       if (err) {
                         return res.status(500).json({
                           status: 'failed',
-                          description: 'phone number not found'
-                        })
-                      }
-                      if (number.length > 0) {
-                        let findNumber = []
-                        let findPage = []
-                        for (let a = 0; a < number.length; a++) {
-                          findNumber.push(number[a].number)
-                          findPage.push(number[a].pageId)
-                        }
-                        let subscriberFindCriteria = {isSubscribedByPhoneNumber: true, companyId: companyUser.companyId, isSubscribed: true}
-                        subscriberFindCriteria = _.merge(subscriberFindCriteria, {
-                          phoneNumber: {
-                            $in: findNumber
-                          },
-                          pageId: {
-                            $in: findPage
-                          }
-                        })
-                        Subscribers.find(subscriberFindCriteria).populate('pageId').exec((err, subscribers) => {
-                          if (err) {
-                            return res.status(500).json({
-                              status: 'failed',
-                              description: `Internal Server Error ${JSON.stringify(err)}`
-                            })
-                          }
-                          let temp = []
-                          for (let i = 0; i < subscribers.length; i++) {
-                            temp.push(subscribers[i]._id)
-                          }
-                          Lists.update({listName: newFileName, userId: req.user._id, companyId: companyUser.companyId}, {
-                            content: temp
-                          }, (err2, savedList) => {
-                            if (err) {
-                              return res.status(500).json({
-                                status: 'failed',
-                                description: `Internal Server Error ${JSON.stringify(err)}`
-                              })
-                            }
-                          })
+                          description: `Internal Server Error ${JSON.stringify(err)}`
                         })
                       }
                     })
                   })
                 }
               })
+            })
+          }
+        })
 
-              let pagesFindCriteria = {userId: req.user._id, connected: true, pageId: req.body.pageId}
-              Pages.find(pagesFindCriteria, (err, pages) => {
-                if (err) {
-                  logger.serverLog(TAG, `Error ${JSON.stringify(err)}`)
-                }
-                pages.forEach(page => {
-                  let messageData = {
-                    'messaging_type': 'UPDATE',
-                    'recipient': JSON.stringify({
-                      'phone_number': result
-                    }),
-                    'message': JSON.stringify({
-                      'text': req.body.text,
-                      'metadata': 'This is a meta data'
-                    })
-                  }
-                  request(
-                    {
-                      'method': 'POST',
-                      'json': true,
-                      'formData': messageData,
-                      'uri': 'https://graph.facebook.com/v2.6/me/messages?access_token=' +
-                      page.accessToken
-                    },
-                    function (err, res) {
-                      if (err) {
-                        return logger.serverLog(TAG,
-                          `At invite to messenger using phone ${JSON.stringify(
-                            err)}`)
-                      } else {
-                      }
-                    })
-                })
+        let pagesFindCriteria = {userId: req.user._id, connected: true, pageId: req.body.pageId}
+        Pages.find(pagesFindCriteria, (err, pages) => {
+          if (err) {
+            logger.serverLog(TAG, `Error ${JSON.stringify(err)}`)
+          }
+          pages.forEach(page => {
+            let messageData = {
+              'messaging_type': 'UPDATE',
+              'recipient': JSON.stringify({
+                'phone_number': result
+              }),
+              'message': JSON.stringify({
+                'text': req.body.text,
+                'metadata': 'This is a meta data'
               })
-
-              if (respSent === false) {
-                respSent = true
-                return res.status(201)
-                  .json({
-                    status: 'success',
-                    description: 'Contacts were invited to your messenger'
-                  })
-              }
-            } else {
-              return res.status(404)
-                .json({status: 'failed', description: 'Incorrect column names'})
             }
+            request(
+              {
+                'method': 'POST',
+                'json': true,
+                'formData': messageData,
+                'uri': 'https://graph.facebook.com/v2.6/me/messages?access_token=' +
+                page.accessToken
+              },
+              function (err, res) {
+                if (err) {
+                  return logger.serverLog(TAG,
+                    `At invite to messenger using phone ${JSON.stringify(
+                      err)}`)
+                } else {
+                }
+              })
           })
-          .on('end', function () {
-            fs.unlinkSync(dir + '/userfiles' + serverPath)
-          })
+        })
+
+        if (respSent === false) {
+          respSent = true
+          return res.status(201)
+            .json({
+              status: 'success',
+              description: 'Contacts were invited to your messenger'
+            })
+        }
+      })
+      .on('end', function () {
+        fs.unlinkSync(req.body.path)
       })
   })
 }
-*/
 exports.sendNumbers = function (req, res) {
   let parametersMissing = false
 
