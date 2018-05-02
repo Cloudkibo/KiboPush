2bfb2ef77405df24b4ae4b7524987adeb644cd9c
Smat26 <ahsan.tanweer.26@gmail.com>
2018-05-03 03:35:12 +0500

    Fixing all sidebar icons

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -412,7 +412,7 @@ class Sidebar extends Component {
               {this.props.user && this.state.phoneNumber && this.props.user.plan.customer_matching &&
               <li className='m-menu__item  m-menu__item--submenu' aria-haspopup='true' data-menu-submenu-toggle='hover'>
                 <Link to='/customerMatchingUsingPhNum' className='m-menu__link m-menu__toggle'>
-                  <i className='m-menu__link-icon flaticon-support' />
+                  <i className='m-menu__link-icon flaticon-list-3' />
                   <span className='m-menu__link-text'>Invite using phone number</span>
                 </Link>
               </li>
