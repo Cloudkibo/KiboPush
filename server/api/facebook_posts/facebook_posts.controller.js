b581907c60f14e1c5d2f70815517036a7205052d
anishachhatwani <i130315@nu.edu.pk>
2018-05-20 22:05:04 +0500

    issues

--- server/api/facebook_posts/facebook_posts.controller.js
+++ server/api/facebook_posts/facebook_posts.controller.js
@@ -140,10 +140,11 @@ exports.create = function (req, res) {
                   messageData.url = payloadItem.url
                 } else if (payloadItem.componentType === 'video') {
                   //  let dir = path.resolve(__dirname, '../../../broadcastFiles/userfiles')
-                  let dir = path.resolve(__dirname, '../../../broadcastFiles/userfiles')
+                  //  let dir = path.resolve(__dirname, '../../../broadcastFiles/userfiles')
                   //  let fileReaderStream = fs.createReadStream(dir + '/' + payloadItem.id)
                   //  let url = payloadItem.url + '&f=filename.mov'
                   //  let fileReaderStream = fs.createReadStream(url)
+                  messageData.description = messageData.message
                   messageData.video = true
                   messageData.file_url = payloadItem.url
                 }
