95a580186998e69125b75e87b01637dacd1dc3ad
Baqar <bjafri5@gmail.com>
2018-06-04 15:31:31 +0500

    bootstrap switch

--- client/src/containers/settings/settings.js
+++ client/src/containers/settings/settings.js
@@ -134,12 +134,14 @@ class Settings extends React.Component {
     if (this.state.saveState === true || this.state.saveState === false) {
       this.initializeSwitch(this.state.saveState)
     }
-  }
-  componentDidUpdate () {
-    console.log('in componentDidUpdate', this.state.saveState)
     this.initializeSwitch(this.state.buttonState)
     this.initializeSwitchNGP(this.state.ngpButtonState)
   }
+  // componentDidUpdate () {
+  //   console.log('in componentDidUpdate', this.state.saveState)
+  //   this.initializeSwitch(this.state.buttonState)
+  //   this.initializeSwitchNGP(this.state.ngpButtonState)
+  // }
   changeType (e) {
     if (this.state.type === 'password') {
       this.setState({type: 'text', buttonText: 'Hide'})
