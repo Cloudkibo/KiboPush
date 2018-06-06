0ba4163cd71d71888af0f08b79f28b9952a02ea3
sania <sania@cloudkibo.com>
2018-06-05 16:49:30 -0800

    #3182

--- client/src/containers/convo/editTemplate.js
+++ client/src/containers/convo/editTemplate.js
@@ -152,7 +152,7 @@ class EditTemplate extends React.Component {
     /* eslint-disable */
     $('[href="#tab_2"]').removeClass('active')
     /* eslint-enable */
-    this.setState({tabActive: 'broadcast'})
+    this.setState({tabActive: 'broadcast', resetTarget: false})
   }
   onTargetClick () {
     /* eslint-disable */
