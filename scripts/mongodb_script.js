200dd585a215c32ce91d5970ab7e07f15639ad22
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-06-28 16:21:05 +0500

    partial scheduler node script

--- scripts/mongodb_script.js
+++ scripts/mongodb_script.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
+const logger = require('../server/components/logger')
 const config = require('../server/config/environment')
 const automationQueue = require('../server/api/automation_queue/automation_queue.model')
+const surveyQuestions = require('../server/api/surveys/surveyquestions.model')
+const Surveys = require('../server/api/surveys/surveys.model')
+const TAG = 'scripts/monodb_script.js'
 
 mongoose.connect(config.mongo.uri)
 
@@ -10,16 +14,22 @@ automationQueue.find({}, (err, data) => {
   }
 
   if (data) {
-    let queueOfMessages = data
-    myFunction(queueOfMessages)
-  }
-})
+    for (let i = 0; i < data.length; i++) {
+      let obj = data[i]
+      if (obj.type === 'survey') {
+        // If the saved automated message type is survey
+        console.log('survey data: ' + obj)
+        surveyQuestions.find({ surveyId: obj._id })
+        .populate('surveyId')
+        .exec((err2, questions) => {
+          if (err2) {
+            return logger.serverLog(TAG, `At survey question ${JSON.stringify(err2)}`)
+          }
 
-const myFunction = (queueOfMessages) => {
-  queueOfMessages.forEach(message => {
-    if (message.type === 'survey') {
-      console.log('survey data: ' + message)
-      mongoose.disconnect()
+          // Do the remaining work here
+        })
+      }
     }
-  })
-}
+  }
+  mongoose.disconnect()
+})
