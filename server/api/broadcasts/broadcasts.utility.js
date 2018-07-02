551a2db36af727e74649418b20c52dc42c8eec2e
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-07-02 16:41:51 +0500

    indent fix

--- server/api/broadcasts/broadcasts.utility.js
+++ server/api/broadcasts/broadcasts.utility.js
@@ -81,27 +81,13 @@ function validateInput (body) {
         }
       }
       if (body.payload[i].componentType === 'list') {
-//        if (body.payload[i].topElementStyle === undefined) return false
-//        if (body.payload[i].topElementStyle === '') return false
         if (body.payload[i].cards === undefined) return false
         if (body.payload[i].cards.length === 0) return false
         for (let j = 0; j < body.payload[i].cards.length; j++) {
           if (body.payload[i].cards[j].title === undefined ||
             body.payload[i].cards[j].title === '') return false
-//          if (body.payload[i].cards[j].subtitle === undefined ||
-//            body.payload[i].cards[j].subtitle === '') return false
           if (body.payload[i].cards[j].image_url === undefined ||
             body.payload[i].cards[j].image_url === '') return false
-//          if (body.payload[i].cards[j].buttons === undefined) return false
-//          if (body.payload[i].cards[j].buttons.length === 0) return false
-          // for (let k = 0; k < body.payload[i].cards[j].buttons.length; k++) {
-          //   if (body.payload[i].cards[j].buttons[k].type === 'web_url') {
-          //     if (!utility.validateUrl(
-          //       body.payload[i].cards[j].buttons[k].url)) return false
-          //   }
-          // }
-          // if (body.payload[i].buttons === undefined) return false
-          // if (body.payload[i].buttons.length === 0) return false
         }
       }
     }
