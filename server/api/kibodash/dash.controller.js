899f86f2f8e5e364efb1064aa437b749c4ca14e5
Mehrish Yousuf <mehrish.yousuf@gmail.com>
2018-07-18 16:52:47 +0500

    user wise aggregates scripts APIs

--- server/api/kibodash/dash.controller.js
+++ server/api/kibodash/dash.controller.js
@@ -1,18 +1,18 @@
 /**
  * Created by sojharo on 25/09/2017.
  */
-// const logger = require('../../components/logger')
-// const TAG = 'api/backdoor/backdoor.controller.js'
+const logger = require('../../components/logger')
+const TAG = 'api/kibodash/dash.controller.js'
 const Users = require('../user/Users.model')
 const Pages = require('../pages/Pages.model')
-const { filterConnectedPages, countResults } = require('./pipeline')
+const { filterConnectedPages, countResults, joinCompanyWithSubscribers, selectCompanyFields, companyWiseAggregate,
+  companyWisePageCount } = require('./pipeline')
 const Subscribers = require('../subscribers/Subscribers.model')
 const Broadcasts = require('../broadcasts/broadcasts.model')
 const Polls = require('../polls/Polls.model')
 const Surveys = require('../surveys/surveys.model')
-// const config = require('./../../config/environment/index')
- // const mongoose = require('mongoose')
-// var json2csv = require('json2csv')
+const CompanyUsers = require('../companyuser/companyuser.model')
+const mongoose = require('mongoose')
 
 exports.platformWiseData = function (req, res) {
   let connectetPages = Pages.aggregate([filterConnectedPages, countResults]).exec()
@@ -24,7 +24,7 @@ exports.platformWiseData = function (req, res) {
   let totalSurveys = Surveys.aggregate([countResults]).exec()
 
   let finalResults = Promise.all([connectetPages, totalPages, totalUsers, totalSubscribers, totalBroadcasts, totalPolls, totalSurveys])
-
+  logger.serverLog(TAG, `user not found for page ${JSON.stringify(finalResults)}`)
   finalResults.then(function (results) {
     let data = {
       connectetPages: (results[0].length === 0) ? 0 : results[0][0].count,
@@ -50,13 +50,104 @@ exports.platformWiseData = function (req, res) {
 exports.pageWiseData = function (req, res) {
   res.status(200).json({
     status: 'success',
-    payload: {text: 'Sher aya k chor aya!!!'}
+    payload: { text: '' }
   })
 }
 
 exports.userWiseData = function (req, res) {
-  res.status(200).json({
-    status: 'success',
-    payload: {text: 'The man who walks with the crowd can get no further than the crowd'}
+  let companySubscribers = CompanyUsers.aggregate([joinCompanyWithSubscribers, selectCompanyFields]).exec()
+  let numberOfBroadcasts = Broadcasts.aggregate([companyWiseAggregate]).exec()
+  let numberOfPolls = Polls.aggregate([companyWiseAggregate]).exec()
+  let numberOfSurveys = Surveys.aggregate([companyWiseAggregate]).exec()
+  let companyPagesCount = Pages.aggregate([companyWisePageCount]).exec()
+  let companyConnectedPagesCount = Pages.aggregate([filterConnectedPages, companyWisePageCount]).exec()
+  let finalResults = Promise.all([companySubscribers, numberOfBroadcasts, numberOfPolls, numberOfSurveys,
+    companyPagesCount, companyConnectedPagesCount])
+
+  finalResults.then(function (results) {
+    let data = {}
+    data = results[0]
+    for (let i = 0; i < data.length; i++) {
+      let userId = mongoose.Types.ObjectId(data[i].userId)
+      Users.findOne({ '_id': userId }, (err, user) => {
+        if (err) {
+          return res.status(500).json({
+            status: 'failed',
+            description: `Internal Server Error ${JSON.stringify(err)}`})
+        }
+        data[i].userName = user.name
+        if (i === (data.length - 1)) {
+          setBroadcastsCount(results, data)
+          setPollsCount(results, data)
+          setSurveysCount(results, data)
+          setTotalPagesCount(results, data)
+          setConnectedPagesCount(results, data)
+
+          res.status(200).json({
+            status: 'success',
+            payload: data
+          })
+        }
+      })
+    }
+  }).catch((err) => {
+    res.status(500).json({
+      status: 'failed',
+      error: err
+    })
   })
+}
+
+function setBroadcastsCount (results, data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < results[1].length; j++) {
+      if (results[1][j]._id.toString() === data[i].companyId.toString()) {
+        let broadcasts = results[1][j]
+        data[i].numberOfBroadcasts = broadcasts.totalCount
+      }
+    }
+  }
+}
+
+function setPollsCount (results, data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < results[2].length; j++) {
+      if (results[2][j]._id.toString() === data[i].companyId.toString()) {
+        let polls = results[2][j]
+        data[i].numberOfPolls = polls.totalCount
+      }
+    }
+  }
+}
+
+function setSurveysCount (results, data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < results[3].length; j++) {
+      if (results[3][j]._id.toString() === data[i].companyId.toString()) {
+        let surveys = results[3][j]
+        data[i].numberOfSurveys = surveys.totalCount
+      }
+    }
+  }
+}
+function setTotalPagesCount (results, data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < results[4].length; j++) {
+      if (results[4][j]._id.toString() === data[i].companyId.toString()) {
+        let pages = results[4][j]
+        data[i].numberOfPages = pages.totalPages
+      }
+    }
+  }
+}
+
+function setConnectedPagesCount (results, data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < results[5].length; j++) {
+      if (results[5][j]._id.toString() === data[i].companyId.toString()) {
+        let pages = results[5][j]
+        data[i].numberOfConnectedPages = pages.totalPages
+      }
+    }
+  }
 }
