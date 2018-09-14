6a26775575166cf2bc9f62893732580cc8a39511
Imran Shoukat <imran.shoukat1995@gmail.com>
2018-09-14 14:01:43 +0500

    broadcast issue #4332

--- client/src/redux/actions/actions.utility.js
+++ client/src/redux/actions/actions.utility.js
@@ -7,7 +7,7 @@ export function removeButtonOldurl (data) {
     } else {
       if (data.payload[i].buttons && data.payload[i].buttons.length > 0) {
         for (let j = 0; j < data.payload[i].buttons.length; j++) {
-          if (data.payload[i].buttons[j].type === 'web_url') {
+          if (data.payload[i].buttons[j].type === 'web_url' && data.payload[i].buttons[j].newUrl) {
             let url = data.payload[i].buttons[j].newUrl
             data.payload[i].buttons[j].url = url
             delete data.payload[i].buttons[j].newUrl
@@ -17,7 +17,7 @@ export function removeButtonOldurl (data) {
       if (data.payload[i].componentType === 'gallery') {
         for (let j = 0; j < data.payload[i].cards.length; j++) {
           for (let k = 0; k < data.payload[i].cards[j].buttons.length; k++) {
-            if (data.payload[i].cards[j].buttons[k].type === 'web_url') {
+            if (data.payload[i].cards[j].buttons[k].type === 'web_url' && data.payload[i].cards[j].buttons[k].newUrl) {
               let url = data.payload[i].cards[j].buttons[k].newUrl
               data.payload[i].cards[j].buttons[k].url = url
               delete data.payload[i].cards[j].buttons[k].newUrl
@@ -28,7 +28,7 @@ export function removeButtonOldurl (data) {
       if (data.payload[i].componentType === 'list') {
         for (let j = 0; j < data.payload[i].listItems.length; j++) {
           for (let k = 0; k < data.payload[i].listItems[j].buttons.length; k++) {
-            if (data.payload[i].listItems[j].buttons[k].type === 'web_url') {
+            if (data.payload[i].listItems[j].buttons[k].type === 'web_url' && data.payload[i].listItems[j].buttons[k].newUrl) {
               let url = data.payload[i].listItems[j].buttons[k].newUrl
               data.payload[i].listItems[j].buttons[k].url = url
               delete data.payload[i].listItems[j].buttons[k].newUrl
