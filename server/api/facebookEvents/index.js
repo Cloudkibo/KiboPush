51ad1302e885c869c7ebd660331a71214e5ea8ce
anishachhatwani <i130315@nu.edu.pk>
2018-09-25 16:00:57 +0500

    webhook work

--- server/api/facebookEvents/index.js
+++ server/api/facebookEvents/index.js
@@ -4,8 +4,10 @@ const express = require('express')
 const router = express.Router()
 const autopostingController = require('./autoposting.controller')
 const commentController = require('./comment.controller')
+const changePageNameController = require('./changePageName.controller')
 
 router.post('/autoposting', autopostingController.autoposting)
 router.post('/comment', commentController.sendCommentReply)
+router.post('/changePageName', changePageNameController.changePageName)
 
 module.exports = router
