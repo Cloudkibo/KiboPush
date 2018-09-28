d67c71c590c4351702680b224e1a0922d322f3f9
sania <sania@cloudkibo.com>
2018-09-28 15:33:13 -0800

    #4416

--- client/src/containers/polls/CreatePoll.js
+++ client/src/containers/polls/CreatePoll.js
@@ -252,83 +252,85 @@ class CreatePoll extends React.Component {
       transition: 'scale'
     }
     return (
-      <div className='m-grid__item m-grid__item--fluid m-wrapper'>
+      <div>
         <AlertContainer ref={a => { this.msg = a }} {...alertOptions} />
-        {
-          this.state.isShowingModalGuideLines &&
-          <ModalContainer style={{width: '500px'}}
-            onClose={this.closeGuideLinesDialog}>
-            <ModalDialog style={{width: '500px'}}
+        <div className='m-grid__item m-grid__item--fluid m-wrapper'>
+          {
+            this.state.isShowingModalGuideLines &&
+            <ModalContainer style={{width: '500px'}}
               onClose={this.closeGuideLinesDialog}>
-              <h4>Message Types</h4>
-              <p> Following are the types of messages that can be sent to facebook messenger.</p>
-              <div className='panel-group accordion' id='accordion1'>
-                <div className='panel panel-default'>
-                  <div className='panel-heading guidelines-heading'>
-                    <h4 className='panel-title'>
-                      <a className='guidelines-link accordion-toggle accordion-toggle-styled collapsed' data-toggle='collapse' data-parent='#accordion1' href='#collapse_1' aria-expanded='false'>Subscription Messages</a>
-                    </h4>
+              <ModalDialog style={{width: '500px'}}
+                onClose={this.closeGuideLinesDialog}>
+                <h4>Message Types</h4>
+                <p> Following are the types of messages that can be sent to facebook messenger.</p>
+                <div className='panel-group accordion' id='accordion1'>
+                  <div className='panel panel-default'>
+                    <div className='panel-heading guidelines-heading'>
+                      <h4 className='panel-title'>
+                        <a className='guidelines-link accordion-toggle accordion-toggle-styled collapsed' data-toggle='collapse' data-parent='#accordion1' href='#collapse_1' aria-expanded='false'>Subscription Messages</a>
+                      </h4>
+                    </div>
+                    <div id='collapse_1' className='panel-collapse collapse' aria-expanded='false' style={{height: '0px'}}>
+                      <div className='panel-body'>
+                        <p>Subscription messages can&#39;t contain ads or promotional materials, but can be sent at any time regardless of time passed since last user activity. In order to send Subscription Messages, please apply for Subscription Messages Permission by following the steps given on this&nbsp;
+                        <a href='https://developers.facebook.com/docs/messenger-platform/policy/app-to-page-subscriptions' target='_blank'>link.</a></p>
+                      </div>
+                    </div>
                   </div>
-                  <div id='collapse_1' className='panel-collapse collapse' aria-expanded='false' style={{height: '0px'}}>
-                    <div className='panel-body'>
-                      <p>Subscription messages can&#39;t contain ads or promotional materials, but can be sent at any time regardless of time passed since last user activity. In order to send Subscription Messages, please apply for Subscription Messages Permission by following the steps given on this&nbsp;
-                      <a href='https://developers.facebook.com/docs/messenger-platform/policy/app-to-page-subscriptions' target='_blank'>link.</a></p>
+                  <div className='panel panel-default'>
+                    <div className='panel-heading guidelines-heading'>
+                      <h4 className='panel-title'>
+                        <a className='guidelines-link accordion-toggle collapsed' data-toggle='collapse' data-parent='#accordion1' href='#collapse_2' aria-expanded='false'>Promotional Messages</a>
+                      </h4>
+                    </div>
+                    <div id='collapse_2' className='panel-collapse collapse' aria-expanded='false' style={{height: '0px'}}>
+                      <div className='panel-body'>
+                        Promotional messages can contain ads and promotional materials, but can only be sent to subscribers who were active in the past 24 hours.
+                      </div>
+                    </div>
+                  </div>
+                  <div className='panel panel-default'>
+                    <div className='panel-heading guidelines-heading'>
+                      <h4 className='panel-title'>
+                        <a className='guidelines-link accordion-toggle collapsed' data-toggle='collapse' data-parent='#accordion1' href='#collapse_3' aria-expanded='false'>Follow-Up Messages</a>
+                      </h4>
+                    </div>
+                    <div id='collapse_3' className='panel-collapse collapse' aria-expanded='false' style={{height: '0px'}}>
+                      <div className='panel-body'>
+                        After the end of the 24 hours window you have an ability to send "1 follow up message" to these recipients. After that you won&#39;t be able to send them ads or promotional messages until they interact with you again.
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className='panel panel-default'>
-                  <div className='panel-heading guidelines-heading'>
-                    <h4 className='panel-title'>
-                      <a className='guidelines-link accordion-toggle collapsed' data-toggle='collapse' data-parent='#accordion1' href='#collapse_2' aria-expanded='false'>Promotional Messages</a>
-                    </h4>
-                  </div>
-                  <div id='collapse_2' className='panel-collapse collapse' aria-expanded='false' style={{height: '0px'}}>
-                    <div className='panel-body'>
-                      Promotional messages can contain ads and promotional materials, but can only be sent to subscribers who were active in the past 24 hours.
-                    </div>
-                  </div>
-                </div>
-                <div className='panel panel-default'>
-                  <div className='panel-heading guidelines-heading'>
-                    <h4 className='panel-title'>
-                      <a className='guidelines-link accordion-toggle collapsed' data-toggle='collapse' data-parent='#accordion1' href='#collapse_3' aria-expanded='false'>Follow-Up Messages</a>
-                    </h4>
-                  </div>
-                  <div id='collapse_3' className='panel-collapse collapse' aria-expanded='false' style={{height: '0px'}}>
-                    <div className='panel-body'>
-                      After the end of the 24 hours window you have an ability to send "1 follow up message" to these recipients. After that you won&#39;t be able to send them ads or promotional messages until they interact with you again.
-                    </div>
-                  </div>
-                </div>
+              </ModalDialog>
+            </ModalContainer>
+          }
+          <div className='m-subheader '>
+            <div className='d-flex align-items-center'>
+              <div className='mr-auto'>
+                <h3 className='m-subheader__title'>Create Poll</h3>
               </div>
-            </ModalDialog>
-          </ModalContainer>
-        }
-        <div className='m-subheader '>
-          <div className='d-flex align-items-center'>
-            <div className='mr-auto'>
-              <h3 className='m-subheader__title'>Create Poll</h3>
             </div>
           </div>
-        </div>
-        <div className='m-content'>
-          <div className='m-alert m-alert--icon m-alert--air m-alert--square alert alert-dismissible m--margin-bottom-30' role='alert'>
-            <div className='m-alert__icon'>
-              <i className='flaticon-exclamation m--font-brand' />
+          <div className='m-content'>
+            <div className='m-alert m-alert--icon m-alert--air m-alert--square alert alert-dismissible m--margin-bottom-30' role='alert'>
+              <div className='m-alert__icon'>
+                <i className='flaticon-exclamation m--font-brand' />
+              </div>
+              <div className='m-alert__text'>
+                View Facebook guidelines regarding types of messages here: <Link className='linkMessageTypes' style={{color: '#5867dd', cursor: 'pointer'}} onClick={this.showGuideLinesDialog} >Message Types</Link>
+              </div>
             </div>
-            <div className='m-alert__text'>
-              View Facebook guidelines regarding types of messages here: <Link className='linkMessageTypes' style={{color: '#5867dd', cursor: 'pointer'}} onClick={this.showGuideLinesDialog} >Message Types</Link>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col-lg-8 col-md-8 col-sm-4 col-xs-12'>
-              <div className='m-portlet' style={{height: '100%'}}>
-                <div className='m-portlet__head'>
-                  <div className='m-portlet__head-caption'>
-                    <div className='m-portlet__head-title'>
-                      <h3 className='m-portlet__head-text'>
-                      Ask Facebook Subscribers a Question
-                      </h3>
+            <div className='row'>
+              <div className='col-lg-8 col-md-8 col-sm-4 col-xs-12'>
+                <div className='m-portlet' style={{height: '100%'}}>
+                  <div className='m-portlet__head'>
+                    <div className='m-portlet__head-caption'>
+                      <div className='m-portlet__head-title'>
+                        <h3 className='m-portlet__head-text'>
+                        Ask Facebook Subscribers a Question
+                        </h3>
+                      </div>
                     </div>
                   </div>
                   <div className='m-portlet__body'>
--- client/src/containers/polls/poll.js
+++ client/src/containers/polls/poll.js
@@ -350,7 +350,7 @@ class Poll extends React.Component {
                   <div className='m-portlet__head-tools'>
                     {
                       this.props.subscribers && this.props.subscribers.length === 0
-                      ? <button className='btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill' onClick={this.showDialog} disabled>
+                      ? <button className='btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill' onClick={this.showDialog} >
                         <span>
                           <i className='la la-plus' />
                           <span>
