69d89fbe17ba4f81e77384ac13164965045ff758
sania <sania@cloudkibo.com>
2018-10-03 15:24:21 +0500

    Modify KiboPush to use Accounts app for Permissions table (billing and pricing) #80

---
+++ server/api/v2/permissions/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../../../auth/auth.service')
+const validate = require('express-jsonschema').validate
+
+const validationSchema = require('./validationSchema')
+const controller = require('./permissions.controller')
+
+router.post('/create',
+  auth.isAuthenticated(),
+  validate({body: validationSchema.create}),
+  controller.create)
+
+router.post('/update',
+  auth.isAuthenticated(),
+  validate({body: validationSchema.update}),
+  controller.update)
+
+router.post('/updatePermissions',
+    auth.isAuthenticated(),
+    validate({body: validationSchema.updatePermissions}),
+    auth.hasRole('buyer'),
+    controller.updatePermissions)
+
+router.get('/:role',
+        auth.isAuthenticated(),
+        controller.index)
+
+router.get('/populateRolePermissions',
+    auth.isAuthenticated(),
+    controller.populateRolePermissions)
+
+router.get('/fetchPermissions',
+    auth.isAuthenticated(),
+    controller.fetchPermissions)
+
+module.exports = router
