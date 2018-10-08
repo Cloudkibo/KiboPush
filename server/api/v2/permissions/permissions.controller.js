07786ec3d10aeabb4dfedb9d52ec60600cadf633
Imran Shoukat <imran.shoukat1995@gmail.com>
2018-10-08 11:42:56 +0500

    Update permissions.controller.js

--- server/api/v2/permissions/permissions.controller.js
+++ server/api/v2/permissions/permissions.controller.js
@@ -1,62 +1,6 @@
 const utility = require('../utility')
 const logicLayer = require('./permissions.logiclayer')
 
-exports.index = function (req, res) {
-  utility.callApi(`permission/${req.params.role}`)
-  .then(permissions => {
-    res.status(200).json({
-      status: 'success',
-      payload: permissions
-    })
-  })
-    .catch(error => {
-      res.status(500).json({
-        status: 'failed',
-        payload: `Internal Server Error ${JSON.stringify(error)}${JSON.stringify(error)}`
-      })
-    })
-}
-
-exports.update = function (req, res) {
-  utility.callApi(`permission/update`, 'post', {permissions: req.body.permissions, role: req.body.permissions.role})
-  .then(result => {
-    res.status(201).json({status: 'success', payload: result})
-  })
-  .catch(error => {
-    res.status(500).json({
-      status: 'failed',
-      description: `Internal Server Error ${JSON.stringify(error)}`
-    })
-  })
-}
-
-exports.create = function (req, res) {
-  let query = {}
-  query = logicLayer.prepareCreateQuery(req.body)
-  utility.callApi(`permission/roleAggregate`, 'post', [{$addFields: query}, {$out: 'role_permissions'}])
-  .then(result => {
-    utility.callApi(`permission/aggregate`, 'post', [{$addFields: query}, {$out: 'permissions'}])
-    .then(result => {
-      res.status(200).json({
-        status: 'success',
-        description: 'Permission has been added successfully!'
-      })
-    })
-    .catch(error => {
-      res.status(500).json({
-        status: 'failed',
-        description: error
-      })
-    })
-  })
-  .catch(error => {
-    res.status(500).json({
-      status: 'failed',
-      description: `Internal Server Error ${JSON.stringify(error)}`
-    })
-  })
-}
-
 exports.updatePermissions = function (req, res) {
   utility.callApi(`permission/genericFind`, 'post', { companyId: req.body.companyId, userId: req.body.userId })
   .then(permission => {
