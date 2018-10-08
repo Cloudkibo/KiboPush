eaa2a5e6b7c96e298e8efc504e1fb43adb6d0fce
Imran Shoukat <imran.shoukat1995@gmail.com>
2018-10-08 11:42:23 +0500

    Update index.js

--- server/api/v2/permissions/index.js
+++ server/api/v2/permissions/index.js
@@ -6,25 +6,11 @@ const validate = require('express-jsonschema').validate
 const validationSchema = require('./validationSchema')
 const controller = require('./permissions.controller')
 
-router.post('/create',
-  auth.isAuthenticated(),
-  validate({body: validationSchema.create}),
-  controller.create)
-
-router.post('/update',
-  auth.isAuthenticated(),
-  validate({body: validationSchema.update}),
-  controller.update)
-
 router.post('/updatePermissions',
     auth.isAuthenticated(),
     validate({body: validationSchema.updatePermissions}),
     auth.hasRole('buyer'),
     controller.updatePermissions)
-
-router.get('/:role',
-        auth.isAuthenticated(),
-        controller.index)
 
 router.get('/fetchPermissions',
     auth.isAuthenticated(),
