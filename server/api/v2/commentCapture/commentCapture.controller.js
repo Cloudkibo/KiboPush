4d3f945d6c3732783b437ca79837e25f9f212ea2
Imran Shoukat <ishoukat@localhost.localdomain>
2018-10-16 15:49:13 +0500

    integration work

--- server/api/v2/commentCapture/commentCapture.controller.js
+++ server/api/v2/commentCapture/commentCapture.controller.js
@@ -54,7 +54,7 @@ exports.create = function (req, res) {
       userId: req.user._id,
       reply: req.body.reply,
       payload: req.body.payload,
-      includedKeywords: req.body.includeKeywords,
+      includedKeywords: req.body.includedKeywords,
       excludedKeywords: req.body.excludedKeywords
     }, req.headers.authorization)
     .then(postCreated => {
