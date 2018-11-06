138e74f68cb5764547ea8b79d220ed0dad76c177
sania <sania@cloudkibo.com>
2018-11-06 16:47:29 +0500

    #4560

--- server/api/v1/broadcasts/broadcasts2.controller.js
+++ server/api/v1/broadcasts/broadcasts2.controller.js
@@ -297,7 +297,7 @@ exports.upload = function (req, res) {
   let fext = req.files.file.name.split('.')
   serverPath += '.' + fext[fext.length - 1].toLowerCase()
 
-  let dir = path.resolve(__dirname, '../../../../broadcastFiles/')
+  let dir = path.resolve(__dirname, '../../../../../broadcastFiles/')
 
   if (req.files.file.size === 0) {
     return res.status(400).json({
