68a7abed652e13473ab721660d3999e5b3ffd2f3
Imran Shoukat <ishoukat@192.168.0.103>
2018-11-13 09:11:28 +0500

    social security demo

--- server/api/v1/smart_replies/logiclayer.js
+++ server/api/v1/smart_replies/logiclayer.js
@@ -15,25 +15,42 @@ const dir = path.resolve(__dirname, '../../../../smart-replies-files/')
 const downloadVideo = (data) => {
   return new Promise((resolve, reject) => {
     let video = youtubedl(data.url)
-    let stream
+    let downloaded = 0
+    // let stream
 
     video.on('info', (info) => {
       logger.serverLog(TAG, 'Download started')
       logger.serverLog(TAG, 'filename: ' + info.filename)
       logger.serverLog(TAG, 'size: ' + info.size)
-      let size = info.size
-      if (size < 25000000) {
-        stream = video.pipe(fs.createWriteStream(`${dir}/bot-video.mp4`))
-        stream.on('error', (error) => {
-          stream.end()
-          reject(util.inspect(error))
-        })
-        stream.on('finish', () => {
-          resolve(`${dir}/bot-video.mp4`)
-        })
-      } else {
-        resolve('ERR_LIMIT_REACHED')
+      // let size = info.size
+      // if (size < 25000000) {
+      //   stream = video.pipe(fs.createWriteStream(`${dir}/bot-video.mp4`))
+      //   stream.on('error', (error) => {
+      //     stream.end()
+      //     reject(util.inspect(error))
+      //   })
+      //   stream.on('finish', () => {
+      //     resolve(`${dir}/bot-video.mp4`)
+      //   })
+      // } else {
+      //   resolve('ERR_LIMIT_REACHED')
+      // }
+    })
+
+    let stream = video.pipe(fs.createWriteStream(`${dir}/bot-video.mp4`))
+    video.on('data', (chunk) => {
+      downloaded += chunk
+      if (downloaded > 5000000) {
+        stream.end()
+        resolve(`${dir}/bot-video.mp4`)
       }
+    })
+    stream.on('error', (error) => {
+      stream.end()
+      reject(util.inspect(error))
+    })
+    stream.on('finish', () => {
+      resolve(`${dir}/bot-video.mp4`)
     })
   })
 }
