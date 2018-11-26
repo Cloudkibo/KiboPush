f29c0cb06ecb4b21185ef2a942a26cc3f364efe9
anishachhatwani <i130315@nu.edu.pk>
2018-11-26 16:23:04 +0500

    messenger code growth tool

--- client/src/redux/actions/messengerCode.actions.js
+++ client/src/redux/actions/messengerCode.actions.js
@@ -1,9 +1,9 @@
 import callApi from '../../utility/api.caller.service'
 
 export function requestMessengerCode (data) {
+  console.log('data in requestMessengerCode', data)
   return (dispatch) => {
     callApi('messengerCode/generateMessengerCode', 'post', data)
-    console.log('data in requestMessengerCode', data)
       .then(res => {
         console.log('response from requestMessengerCode', res)
       })
