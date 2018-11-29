5040dcde1847ea19e93c8de96ee4a35e1579abbd
anishachhatwani <i130315@nu.edu.pk>
2018-11-29 15:40:06 +0500

    changing url in actions

--- client/src/redux/actions/messengerCode.actions.js
+++ client/src/redux/actions/messengerCode.actions.js
@@ -3,7 +3,7 @@ import callApi from '../../utility/api.caller.service'
 export function requestMessengerCode (data) {
   console.log('data in requestMessengerCode', data)
   return (dispatch) => {
-    callApi('messengerCode/generateMessengerCode', 'post', data)
+    callApi('messenger_code', 'post', data)
       .then(res => {
         console.log('response from requestMessengerCode', res)
       })
