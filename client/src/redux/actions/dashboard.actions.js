a62387c8395ac9072ad322f0f4e788bb6d16f352
Imran Shoukat <ishoukat@192.168.0.107>
2018-12-19 09:59:00 +0500

    logs

--- client/src/redux/actions/dashboard.actions.js
+++ client/src/redux/actions/dashboard.actions.js
@@ -55,7 +55,10 @@ export function sentVsSeen (pageId) {
   // here we will fetch list of subscribers from endpoint
   return (dispatch) => {
     callApi(`dashboard/sentVsSeen/${pageId}`)
-      .then(res => dispatch(updateSentVsSeen(res.payload)))
+      .then(res => {
+        console.log('sentVsSeen response', res)
+        dispatch(updateSentVsSeen(res.payload))
+      })
   }
 }
 export function loadGraphData (days) {
