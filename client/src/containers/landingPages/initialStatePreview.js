490530e6a3e426033e687679bff6e8fe5932743a
anishachhatwani <i130315@nu.edu.pk>
2018-12-23 22:54:10 +0500

    landing page

--- client/src/containers/landingPages/initialStatePreview.js
+++ client/src/containers/landingPages/initialStatePreview.js
@@ -50,7 +50,7 @@ class PreviewInitialSate extends React.Component {
         fjs.parentNode.insertBefore(js, fjs)
       }(document, 'script', 'facebook-jssdk'))
       FB.init({
-      appId: '{your-app-id}',
+      appId: 'nextProps.fbAppId',
       status: true,
       xfbml: true,
       version: 'v2.7' // or v2.6, v2.5, v2.4, v2.3
