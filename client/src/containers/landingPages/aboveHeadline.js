bf507dca998bb48d281bf834c7131930cbfbdbca
anishachhatwani <i130315@nu.edu.pk>
2018-12-23 22:26:09 +0500

    landing page

--- client/src/containers/landingPages/aboveHeadline.js
+++ client/src/containers/landingPages/aboveHeadline.js
@@ -6,23 +6,6 @@
 import React from 'react'
 
 class AboveHeadline extends React.Component {
-  componentDidMount () {
-    window.fbAsyncInit = function () {
-      FB.init({
-        appId: this.props.fbAppId,
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: 'v3.2'
-      })
-    };
-    (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0]
-      if (d.getElementById(id)) { return }
-      js = d.createElement(s); js.id = id
-      js.src = 'https://connect.facebook.net/en_US/sdk.js'
-      fjs.parentNode.insertBefore(js, fjs)
-    }(document, 'script', 'facebook-jssdk'))
-  }
   render () {
     console.log('this.props in aboveHeadline', this.props)
     return (
--- client/src/containers/landingPages/initialStatePreview.js
+++ client/src/containers/landingPages/initialStatePreview.js
@@ -32,6 +32,26 @@ class PreviewInitialSate extends React.Component {
     this.props.updateLandingPageData(this.props.landingPage, this.props.landingPage.currentTab, 'description', e.target.value)
   }
 
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.fbAppId) {
+      window.fbAsyncInit = function () {
+        FB.init({
+          appId: this.props.fbAppId,
+          autoLogAppEvents: true,
+          xfbml: true,
+          version: 'v3.2'
+        })
+      };
+      (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0]
+        if (d.getElementById(id)) { return }
+        js = d.createElement(s); js.id = id
+        js.src = 'https://connect.facebook.net/en_US/sdk.js'
+        fjs.parentNode.insertBefore(js, fjs)
+      }(document, 'script', 'facebook-jssdk'))
+    }
+  }
+
   render () {
     return (
       <div>
