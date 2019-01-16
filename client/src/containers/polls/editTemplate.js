e85c67676458a1e6ebd3ceb1372f11010e0fa8ed
Faizan Rasheed <faizanrasheed4@gmail.com>
2019-01-16 11:09:38 +0500

    Subscription messaging guide #4904

--- client/src/containers/polls/editTemplate.js
+++ client/src/containers/polls/editTemplate.js
@@ -263,7 +263,7 @@ class EditPoll extends React.Component {
                   <div id='collapse_1' className='panel-collapse collapse' aria-expanded='false' style={{height: '0px'}}>
                     <div className='panel-body'>
                       <p>Subscription messages can&#39;t contain ads or promotional materials, but can be sent at any time regardless of time passed since last user activity. In order to send Subscription Messages, please apply for Subscription Messages Permission by following the steps given on this&nbsp;
-                      <a href='https://developers.facebook.com/docs/messenger-platform/policy/app-to-page-subscriptions' target='_blank'>link.</a></p>
+                      <a href='https://kibopush.com/subscription-messaging/' target='_blank'>link.</a></p>
                     </div>
                   </div>
                 </div>
