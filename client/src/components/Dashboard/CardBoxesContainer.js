45bc6027d6d7aa8b0284f411a677e0836e541642
sania <sania@cloudkibo.com>
2019-01-21 12:53:58 +0500

    #4940

--- client/src/components/Dashboard/CardBoxesContainer.js
+++ client/src/components/Dashboard/CardBoxesContainer.js
@@ -5,6 +5,7 @@
 
 import React from 'react'
 import CardBox from './CardBox'
+import { Link } from 'react-router'
 
 /* card box style must be any of these
   band
@@ -19,28 +20,32 @@ class CardBoxesContainer extends React.Component {
     return (
       <div className='col-xl-12'>
         <div className='row m-row--full-height'>
-          <div className='col-sm-3 col-md-3 col-lg-3'>
-            <CardBox
-              style='brand'
-              value={this.props.data.totalPages ? this.props.data.totalPages : 0}
-              label='Total Pages'
-            />
+          <div className='col-sm-3 col-md-3 col-lg-3' title='Total pages on Facebook'>
+            <Link to='/addPages' >
+              <CardBox
+                style='brand'
+                value={this.props.data.totalPages ? this.props.data.totalPages : 0}
+                label='Total Pages'
+              />
+            </Link>
           </div>
-          <div className='col-sm-3 col-md-3 col-lg-3'>
-            <CardBox
-              style='success'
-              value={this.props.data.pages ? this.props.data.pages : 0}
-              label='Connected Pages'
-            />
+          <div className='col-sm-3 col-md-3 col-lg-3' title='Total pages connected to KiboPush'>
+            <Link to='/addPages' >
+              <CardBox
+                style='success'
+                value={this.props.data.pages ? this.props.data.pages : 0}
+                label='Connected Pages'
+              />
+            </Link>
           </div>
-          <div className='col-sm-3 col-md-3 col-lg-3'>
+          <div className='col-sm-3 col-md-3 col-lg-3' title='Total Subscribers'>
             <CardBox
               style='danger'
               value={this.props.data.subscribers ? this.props.data.subscribers : 0}
               label='Subscribers'
             />
           </div>
-          <div className='col-sm-3 col-md-3 col-lg-3'>
+          <div className='col-sm-3 col-md-3 col-lg-3' title={url.includes('kibochat.cloudkibo.com') ? 'New Messages' : 'No.of Sequences Created'}>
             <CardBox
               style='accent'
               value={url.includes('kibochat.cloudkibo.com') ? (this.props.data.unreadCount ? this.props.data.unreadCount : 0) : (this.props.data.sequences ? this.props.data.sequences : 0)}
