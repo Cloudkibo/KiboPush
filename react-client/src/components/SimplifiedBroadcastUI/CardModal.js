27c289bc370f99af682f6eaf85138a3c0cebe636
Baqar <bjafri5@gmail.com>
2019-05-09 10:18:19 +0500

    fix card image ui

--- react-client/src/components/SimplifiedBroadcastUI/CardModal.js
+++ react-client/src/components/SimplifiedBroadcastUI/CardModal.js
@@ -132,7 +132,7 @@ class CardModal extends React.Component {
                 <div className='ui-block' style={{border: '1px solid rgba(0,0,0,.1)', borderRadius: '10px', minHeight: '200px', maxWidth: '250px', margin: 'auto', marginTop: '100px'}} >
                   {
                       this.state.imgSrc &&
-                      <img src={this.state.imgSrc} style={{minHeight: '130px', maxWidth: '250px', padding: '25px', margin: '-25px'}} />
+                      <img src={this.state.imgSrc} style={{maxHeight: '130px', minWidth: '250px', padding: '25px', margin: '-25px'}} />
                   }
                   <hr style={{marginTop: this.state.imgSrc ? '' : '100px', marginBottom: '5px'}} />
                   <h6 style={{textAlign: 'justify', marginLeft: '10px', marginTop: '10px', fontSize: '16px'}}>{this.state.title}</h6>
