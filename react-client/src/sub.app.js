c749354272339e1ece973cc9e76fff1c77395b2c
anishachhatwani <i130315@nu.edu.pk>
2019-06-19 11:46:11 +0500

    loader

--- react-client/src/sub.app.js
+++ react-client/src/sub.app.js
@@ -61,7 +61,7 @@ class App extends Component {
              <Header />
              <div className='m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body'>
                <Sidebar />
-               { this.props.children }
+               { this.state.showContent && this.props.children }
              </div>
            </div>
            : ['/addfbpages', '/facebookIntegration', '/integrations'].indexOf(this.state.path) > -1
