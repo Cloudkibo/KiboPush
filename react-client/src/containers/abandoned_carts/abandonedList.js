a04d559fddb4a8cb9f2f0cf819fa77e5844551a9
Cloudkibo-Faizan <faizanrasheed4@gmail.com>
2019-08-07 15:49:04 +0500

    Abandoned Carts table in not displaying no records found when there is no record.

--- react-client/src/containers/abandoned_carts/abandonedList.js
+++ react-client/src/containers/abandoned_carts/abandonedList.js
@@ -13,7 +13,7 @@ import { Link } from 'react-router'
 import OrderList from './orderList'
 
 class AbandonedList extends React.Component {
-  constructor (props, context) {
+  constructor(props, context) {
     super(props, context)
     this.props.getAbandonedCarts()
     this.state = {
@@ -24,27 +24,27 @@ class AbandonedList extends React.Component {
     this.onClickSendNow = this.onClickSendNow.bind(this)
   }
 
-  handlePageClick () {
+  handlePageClick() {
     console.log('Need to handle the page click logic here')
   }
 
-  componentWillReceiveProps (nextprops) {
+  componentWillReceiveProps(nextprops) {
     console.log('nextprops', nextprops.abandonedList)
   }
 
-  handleStatusChange () {
+  handleStatusChange() {
     // I am putting 0 because dayem said only one store will be existing for right now
-    this.setState({isActive: !this.state.isActive})
+    this.setState({ isActive: !this.state.isActive })
     let shopId = this.state.store._id
     let statusValue = this.state.isActive
-    this.props.updateStoreStatus({shopId: shopId, isActive: !statusValue}, this.msg)
+    this.props.updateStoreStatus({ shopId: shopId, isActive: !statusValue }, this.msg)
   }
 
-  onClickSendNow (id) {
-    this.props.sendAbandonedCartNow({id: id}, this.msg)
+  onClickSendNow(id) {
+    this.props.sendAbandonedCartNow({ id: id }, this.msg)
   }
 
-  render () {
+  render() {
     var alertOptions = {
       offset: 14,
       position: 'top right',
@@ -72,7 +72,7 @@ class AbandonedList extends React.Component {
                 Or check out this <a>video tutorial</a>
             </div>
           </div>
-            <Analytics />
+          <Analytics />
 
           <div className='row'>
             <div className='col-xl-12'>
@@ -111,92 +111,97 @@ class AbandonedList extends React.Component {
                   </div>
                 </div>
                 <div className='m-portlet__body'>
-                  <div className='m_datatable m-datatable m-datatable--default m-datatable--loaded' id='ajax_data'>
-                    <table className='m-datatable__table' style={{ display: 'block', height: 'auto', overflow: 'inherit' }}>
-                      <thead className='m-datatable__head'>
-                        <tr className='m-datatable__row'
-                          style={{ height: '53px' }}>
-                          <th data-field='page'
-                            className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
-                            <span style={{width: '125px'}}>Created At</span>
-                          </th>
-                          <th data-field='page'
-                            className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
-                            <span style={{width: '125px'}}>Scheduled At</span>
-                          </th>
-                          <th data-field='value'
-                            className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
-                            <span style={{ width: '125px' }}>Cart Value</span>
-                          </th>
-                          <th data-field='status'
-                            className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
-                            <span style={{ width: '125px' }}>Status</span>
-                          </th>
-                          <th data-field='sentCount'
-                            className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
-                            <span style={{ width: '125px' }}>Sent Count</span>
-                          </th>
-                          <th data-field='actions'
-                            className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
-                            <span style={{width: '250px'}}>Actions</span>
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody className='m-datatable__body' style={{textAlign: 'center'}}>
-                        {
-                        (this.props.abandonedList) && this.props.abandonedList.map((item) => {
-                          return <tr className='m-datatable__row m-datatable__row--even' key={item._id}
-                            style={{height: '55px'}}>
-                            <td data-field='page' className='m-datatable__cell'><span style={{width: '125px'}}>{new Date(item.created_at).toString()}</span></td>
-                            <td data-field='page' className='m-datatable__cell'><span style={{width: '125px'}}>{new Date(item.scheduled_at).toString()}</span></td>
-                            <td data-field='value' className='m-datatable__cell'><span style={{width: '125px'}}>{item.totalPrice}</span></td>
-                            <td data-field='status' className='m-datatable__cell'><span style={{width: '125px'}}>{item.status}</span></td>
-                            <td data-field='sentCount' className='m-datatable__cell'><span style={{width: '125px'}}>{item.sentCount}</span></td>
-                            <td data-field='status' className='m-datatable__cell'><span style={{width: '250px'}}>
-                              {
-                                <button className='btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill' onClick={() => { this.onClickSendNow(item._id) }}>
-                                  <span>
-                                    <i className='la la-info' />
-                                    <span>
-                                      {
-                                        'Send Now'
-                                      }
-                                    </span>
-                                  </span>
-                                </button>
-                              }
-                            </span></td>
+                  {this.props.orderList && this.props.orderList.length > 0
+                    ? <div className='m_datatable m-datatable m-datatable--default m-datatable--loaded' id='ajax_data'>
+                      <table className='m-datatable__table' style={{ display: 'block', height: 'auto', overflow: 'inherit' }}>
+                        <thead className='m-datatable__head'>
+                          <tr className='m-datatable__row'
+                            style={{ height: '53px' }}>
+                            <th data-field='page'
+                              className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
+                              <span style={{ width: '125px' }}>Created At</span>
+                            </th>
+                            <th data-field='page'
+                              className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
+                              <span style={{ width: '125px' }}>Scheduled At</span>
+                            </th>
+                            <th data-field='value'
+                              className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
+                              <span style={{ width: '125px' }}>Cart Value</span>
+                            </th>
+                            <th data-field='status'
+                              className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
+                              <span style={{ width: '125px' }}>Status</span>
+                            </th>
+                            <th data-field='sentCount'
+                              className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
+                              <span style={{ width: '125px' }}>Sent Count</span>
+                            </th>
+                            <th data-field='actions'
+                              className='m-datatable__cell--center m-datatable__cell m-datatable__cell--sort'>
+                              <span style={{ width: '250px' }}>Actions</span>
+                            </th>
                           </tr>
-                        })
-                      }
-                      </tbody>
-                    </table>
-                    <div className='pagination'>
-                      <ReactPaginate previousLabel={'previous'}
-                        nextLabel={'next'}
-                        breakLabel={<a>...</a>}
-                        breakClassName={'break-me'}
-                        pageCount={Math.ceil((this.props.abandonedList) ? this.props.abandonedList.length / 10 : 1)}
-                        marginPagesDisplayed={2}
-                        pageRangeDisplayed={3}
-                        onPageChange={this.handlePageClick}
-                        containerClassName={'pagination'}
-                        subContainerClassName={'pages pagination'}
-                        activeClassName={'active'} />
+                        </thead>
+                        <tbody className='m-datatable__body' style={{ textAlign: 'center' }}>
+                          {
+                            (this.props.abandonedList) && this.props.abandonedList.map((item) => {
+                              return <tr className='m-datatable__row m-datatable__row--even' key={item._id}
+                                style={{ height: '55px' }}>
+                                <td data-field='page' className='m-datatable__cell'><span style={{ width: '125px' }}>{new Date(item.created_at).toString()}</span></td>
+                                <td data-field='page' className='m-datatable__cell'><span style={{ width: '125px' }}>{new Date(item.scheduled_at).toString()}</span></td>
+                                <td data-field='value' className='m-datatable__cell'><span style={{ width: '125px' }}>{item.totalPrice}</span></td>
+                                <td data-field='status' className='m-datatable__cell'><span style={{ width: '125px' }}>{item.status}</span></td>
+                                <td data-field='sentCount' className='m-datatable__cell'><span style={{ width: '125px' }}>{item.sentCount}</span></td>
+                                <td data-field='status' className='m-datatable__cell'><span style={{ width: '250px' }}>
+                                  {
+                                    <button className='btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill' onClick={() => { this.onClickSendNow(item._id) }}>
+                                      <span>
+                                        <i className='la la-info' />
+                                        <span>
+                                          {
+                                            'Send Now'
+                                          }
+                                        </span>
+                                      </span>
+                                    </button>
+                                  }
+                                </span></td>
+                              </tr>
+                            })
+                          }
+                        </tbody>
+                      </table>
+                      <div className='pagination'>
+                        <ReactPaginate previousLabel={'previous'}
+                          nextLabel={'next'}
+                          breakLabel={<a>...</a>}
+                          breakClassName={'break-me'}
+                          pageCount={Math.ceil((this.props.abandonedList) ? this.props.abandonedList.length / 10 : 1)}
+                          marginPagesDisplayed={2}
+                          pageRangeDisplayed={3}
+                          onPageChange={this.handlePageClick}
+                          containerClassName={'pagination'}
+                          subContainerClassName={'pages pagination'}
+                          activeClassName={'active'} />
+                      </div>
                     </div>
-                  </div>
-                </div>
+                    : <div className='table-responsive'>
+                      <p> No data to display </p>
+                    </div>
+                  }
               </div>
             </div>
           </div>
-          <OrderList />
         </div>
+        <OrderList />
       </div>
+      </div >
     )
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   console.log('state', state)
   return {
     abandonedList: (state.abandonedInfo.abandonedList),
@@ -205,7 +210,7 @@ function mapStateToProps (state) {
   }
 }
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return bindActionCreators(
     {
       getAbandonedCarts: getAbandonedCarts,
