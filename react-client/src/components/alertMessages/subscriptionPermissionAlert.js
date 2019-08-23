6453f8bb9ecfaf919319aa425262a06e07d1432a
anishachhatwani <i130315@nu.edu.pk>
2019-08-23 14:59:19 +0500

    alert message

--- react-client/src/components/alertMessages/subscriptionPermissionAlert.js
+++ react-client/src/components/alertMessages/subscriptionPermissionAlert.js
@@ -35,7 +35,7 @@ class SubscriptionPermissionAlert extends React.Component {
   render () {
     if(this.state.pages.length > 0) {
       return (
-        <div className='alert alert-primary alert-dismissible fade show' role='alert' style={{marginBottom: '-0.17rem'}}>
+        <div className='alert alert-danger alert-dismissible fade show' role='alert' style={{marginBottom: '-0.17rem'}}>
           {this.state.isShowingModal &&
             <ModalContainer style={{width: '500px'}}
               onClose={this.closeModal}>
@@ -43,27 +43,31 @@ class SubscriptionPermissionAlert extends React.Component {
                 onClose={this.closeModal}>
                 <h5>Subscription Messaging Policy Change</h5>
                 <br />
-                <p>You do not have Page Level Subscription Permission for the following pages.</p>
-                  {this.state.pages.map((page, i) => (
+                <p> Here is the Page Level Subscription Permission status for your page(s):</p>
+                  {this.props.pages.map((page, i) => (
                   <span>
                     <span>
                       <img alt='pic' style={{height: '30px'}} src={(page.pagePic) ? page.pagePic : 'https://cdn.cloudkibo.com/public/icons/users.jpg'} />&nbsp;&nbsp;
                       <span>{page.pageName}</span>&nbsp;&nbsp;&nbsp;
+                      {page.gotPageSubscriptionPermission
+                      ? <span className='m-badge m-badge--wide m-badge--success'>Granted</span>
+                      : <span className='m-badge m-badge--wide m-badge--danger'>Not Granted</span>
+                      }
                     </span>
                     <br /><br />
                   </span>
                   ))
                 }
-                <p>This means that you will not be able to send subscription messages to your subscribers outside of the 24 hour window. Please click <a href='https://kibopush.com/2019/08/02/facebook-subscription-messaging-policy-change/' target='_blank' onClick={this.closeModal}>Here</a> to know how you can apply for this permission.</p>
+                <p>This means that outside of the 24 hour window, you will not be able to send subscription messages to subscribers of those pages that have not been granted this permission. Please click <a href='https://kibopush.com/2019/08/02/facebook-subscription-messaging-policy-change/' target='_blank' onClick={this.closeModal}>Here</a> to know how you can apply for this permission.</p>
               </ModalDialog>
             </ModalContainer>
           }
-          <button type='button' className='close' data-dismiss='alert' aria-label='Close' style={{top: '-0.8rem'}}></button>
+          <button type='button' className='close' data-dismiss='alert' aria-label='Close' style={{marginTop: '-0.5rem'}}></button>
           <strong style={{fontWeight: 'bold'}}>
             Attention!
           </strong>&nbsp;
-          As per the New Facebook Policy, you won't be able to send messages to your subscribers! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <button onClick={this.showModal} type='button' className='btn btn-sm m-btn--pill m-btn--air btn-secondary'>View More</button>
+          As per the New Facebook Policy, you will not be able to send messages to your subscribers! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <button onClick={this.showModal} type='button' className='btn btn-sm btn-secondary'>View More</button>
         </div>
       )
     } else {
