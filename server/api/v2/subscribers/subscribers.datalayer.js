768b83eecdc31bb44bbbbfb8269d7e3f1000f3fc
Sojharo <sojharo@live.com>
2019-08-23 13:15:55 +0500

    removed old code which is not used

--- server/api/v2/subscribers/subscribers.datalayer.js
+++
@@ -1,7 +0,0 @@
-const TagsSubscribers = require('./../../v1/tags_subscribers/tags_subscribers.model')
-
-exports.findTaggedSubscribers = (query) => {
-  return TagsSubscribers.find(query)
-    .populate('tagId')
-    .exec()
-}
