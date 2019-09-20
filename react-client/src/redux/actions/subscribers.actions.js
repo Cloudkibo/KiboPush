46a68e59a3954eca30194e50ef6beb33e4849d56
Baqar <bjafri5@gmail.com>
2019-09-20 19:50:02 +0500

    live chat profile picture logic change

--- react-client/src/redux/actions/subscribers.actions.js
+++ react-client/src/redux/actions/subscribers.actions.js
@@ -118,7 +118,7 @@ export function updatePicture (subscriberData, fetchData, callback) {
           })
         }
         if (callback) {
-          callback()
+          callback(res.payload)
         }
       }
     })
