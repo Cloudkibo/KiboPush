6825b315dc1a1ead2506890fd49f7c307c542428
Baqar <bjafri5@gmail.com>
2019-10-10 15:09:28 +0500

    fix

--- react-client/src/utility/socketio.js
+++ react-client/src/utility/socketio.js
@@ -110,7 +110,7 @@ socket.on('message', (data) => {
   } else if (['tag_assign', 'tag_unassign'].indexOf(data.action) > -1) {
     store.dispatch(loadAllSubscribersListNew({last_id: 'none', number_of_records: 10, first_page: 'first', filter: false, filter_criteria: {search_value: '', gender_value: '', page_value: '', locale_value: '', tag_value: '', status_value: ''}}))
   } else if (data.action === 'session_assign') {
-    store.dispatch(fetchSingleSession(data.payload.session_id))
+    store.dispatch(updateSessions(data.payload.data))
   } else if (data.action === 'session_status') {
     if (data.payload.status === 'new') {
       store.dispatch(fetchSingleSession(data.payload.session_id, {appendTo: 'open', deleteFrom: 'close'}))
