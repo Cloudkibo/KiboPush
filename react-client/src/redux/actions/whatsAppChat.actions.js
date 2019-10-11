84ecd408405875275e835b4ffe33576ec0c8044a
Sojharo <sojharo@live.com>
2019-10-11 17:29:33 +0500

    merge conflicts #5134

--- react-client/src/redux/actions/whatsAppChat.actions.js
+++ react-client/src/redux/actions/whatsAppChat.actions.js
@@ -124,21 +124,7 @@ export function sendAttachment (data, handleSendAttachment) {
     })
   }
 }
-export function changeStatus (data, handleStatus) {
-  return (dispatch) => {
-    callApi('whatsAppChat/changeStatus', 'post', data).then(res => {
-      handleStatus(res)
-    })
-  }
-}
 
-export function updatePendingResponse (data, handlePendingResponse) {
-  return (dispatch) => {
-    callApi(`whatsAppChat/updatePendingResponse`, 'post', data).then(res => {
-      handlePendingResponse(res)
-    })
-  }
-}
 export function unSubscribe (id, data) {
   return (dispatch) => {
     callApi(`whatsAppContacts/update/${id}`, 'post', data).then(res => {
