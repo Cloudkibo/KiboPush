d22829ab25237896b02428a42529e24afb58ae52
Baqar <bjafri5@gmail.com>
2019-11-19 14:53:27 +0500

    changes

--- react-client/src/components/SimplifiedBroadcastUI/GenericMessage.js
+++ react-client/src/components/SimplifiedBroadcastUI/GenericMessage.js
@@ -98,6 +98,8 @@ class GenericMessage extends React.Component {
   }
 
   componentDidMount () {
+    window.closeAddComponentModal = this.closeAddComponentModal.bind(this)
+
     console.log('genericMessage props in componentDidMount', this.props)
     if (this.state.broadcast && this.state.broadcast.length > 0) {
       this.initializeList(this.state.broadcast)
@@ -174,7 +176,7 @@ class GenericMessage extends React.Component {
   }
 
   closeAddComponentModal () {
-    // document.body.style.overflow = 'auto'
+    document.body.style.overflow = 'auto'
     this.setState({isShowingAddComponentModal: false, editData: null})
     this.refs.singleModal.click()
   }
