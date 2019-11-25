deeefad6c0836eab68c2018cc0c431bd4abf5edb
ImranBinShoukat <imran.shoukat1995@gmail.com>
2019-11-25 14:57:52 +0500

    handle action block

--- react-client/src/components/FlowBuilder/sidebar.js
+++ react-client/src/components/FlowBuilder/sidebar.js
@@ -1,11 +1,61 @@
 import React from "react"
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import SIDEBARITEM from './sidebarItem'
+import { REACT_FLOW_CHART } from "@mrblenny/react-flow-chart"
 
 class Sidebar extends React.Component {
   constructor (props, context) {
     super(props, context)
     this.state = {}
+    this.onDragMessageBlock = this.onDragMessageBlock.bind(this)
+    this.onDragActionBlock = this.onDragActionBlock.bind(this)
+  }
+
+  componentDidMount () {
+    document.addEventListener('drop', function (event) {
+      let data = event.dataTransfer.getData(REACT_FLOW_CHART)
+      console.log('REACT_FLOW_CHART', data)
+    })
+  }
+
+  onDragMessageBlock (event) {
+    let id = new Date().getTime() + Math.floor(Math.random() * 100)
+    let data = {
+      id,
+      title: 'Message Block',
+      messageContent: []
+    }
+    this.props.unlinkedMessages.push(data)
+    let chartData = {
+      type: 'component_block',
+      ports: {
+        port1: {
+          id: 'port1',
+          type: 'input'
+        }
+      },
+      properties: {
+        id
+      }
+    }
+    event.dataTransfer.setData(REACT_FLOW_CHART, JSON.stringify(chartData))
+  }
+
+  onDragActionBlock (event) {
+    let id = new Date().getTime() + Math.floor(Math.random() * 100)
+    let chartData = {
+      type: 'action_block',
+      ports: {
+        port1: {
+          id: 'port1',
+          type: 'input'
+        }
+      },
+      properties: {
+        id
+      }
+    }
+    event.dataTransfer.setData(REACT_FLOW_CHART, JSON.stringify(chartData))
   }
 
   render () {
@@ -20,14 +70,12 @@ class Sidebar extends React.Component {
           <SIDEBARITEM
             name='Message Block'
             type='component_block'
-            ports={{}}
-            properties={{}}
+            onDrag={this.onDragMessageBlock}
           />
           <SIDEBARITEM
             name='Actions Block'
             type='action_block'
-            ports={{}}
-            properties={{}}
+            onDrag={this.onDragActionBlock}
           />
         </div>
       </div>
@@ -36,7 +84,7 @@ class Sidebar extends React.Component {
 }
 
 Sidebar.propTypes = {
-
+  'unlinkedMessages': PropTypes.array.isRequired
 }
 
 export default Sidebar
