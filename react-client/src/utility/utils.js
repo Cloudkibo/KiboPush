086d5d4851f9bdc557d32bc20d5d30085f09ad34
sania <sania@cloudkibo.com>
2019-12-18 14:28:19 +0500

    Making Auto Reply Editable in Comment Ca[tyre

--- react-client/src/utility/utils.js
+++ react-client/src/utility/utils.js
@@ -91,7 +91,7 @@ export function isWebViewUrl (value) {
 }
 export function isFacebookPageUrl (value) {
   /* contains facebook url followed by alphabets, numerals or dot */
-  let regexp = /^(https?:\/\/)?(www\.)?facebook.com\/(?!\/)[.a-zA-Z0-9/]*/g
+  let regexp = /^(https?:\/\/)?(www\.|web\.)?facebook.com\/(?!\/)[.a-zA-Z0-9/]*/g
   return regexp.test(value)
 }
 export function isTwitterUrl (value) {
