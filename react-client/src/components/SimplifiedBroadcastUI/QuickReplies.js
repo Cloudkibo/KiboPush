caf6498450437b854f1e4951b9dc4b7f38c17a6f
Baqar <bjafri5@gmail.com>
2020-02-06 15:35:30 +0500

    quick reply popover

--- react-client/src/components/SimplifiedBroadcastUI/QuickReplies.js
+++ react-client/src/components/SimplifiedBroadcastUI/QuickReplies.js
@@ -711,7 +711,7 @@ class QuickReplies extends React.Component {
                 </button>
             }
 
-            <Popover placement='auto' isOpen={this.state.addingQuickReply} target={'addQuickReply'+this.props.currentId}>
+            <Popover container={document.getElementById('quickReplyPopover')} placement='auto' isOpen={this.state.addingQuickReply} target={'addQuickReply'+this.props.currentId}>
                 <PopoverBody>
                     <div style={{paddingRight: '10px', maxHeight: '500px', overflowY: 'scroll', overflowX: 'hidden'}}>
                     <div data-toggle="modal" data-target={this.state.editing ? "#closeQuickReply" : ""} onClick={this.closeQuickReply} style={{marginLeft: '98%', cursor: 'pointer'}}><span role='img' aria-label='times'>❌</span></div>
