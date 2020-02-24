ee4aa4ae32f8375d63146696dd99b4a075f41efe
Baqar <bjafri5@gmail.com>
2020-02-24 16:18:23 +0500

    session header logic

--- react-client/src/components/LiveChat/sessions/index.js
+++ react-client/src/components/LiveChat/sessions/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
-import YouTube from 'react-youtube'
+import HEADER from './header'
 
 class Sessions extends React.Component {
   constructor(props, context) {
@@ -12,7 +11,22 @@ class Sessions extends React.Component {
   render() {
     return (
       <div className='col-xl-4'>
-
+        <div className='m-portlet m-portlet--full-height'>
+          <HEADER
+              pages={this.props.pages ? this.props.pages : []}
+              removeFilters={this.props.removeFilters}
+              updateFilterSort={this.props.updateFilterSort}
+              updateFilterPage={this.props.updateFilterPage}
+              updateFilterSearch={this.props.updateFilterSearch}
+              updateFilterPending={this.props.updateFilterPending}
+              updateFilterUnread={this.props.updateFilterUnread}
+              filterSort={this.props.filterSort}
+              filterPage={this.props.filterPage}
+              filterSearch={this.props.filterSearch}
+              filterPending={this.props.filterPending}
+              filterUnread={this.props.filterUnread}
+          />
+        </div>
       </div>
     )
   }
