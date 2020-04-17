1c54ba1ccb4faa51614c9f72c9abef23b262dd1a
anishachhatwani <i130315@nu.edu.pk>
2020-04-17 12:41:34 +0500

    preview text

--- react-client/src/components/SimplifiedBroadcastUI/TextModal.js
+++ react-client/src/components/SimplifiedBroadcastUI/TextModal.js
@@ -172,8 +172,8 @@ class TextModal extends React.Component {
             <div className='col-5'>
               <h4 style={{ marginLeft: '-50px' }}>Preview:</h4>
               <div className='ui-block' style={{ overflowY: 'auto', border: '1px solid rgba(0,0,0,.1)', borderRadius: '3px', minHeight: '68vh', maxHeight: '68vh', marginLeft: '-50px' }} >
-                <div className='discussion' style={{ display: 'inline-block', marginTop: '100px' }} >
-                  <div style={{ maxWidth: '100%', fontSize: '16px', whiteSpace: 'pre' }} className='bubble recipient'>{this.state.text}</div>
+                <div className='discussion' style={{ display: 'inline-block', marginTop: '100px', paddingLeft: '10px', paddingRight: '10px' }} >
+                  <div style={{ maxWidth: '100%', fontSize: '16px' }} className='bubble recipient'>{this.state.text}</div>
                   {
                     this.state.buttons.map((button, index) => (
                       button.visible && (
