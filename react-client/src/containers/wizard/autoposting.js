b3480c51816901078ed3dda9c5d651905de8e1a5
arveenkumar55 <arveen@cloudkibo.com>
2020-07-01 15:06:34 +0500

    fix_add_button_issue_in_wizard-n

--- react-client/src/containers/wizard/autoposting.js
+++ react-client/src/containers/wizard/autoposting.js
@@ -207,7 +207,7 @@ class Autoposting extends React.Component {
         <div style={{ float: 'left', clear: 'both' }}
           ref={(el) => { this.top = el }} />
 
-        <div style={{ background: 'rgba(33, 37, 41, 0.6)' }} className="modal fade" id="addFeed" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        {/* <div style={{ background: 'rgba(33, 37, 41, 0.6)' }} className="modal fade" id="addFeed" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div style={{ transform: 'translate(0, 0)' }} className="modal-dialog" role="document">
             <div className="modal-content">
               <div style={{ display: 'block' }} className="modal-header">
@@ -225,7 +225,8 @@ class Autoposting extends React.Component {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
+         <AddChannel module='wizard' addFeedClose={this.refs.addFeedClose} onClose={this.closeDialog} data-toggle="modal" data-target="#guide" openGuidelines={this.viewGuide} msg={this.msg} />
         <a href='#/' style={{ display: 'none' }} ref='delete' data-toggle="modal" data-target="#delete">delete</a>
         <div style={{ background: 'rgba(33, 37, 41, 0.6)' }} className="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div style={{ transform: 'translate(0, 0)' }} className="modal-dialog" role="document">
