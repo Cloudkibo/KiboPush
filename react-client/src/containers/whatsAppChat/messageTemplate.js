444653b08cb5178c8a76e8c68ccb1e40f62160de
Baqar <bjafri5@gmail.com>
2020-07-14 18:02:17 +0500

    fix

--- react-client/src/containers/whatsAppChat/messageTemplate.js
+++ react-client/src/containers/whatsAppChat/messageTemplate.js
@@ -130,13 +130,13 @@ class MessageTemplate extends React.Component {
   }
 
   sendTemplate () {
-    this.setState({sendingTemplate: true})
     if (this.props.sendingToNewNumber) {
       const regex = /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*(\d{1,14})$/g
       if (!this.state.number.match(regex)) {
         this.props.alertMsg.error('Invalid Number')
         return
       }
+      this.setState({sendingTemplate: true})
       this.props.createNewContact({
         number: this.state.number
       }, (res) => {
@@ -145,6 +145,7 @@ class MessageTemplate extends React.Component {
         })
       })
     } else {
+      this.setState({sendingTemplate: true})
       this._sendTemplate()
     }
   }
