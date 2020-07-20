9cd9dff0add294a6ebc4595b1d26797fd7035606
anishachhatwani <i130315@nu.edu.pk>
2020-07-20 16:06:16 +0500

    rich media

--- react-client/src/containers/whatsAppChat/whatsAppChat.js
+++ react-client/src/containers/whatsAppChat/whatsAppChat.js
@@ -597,21 +597,25 @@ class WhatsAppChat extends React.Component {
           </div>
         }
 
-        <TEMPLATESMODAL
-          sendChatMessage={this.props.sendChatMessage}
-          setMessageData={this.setMessageData}
-          activeSession={this.state.activeSession}
-          updateState={this.updateState}
-          userChat={this.state.userChat}
-          sessions={this.state.sessions}
-          alertMsg={this.alertMsg}
-          id='messageTemplateNewNumber'
-          sendingToNewNumber={this.state.sendingToNewNumber}
-          heading={'Send Message Template to WhatsApp Number'}
-          createNewContact={this.props.createNewContact}
-          changeActiveSession={this.changeActiveSession}
-        />
-        <NEWMESSAGEBUTTON
+        <div style={{ background: 'rgba(33, 37, 41, 0.6)' }} className="modal fade" id='messageTemplateNewNumber' tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+         <div style={{ transform: 'translate(0, 0)' }} className="modal-dialog" role="document">
+           <MESSAGETEMPLATE
+             sendChatMessage={this.props.sendChatMessage}
+             setMessageData={this.setMessageData}
+             activeSession={this.state.activeSession}
+             updateState={this.updateState}
+             userChat={this.state.userChat}
+             sessions={this.state.sessions}
+             alertMsg={this.alertMsg}
+             id='messageTemplateNewNumber'
+             sendingToNewNumber={this.state.sendingToNewNumber}
+             heading={'Send Message Template to WhatsApp Number'}
+             createNewContact={this.props.createNewContact}
+             changeActiveSession={this.changeActiveSession}
+           />
+         </div>
+       </div>
+       <NEWMESSAGEBUTTON
           dataToggle='modal'
           dataTarget='#messageTemplateNewNumber'
           onClick={() => this.sendingToNewNumber(true)}
