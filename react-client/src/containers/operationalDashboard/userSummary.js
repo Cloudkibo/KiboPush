dc590ca60faf2ca883b81fbefb6d5619182a8495
sania <sania@cloudkibo.com>
2020-07-27 14:19:49 +0500

    Operational dashboard enhancement

--- react-client/src/containers/operationalDashboard/userSummary.js
+++ react-client/src/containers/operationalDashboard/userSummary.js
@@ -5,8 +5,30 @@ import IconStack from '../../components/Dashboard/IconStack'
 class UserSummary extends React.Component {
   constructor(props, context) {
     super(props, context)
+    this.state = {
+      typingInterval: 1000,
+      days: '10'
+    }
+    this.onDaysChange = this.onDaysChange.bind(this)
+  }
+  onDaysChange (e) {
+    this.setState({
+      days: e.target.value
+    })
   }
 
+  componentDidMount () {
+    let typingTimer
+    let doneTypingInterval = this.state.typingInterval
+    let input = document.getElementById(`userSummaryFilter`)
+    input.addEventListener('keyup', () => {
+      clearTimeout(typingTimer)
+      typingTimer = setTimeout(() => {
+          this.props.onDaysChange(this.state.days)
+      }, doneTypingInterval)
+    })
+    input.addEventListener('keydown', () => {clearTimeout(typingTimer)})
+  }
   render() {
     return (
      <div className='row' style={{height: '200px'}}>
@@ -26,7 +48,7 @@ class UserSummary extends React.Component {
                   Show records for last:&nbsp;&nbsp;
                 </span>
                 <div style={{width: '200px'}}>
-                  <input placeholder='Enter number of days' type='number' min='1' step='1' value={this.props.days} className='form-control' onChange={this.props.onDaysChange} />
+                  <input placeholder='Enter number of days' id='userSummaryFilter' type='number' min='1' step='1' value={this.state.days} className='form-control' onChange={this.onDaysChange} />
                 </div>
                 <span htmlFor='example-text-input' className='col-form-label'>
                 &nbsp;&nbsp;days
