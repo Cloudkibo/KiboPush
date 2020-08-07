022fee1b9b50967e12bb70d66bfa2836983d46fe
Baqar <bjafri5@gmail.com>
2020-08-07 15:58:05 +0500

    ui fixes

--- react-client/src/containers/settings/configuration.js
+++ react-client/src/containers/settings/configuration.js
@@ -161,11 +161,7 @@ class Configuration extends React.Component {
 
   handleResponse(payload) {
     this.refs.connectWapp.click()
-    if (payload.showModal) {
-      this.refs.disconnectWhatsApp.click()
-    } else {
-      this.msg.success('Saved Successfully')
-    }
+    this.msg.success('Saved Successfully')
   }
 
   submitWapp(event) {
@@ -206,7 +202,7 @@ class Configuration extends React.Component {
             &times;
           </span>
         </button>
-        <button ref='connectWapp' style={{ marginTop: '-10px', opacity: '0.5', color: 'black', display: 'none' }} type="button" className="close" data-toggle="modal" data-target="#connectWapp" aria-label="Close">
+        <button onClick={this.clearFieldsWapp} ref='connectWapp' style={{ marginTop: '-10px', opacity: '0.5', color: 'black', display: 'none' }} type="button" className="close" data-toggle="modal" data-target="#connectWapp" aria-label="Close">
           <span aria-hidden="true">
             &times;
           </span>
