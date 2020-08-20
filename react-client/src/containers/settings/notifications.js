1c64dd2eb1f577a76386956d175ac15a619a2590
sania <sania@cloudkibo.com>
2020-08-20 14:52:43 +0500

    Typo

--- react-client/src/containers/settings/notifications.js
+++ react-client/src/containers/settings/notifications.js
@@ -26,7 +26,7 @@ class Notifications extends React.Component {
   }
   getAlertDescription(type) {
     if (type === 'unresolveSessionAlert') {
-      return 'This alert will let the assigned team members know that a livechat session has been unresolved for the last `n` minutes'
+      return 'This alert will let the assigned team members know that a livechat session is unresolved for the last `n` minutes'
     } else if (type === 'pendingSessionAlert') {
       return 'This alert will let the assigned team members know that a livechat session has been pending response for the last `n` minutes'
     }
