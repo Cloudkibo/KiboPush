fa50102e278ae5e54019f002419f11f4babb43a0
arveen kumar <arveenkumar55@gmail.com>
2020-09-16 17:11:32 +0500

    fix_bug

--- react-client/src/components/header/header.js
+++ react-client/src/components/header/header.js
@@ -255,7 +255,7 @@ class Header extends React.Component {
       showHeaderTopbar
     } = this.props
     return (
-      <header className='m-grid__item m-header ' data-minimize-offset='200' data-minimize-mobile-offset='200' >
+      <header id='headerDiv' className='m-grid__item m-header ' data-minimize-offset='200' data-minimize-mobile-offset='200' >
         <div className='m-container m-container--fluid m-container--full-height'>
           <div className='m-stack m-stack--ver m-stack--desktop'>
             <div className={`m-stack__item m-brand ${skin === 'dark' ? 'm-brand--skin-dark' : ''}`}>
