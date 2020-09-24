c60dc761aaffaa8b0b2558eaef79ec93566c0b93
sania <sania@cloudkibo.com>
2020-09-24 15:49:18 +0500

    notifications redirecting to chat session

--- react-client/src/components/header/header.js
+++ react-client/src/components/header/header.js
@@ -190,17 +190,17 @@ class Header extends React.Component {
       if (this.props.user.platform === 'messenger') {
         this.props.history.push({
           pathname: `/liveChat`,
-          state: { id: id }
+          state: { id: id, module: 'notifications'}
         })
       } else if (this.props.user.platform === 'whatsApp') {
         this.props.history.push({
           pathname: `/whatsAppChat`,
-          state: { id: id }
+          state: { id: id, module: 'notifications' }
         })
       } else if (this.props.user.platform === 'sms') {
         this.props.history.push({
           pathname: `/smsChat`,
-          state: { id: id }
+          state: { id: id, module: 'notifications' }
         })
       }
     }
