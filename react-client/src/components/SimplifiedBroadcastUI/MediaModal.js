8edbaeb649f3e86b761dec1d9351e6fc6fa8cd38
arveen kumar <arveenkumar55@gmail.com>
2020-12-15 12:36:47 +0500

    fix_bug

--- react-client/src/components/SimplifiedBroadcastUI/MediaModal.js
+++ react-client/src/components/SimplifiedBroadcastUI/MediaModal.js
@@ -118,8 +118,12 @@ class MediaModal extends React.Component {
     this.setState(status)
   }
 
-  updateImage(imgSrc) {
-    this.setState({ imgSrc, edited: true })
+  updateImage(imgSrc, isFile) {
+    if(isFile) {
+      this.setState({ imgSrc, edited: true, file: ''})
+    } else{
+      this.setState({ imgSrc, edited: true})
+    }
   }
 
   updateFile(file) {
