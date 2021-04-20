b24706394a2d4ba7ee318f7c09474b2557fa6983
Sojharo <sojharo@live.com>
2021-04-21 00:28:52 +0500

    sms work CloudKibo/KiboPush#11448 CloudKibo/KiboPush#11451

--- react-client/src/containers/integrations/SmsBillingScreen.js
+++ react-client/src/containers/integrations/SmsBillingScreen.js
@@ -5,18 +5,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import AlertContainer from 'react-alert'
+import {StripeProvider, Elements} from 'react-stripe-elements'
+import InjectedCheckoutForm from './../wizard/checkout'
+import { updateCard, getKeys } from '../../redux/actions/basicinfo.actions'
 import Header from './../wizard/header'
 
 class SmsBillingScreen extends React.Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      fblink: '',
-      copied: false,
-      selectPage: {},
-      selectedTab: 'becomeSubscriber',
-      sendTestMessage: false,
+      showCardModal: false
     }
+    props.getKeys()
+    this.setCard = this.setCard.bind(this)
+    this.showCardModal = this.showCardModal.bind(this)
+    this.closeCardModal = this.closeCardModal.bind(this)
+    this.onSuccessCallback = this.onSuccessCallback.bind(this)
+  }
+
+  onSuccessCallback () {
+    document.getElementById('_close_payment_modal').click()
+  }
+
+  closeCardModal () {
+    this.setState({showCardModal: false})
+  }
+
+  showCardModal () {
+    this.setState({showCardModal: true})
+  }
+
+  setCard (payload, value) {
+    this.props.updateCard({companyId: this.props.user.companyId, stripeToken: payload}, this.msg, this.onSuccessCallback)
   }
 
   componentDidMount () {
@@ -36,10 +57,17 @@ class SmsBillingScreen extends React.Component {
   }
 
   render () {
+    var alertOptions = {
+      offset: 14,
+      position: 'bottom right',
+      theme: 'dark',
+      time: 5000,
+      transition: 'scale'
+    }
     return (
       <div>
         <Header />
-      <div className="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-grid--tablet-and-mobile m-grid--hor-tablet-and-mobile m-login m-login--1 m-login--singin" style={{ height: '100vh' }}>
+        <div className="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-grid--tablet-and-mobile m-grid--hor-tablet-and-mobile m-login m-login--1 m-login--singin" style={{ height: '100vh' }}>
           <div className="m-grid__item m-grid__item--fluid m-grid m-grid--center m-grid--hor m-grid__item--order-tablet-and-mobile-1	m-login__content" style={{backgroundImage: 'url(https://cdn.cloudkibo.com/public/assets/app/media/img//bg/bg-4.jpg)'}}>
             <div className="m-grid__item m-grid__item--middle">
               <h3 className="m-login__welcome">
@@ -53,59 +81,96 @@ class SmsBillingScreen extends React.Component {
             </div>
           </div>
           <div className="m-grid__item m-grid__item--order-tablet-and-mobile-2 m-login__aside" style={{padding: '2rem'}}>
-            <div className="sequence-box" style={{height: '10em'}}>
-              <span>
-                <span className="sequence-name">
-                  Free Plan
-                </span>
-                <br />
-                <span>
-                  <span>
-                    Connect your Twilio trial account <br /> and use our features for free
-                  </span>
-                </span>
-              </span>
-              <span className="sequence-text sequence-centered-text" style={{position: 'absolute', left: '77%', top: '25%'}}>
-                <span className="sequence-number">
-                  FREE
-                </span>
-              </span>
-            </div>
-            <div className="sequence-box" style={{height: '10em'}}>
-              <span>
-                <span className="sequence-name">
-                  Basic Plan
-                </span>
-                <br />
-                <span>
-                  <span>
-                    Connect your Bandwidth account <br /> to send messages upto 5000
-                  </span>
-                </span>
-              </span>
-              <span className="sequence-text sequence-centered-text" style={{position: 'absolute', left: '77%', top: '25%'}}>
-                <span className="sequence-number">
-                  $35
-                </span>
-              </span>
-            </div>
-            <div className="sequence-box" style={{height: '10em'}}>
-              <span>
-                <span className="sequence-name">
-                  Standard Plan
-                </span>
-                <br />
-                <span>
-                  <span>
-                    Connect your Bandwidth account <br /> to send messages upto 7500
-                  </span>
-                </span>
-              </span>
-              <span className="sequence-text sequence-centered-text" style={{position: 'absolute', left: '77%', top: '25%'}}>
-                <span className="sequence-number">
-                  $50
-                </span>
-              </span>
+            <div id='target' className='col-lg-8 col-md-8 col-sm-8 col-xs-12' style={{maxWidth: '100%' }}>
+              <AlertContainer ref={a => { this.msg = a }} {...alertOptions} />
+              <div style={{height: '82vh'}} className='m-portlet m-portlet--tabs  '>
+                <div className='m-portlet__head'>
+                  <div className='m-portlet__head-tools'>
+                    <ul className='nav nav-tabs m-tabs m-tabs-line   m-tabs-line--left m-tabs-line--primary' role='tablist'>
+                      <li className='nav-item m-tabs__item'>
+                        <span className='nav-link m-tabs__link active'>
+                          <i className='flaticon-share m--hide' />
+                          Payment Methods
+                        </span>
+                      </li>
+                    </ul>
+                    <button className='btn btn-primary m-btn m-btn--custom m-btn--icon m-btn--air m-btn--pill' onClick={this.showCardModal} data-toggle="modal" data-backdrop='static' data-keyboard='false' data-target="#paymentMethode" style={{marginTop: '15px'}}>
+                      <span>
+                        <i className='la la-plus' />
+                        <span>
+                          Add
+                        </span>
+                      </span>
+                    </button>
+                  </div>
+                </div>
+                <div className='tab-content'>
+                  <div className='m-content'>
+                    {
+                      this.props.user && !this.props.user.last4 &&
+                        <label style={{fontWeight: 'inherit'}} onClick={this.change}>
+                          You haven't provided any payment method as yet. Click on Add button to enter credit/debit card details
+                        </label>
+                    }
+                    <div className='row'>
+                      <div className='col-xl-12 col-md-12 col-sm-12'>
+                        <div>
+                          <div className='m-portlet__body'>
+                            <div className='tab-content'>
+                              {
+                                this.props.user && this.props.user.last4 &&
+                                  <div className='tab-pane active' id='m_widget4_tab1_content'>
+                                    <div className='m-widget4' >
+                                      <div className='m-widget4__item'>
+                                        <div className='m-widget4__info'>
+                                          <i className='fa fa-credit-card-alt' />&nbsp;&nbsp;
+                                          <span className='m-widget4__title'>
+                                            xxxx xxxx xxxx {this.props.user.last4}
+                                          </span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                              }
+                            </div>
+                            <div style={{ background: 'rgba(33, 37, 41, 0.6)' }} className="modal fade" id="paymentMethode" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                              <div style={{ transform: 'translate(0, 0)' }} className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                  <div style={{ display: 'block' }} className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">
+                                      Credit/Debit Card Details
+                                    </h5>
+                                    <button id='_close_payment_modal' onClick={this.closeCardModal} style={{ marginTop: '-10px', opacity: '0.5', color: 'black' }} type="button" data-dismiss='modal' className="close" aria-label="Close">
+                                      <span aria-hidden="true">
+                                        &times;
+                                      </span>
+                                    </button>
+                                  </div>
+                                  <div style={{ color: 'black' }} className="modal-body">
+                                    {
+                                      this.state.showCardModal &&
+                                        <div className='col-12'>
+                                          {
+                                            this.props.stripeKey && this.props.captchaKey &&
+                                              <StripeProvider apiKey={this.props.stripeKey}>
+                                                <Elements>
+                                                  <InjectedCheckoutForm setCard={this.setCard} captchaKey={this.props.captchaKey} />
+                                                </Elements>
+                                              </StripeProvider>
+                                          }
+                                        </div>
+                                    }
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -122,7 +187,10 @@ function mapStateToProps (state) {
 }
 
 function mapDispatchToProps (dispatch) {
-  return bindActionCreators({}, dispatch)
+  return bindActionCreators({
+    updateCard: updateCard,
+    getKeys: getKeys
+  }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SmsBillingScreen)
